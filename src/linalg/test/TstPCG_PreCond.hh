65f466d98aaa2dde62aed2b27138ac9f05709e2a
wdn <wdn@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1997-09-26 21:38:01 +0000

    A class to implement the preconditioner for a pcg test problem.

---
+++ src/linalg/test/TstPCG_PreCond.hh
@@ -0,0 +1,37 @@
+//----------------------------------*-C++-*----------------------------------//
+// TstPCG_PreCond.hh
+// Dave Nystrom
+// Fri May  9 13:40:54 1997
+//---------------------------------------------------------------------------//
+// @> 
+//---------------------------------------------------------------------------//
+
+#ifndef __linalg_test_TstPCG_PreCond_hh__
+#define __linalg_test_TstPCG_PreCond_hh__
+
+#include "linalg/PCG_PreCond.hh"
+
+#include "ds++/Mat.hh"
+
+//===========================================================================//
+// class TstPCG_PreCond - 
+
+// 
+//===========================================================================//
+
+template<class T>
+class TstPCG_PreCond : public PCG_PreCond<T> {
+
+  public:
+    TstPCG_PreCond();
+    ~TstPCG_PreCond();
+
+    void  Left_PreCond( Mat1<T>& x, Mat1<T>&b );
+    void Right_PreCond( Mat1<T>& x, Mat1<T>&b );
+};
+
+#endif                          // __linalg_test_TstPCG_PreCond_hh__
+
+//---------------------------------------------------------------------------//
+//                              end of linalg/test/TstPCG_PreCond.hh
+//---------------------------------------------------------------------------//
