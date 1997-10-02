16cbeb290c7a7722228e1dbc04d5c01be2c5fe70
furnish <furnish@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1997-10-02 16:21:57 +0000

    A test case for development of the 3d solver for the radiation energy equation.

---
+++ src/t3/Test_Prob.cc
@@ -0,0 +1,201 @@
+//----------------------------------*-C++-*----------------------------------//
+// Test_Prob.cc
+// Geoffrey Furnish
+// Tue Sep 30 16:52:42 1997
+//---------------------------------------------------------------------------//
+// @> 
+//---------------------------------------------------------------------------//
+
+#include "t3/Test_Prob.hh"
+
+#include "nml/Group.hh"
+
+#include "c4/global.hh"
+#include "c4/SpinLock.hh"
+#include "c4/Baton.hh"
+
+using namespace C4;
+
+#include <math.h>
+
+//---------------------------------------------------------------------------//
+// Set up the supporting data structures.
+//---------------------------------------------------------------------------//
+
+Test_Prob::Test_Prob()
+{
+    NML_Group g( "t3" );
+
+    setup_namelist( g );
+
+    g.readgroup( "t3.in" );
+    g.writegroup( "params.out" );
+
+// Now that we have the user input, we should be able to resize our data
+// arrays, etc. 
+
+    nct = ncx * ncy * ncz;
+    ncp = nct / nodes + ( (nct % nodes) > node );
+
+    SPINLOCK( cout << "node " << node << " has " << ncp
+	      << " cells." << endl << flush );
+
+    nxy = ncx * ncy;
+
+    goff = 0;
+    {
+	Baton<int> s(goff);
+	goff = s;
+	s += ncp;
+    }
+
+    SPINLOCK( cout << "node " << node
+	      << " has global offset " << goff << endl );
+    C4::gsync();
+
+// Allocate the arrays.
+
+    xc = Mat1<double>( ncx );
+    yc = Mat1<double>( ncy );
+    zc = Mat1<double>( ncz );
+
+    xf = Mat1<double>( ncx+1 );
+    yf = Mat1<double>( ncy+1 );
+    zf = Mat1<double>( ncz+1 );
+
+// Initialize the deltas.
+
+    dx = (xmax - xmin) / ncx;
+    dy = (ymax - ymin) / ncy;
+    dz = (zmax - zmin) / ncz;
+
+// Initialize the cell center locations.
+
+    for( int i=0; i < ncx; i++ )
+	xc(i) = (i+.5)*dx;
+    for( int i=0; i < ncy; i++ )
+	yc(i) = (i+.5)*dy;
+    for( int i=0; i < ncz; i++ )
+	zc(i) = (i+.5)*dz;
+
+// Initialize the face locations.
+
+    for( int i=0; i < ncx+1; i++ )
+	xf(i) = i * dx;
+    for( int i=0; i < ncy+1; i++ )
+	yf(i) = i * dy;
+    for( int i=0; i < ncz+1; i++ )
+	zf(i) = i * dz;
+
+// Initialize the cell volumes we will be concerned with.
+
+    vc = Mat1<double>( ncp );
+    for( int i=0; i < ncp; i++ )
+	vc(i) = dx * dy * dz;
+}
+
+//---------------------------------------------------------------------------//
+// Perform the mock simulation.
+//---------------------------------------------------------------------------//
+
+void Test_Prob::run()
+{
+    C4::gsync();
+
+    if (node == 0)
+	cout << "Running the test problem, NOT!" << endl;
+
+// Now we need a mapping between local cell id, and global i,j,k.  This will
+// be needed in order to calculate the values of E and D.
+
+// Set up initial conditions of the problem.
+
+    Mat1<double> Eo(ncp), En(ncp);
+
+    for( int i=0; i < ncp; i++ ) {
+	double x = xc( I(i) );
+	double y = yc( J(i) );
+	double z = zc( K(i) );
+
+	Eo(i) = E(x,y,z,0.);
+    }
+
+    Mat1<double> E_analytic(ncp), rhs(ncp), r(ncp);
+
+    Mat2<double> A( ncp, nct );
+
+// main loop.
+
+    for( int ns=1; ns <= nsteps; ns++ ) {
+
+    // Calculate current time level.
+	double t = dt * ns;
+
+	if (node == 0)
+	    cout << "Begining timestep " << ns << " t=" << t << endl;
+
+    // Calculate analytic solution.
+	for( int i=0; i < ncp; i++ )
+	    E_analytic(i) = E( xc(I(i)), yc(J(i)), zc(K(i)), t );
+
+    // Calculate rhs(t).
+	for( int i=0; i < ncp; i++ ) {
+	    double x = xc( I(i) );
+	    double y = yc( J(i) );
+	    double z = zc( K(i) );
+	    
+	    rhs(i) = Exyz(x,y,z)*dEtdt()
+
+		+ D(x,y,z)*Ey(y)*Ez(z)*d2Exdxx(x)
+		+ Ey(y)*Ez(z)*dExdx(x)*Dy(y)*Dz(z)*dDxdx()
+
+		+ D(x,y,z)*Ex(x)*Ez(z)*d2Eydyy(y)
+		+ Ex(x)*Ez(z)*dEydy(y)*Dx(x)*Dz(z)*dDydy()
+
+		+ D(x,y,z)*Ex(x)*Ey(y)*d2Ezdzz(z)
+		+ Ex(x)*Ey(y)*dEzdz(z)*Dx(x)*Dy(y)*dDzdz();
+	}
+
+    // Calculate boundary values.
+
+    // Begin setting up to solve the problem.  First, set r.
+	for( int i=0; i < ncp; i++ )
+	    r(i) = rhs(i)*dt/vc(i) + Eo(i);
+
+    // Set up coefficient matrix.
+	A = 0.;
+
+	for( int i=0; i < ncp; i++ ) {
+	    A(i,goff+i) = 1.;
+	    
+	}
+
+    // Solve Ax = b
+	En = 0;
+
+    // Compute solution quality metrics
+	double s1=0.;
+	for( int i=0; i < ncp; i++ ) {
+	    double d = E_analytic[i] - En[i];
+	    s1 += d*d;
+	}
+	gsum(s1);
+	s1 = sqrt( s1 / nct );
+	double s2=0.;
+	for( int i=0; i < ncp; i++ )
+	    s2 += E_analytic[i] * E_analytic[i];
+	gsum(s2);
+	s2 = sqrt( s2 / nct );
+	if (s1 > 0.)
+	    s1 /= s2;
+	if (node == 0)
+	    cout << "Difference norm: " << s1 << endl;
+
+    // Prepare for next iteration
+	Eo = En;
+    }
+}
+
+//---------------------------------------------------------------------------//
+//                              end of Test_Prob.cc
+//---------------------------------------------------------------------------//
---
+++ src/t3/Test_Prob.hh
@@ -0,0 +1,91 @@
+//----------------------------------*-C++-*----------------------------------//
+// Test_Prob.hh
+// Geoffrey Furnish
+// Tue Sep 30 16:52:41 1997
+//---------------------------------------------------------------------------//
+// @> 
+//---------------------------------------------------------------------------//
+
+#ifndef __t3_Test_Prob_hh__
+#define __t3_Test_Prob_hh__
+
+#include "t3/Run_DB.hh"
+
+#include "c4/NodeInfo.hh"
+
+#include "ds++/Mat.hh"
+
+//===========================================================================//
+// class Test_Prob - 
+
+// 
+//===========================================================================//
+
+class Test_Prob : private Run_DB, private C4::NodeInfo {
+
+    int nct;			// # of total cells in problem.
+    int ncp;			// # of cells on this processor.
+
+    int goff;			// global offset of this processor's cells.
+    int nxy;
+
+    int I(int x) const { return (x % nxy) % ncx; }
+    int J(int x) const { return (x % nxy) / ncx; }
+    int K(int x) const { return  x / nxy; }
+
+    Mat1<double> xc, yc, zc;
+    Mat1<double> xf, yf, zf;
+    double dx, dy, dz;
+    Mat1<double> vc;
+
+  public:
+    Test_Prob();
+//     Test_Prob( const Test_Prob& );
+//     ~Test_Prob();
+//     Test_Prob& operator=( const Test_Prob& );
+
+    void run();
+
+    double Et( double t ) const { return et0 + et1*t; }
+    double Ex( double x ) const { return ex0 + ex1*x + ex2*x*x; }
+    double Ey( double y ) const { return ey0 + ey1*y + ey2*y*y; }
+    double Ez( double z ) const { return ez0 + ez1*z + ez2*z*z; }
+
+    double Exyz( double x, double y, double z ) const
+    {
+	return Ex(x)*Ey(y)*Ez(z);
+    }
+    double E( double x, double y, double z, double t ) const
+    {
+	return Et(t) * Ex(x) * Ey(y) * Ez(z);
+    }
+
+    double dEtdt() const { return et1; }
+    double dExdx( double x ) const { return ex1 + 2. * ex2 * x; }
+    double dEydy( double y ) const { return ey1 + 2. * ey2 * y; }
+    double dEzdz( double z ) const { return ez1 + 2. * ez2 * z; }
+
+    double d2Exdxx( double x ) const { return 2. * ex2; }
+    double d2Eydyy( double y ) const { return 2. * ey2; }
+    double d2Ezdzz( double z ) const { return 2. * ez2; }
+
+    double Dx( double x ) const { return dx0 + dx1*x; }
+    double Dy( double y ) const { return dy0 + dy1*y; }
+    double Dz( double z ) const { return dz0 + dz1*z; }
+
+    double D( double x, double y, double z ) const
+    {
+	return //(dx0 + dx1*x) * (dy0 + dy1*y) * (dz0 + dz1*z);
+	    Dx(x) * Dy(y) * Dz(z);
+    }
+
+    double dDxdx() const { return dx1; }
+    double dDydy() const { return dy1; }
+    double dDzdz() const { return dz1; }
+};
+
+#endif                          // __t3_Test_Prob_hh__
+
+//---------------------------------------------------------------------------//
+//                              end of t3/Test_Prob.hh
+//---------------------------------------------------------------------------//
