e96aaafeee9b3e2ce662461f3d3b803280185ba2
furnish <furnish@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1997-12-10 23:31:14 +0000

    Completely integrated the diffusion solver.  At this point, the first cut is done.

--- src/3T/Diffusion_XYZ.cc
+++ src/3T/Diffusion_XYZ.cc
@@ -8,6 +8,9 @@
 
 #include "3T/Diffusion_XYZ.hh"
 
+#include "util/dump.hh"
+#include "util/ADFile.hh"
+
 //---------------------------------------------------------------------------//
 // Initialize the diffusion solver.
 //---------------------------------------------------------------------------//
@@ -17,7 +20,11 @@ Diffusion_XYZ<MT>::Diffusion_XYZ( const SP<MT>& spm_, const pcg_DB& pcg_db )
     : MT::Coord_Mapper( spm_->get_Mesh_DB() ),
       spm(spm_),
       A( ncp, nct ),
-      pcg_ctrl( pcg_db, ncp )
+      pcg_ctrl( pcg_db, ncp ),
+
+      vc( spm->get_vc() ),
+
+      dx( spm->get_dx() ), dy( spm->get_dy() ), dz( spm->get_dz() )
 {
     spmv = new MatVec_3T< MT, Diffusion_XYZ<MT> >( spm, this );
     precond = new PreCond<double>();
@@ -29,26 +36,108 @@ Diffusion_XYZ<MT>::Diffusion_XYZ( const SP<MT>& spm_, const pcg_DB& pcg_db )
 
 template<class MT>
 void Diffusion_XYZ<MT>::solve( const typename MT::fcdsf& D,
-			       const typename  MT::cell_array& rhs,
-			       double dt, typename MT::cell_array& x )
+			       typename  MT::cell_array& r,
+			       double dt, typename MT::cell_array& x,
+			       const typename MT::fcdsf& Eb )
 {
+    const Mat1<double>& xA = spm->get_xA();
+    const Mat1<double>& yA = spm->get_yA();
+    const Mat1<double>& zA = spm->get_zA();
+
 // Clear the matrix.
 
     A = 0.;
 
 // Now initialize the matrix.
 
-    for( int i=0; i < ncp; i++ )
-	A(i,goff+i) = 1.;
+    for( int n=0; n < ncp; n++ ) {
+	int i = I(n), j = J(n), k = K(n);
+	double d, fac;
+
+	A(n,goff+n) = 1.;
+
+    // left face.
+
+    //	d = D( xf(i), yc(j), zc(k) );
+	d = D( n, 0 );
+	fac = (d * dt) / (vc(n) * dx);
+	A(n,goff+n) += (fac * xA(i));
+	if (i > 0)
+	    A(n, goffset(i-1,j,k)) -= fac * xA(i);
+	else
+// 	    r(n) += fac * xA(i) * E( xc(i) - dx, yc(j), zc(k), t );
+	    r(n) += fac * xA(i) * Eb( n, 0 );
+
+    // right face.
+
+// 	d = D( xf(i+1), yc(j), zc(k) );
+	d = D( n, 1 );
+	fac = (d * dt) / (vc(n) * dx);
+	A(n,goff+n) += (fac * xA(i+1));
+	if (i < ncx-1)
+	    A(n, goffset(i+1,j,k)) -= fac * xA(i+1);
+	else
+// 	    r(n) += fac * xA(i+1) * E( xc(i)+dx, yc(j), zc(k), t );
+	    r(n) += fac * xA(i+1) * Eb( n, 1 );
+
+    // front face.
+
+// 	d = D( xc(i), yf(j), zc(k) );
+	d = D( n, 2 );
+	fac = (d * dt) / (vc(n) * dy);
+	A(n,goff+n) += (fac * yA(j));
+	if (j > 0)
+	    A(n, goffset(i,j-1,k)) -= fac * yA(j);
+	else
+// 	    r(n) += fac * yA(j) * E( xc(i), yc(j) - dy, zc(k), t );
+	    r(n) += fac * yA(j) * Eb( n, 2 );
+
+    // back face.
+
+// 	d = D( xc(i), yf(j+1), zc(k) );
+	d = D( n, 3 );
+	fac = (d * dt) / (vc(n) * dy);
+	A(n,goff+n) += (fac * yA(j+1));
+	if (j < ncy-1)
+	    A(n, goffset(i,j+1,k)) -= fac * yA(j+1);
+	else
+// 	    r(n) += fac * yA(j+1) * E( xc(i), yc(j)+dy, zc(k), t );
+	    r(n) += fac * yA(j+1) * Eb( n, 3 );
+
+    // bottom face.
+
+// 	d = D( xc(i), yc(j), zf(k) );
+	d = D( n, 4 );
+	fac = (d * dt) / (vc(n) * dz);
+	A(n,goff+n) += (fac * zA(k));
+	if (k > 0)
+	    A(n, goffset(i,j,k-1)) -= fac * zA(k);
+	else
+// 	    r(n) += fac * zA(k) * E( xc(i), yc(j), zc(k) - dz, t );
+	    r(n) += fac * zA(k) * Eb( n, 4 );
+
+    // top face.
+
+// 	d = D( xc(i), yc(j), zf(k+1) );
+	d = D( n, 5 );
+	fac = (d * dt) / (vc(n) * dz);
+	A(n,goff+n) += (fac * zA(k+1));
+	if (k < ncz-1)
+	    A(n, goffset(i,j,k+1)) -= fac * zA(k+1);
+	else
+// 	    r(n) += fac * zA(k+1) * E( xc(i), yc(j), zc(k)+dz, t );
+	    r(n) += fac * zA(k+1) * Eb( n, 5 );
+
+    // Here comes a pig face.    (With appologies to Dr. Seus :-).
+    }
+
+    print2_Mat( A, "A" );
 
 // Now solve the matrix equation A.x = rhs.
 
-//    x = rhs;
-    pcg_ctrl.pcg_fe( x, rhs, spmv, precond );
+    pcg_ctrl.pcg_fe( x, r, spmv, precond );
 
     int pcgits = spmv->get_iterations();
-
-    cout << "Solved equation in " << pcgits << " iterations." << endl;
 }
 
 //---------------------------------------------------------------------------//
--- src/3T/Diffusion_XYZ.hh
+++ src/3T/Diffusion_XYZ.hh
@@ -35,15 +35,22 @@ class Diffusion_XYZ : private MT::Coord_Mapper {
     SP< MatVec_3T< MT, Diffusion_XYZ<MT> > > spmv;
     SP< PCG_PreCond<double> > precond;
 
+    typename MT::cell_array vc;
+
+    double dx, dy, dz;
+
   public:
     typedef double NumT;
 
     Diffusion_XYZ( const SP<MT>& spm_, const pcg_DB& pcg_db );
+
     void solve( const typename MT::fcdsf& D,
-		const typename MT::cell_array& rhs,
-		double dt, typename MT::cell_array& x );
+		typename MT::cell_array& r,
+		double dt, typename MT::cell_array& x,
+		const typename MT::fcdsf& Eb );
 
     Mat2<double>& get_A() { return A; }
+    SP< MatVec_3T< MT, Diffusion_XYZ<MT> > > get_matvec() { return spmv; }
 };
 
 #endif                          // __3T_Diffusion_XYZ_hh__
