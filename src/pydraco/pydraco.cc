7210cc3abfa1bad88f5dbfb095ee4b730e01ab14
furnish <furnish@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1998-01-09 23:42:11 +0000

    Starter files for a Python with Draco extensions.

---
+++ src/pydraco/pydraco.cc
@@ -0,0 +1,8 @@
+/* Minimal main program -- everything is loaded from the library */
+
+extern "C" int Py_Main( int argc, char *argv[] );
+
+int main( int argc, char *argv[] )
+{
+	return Py_Main(argc, argv);
+}
