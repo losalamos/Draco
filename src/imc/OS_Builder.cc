ae44ec2cecb81bca0edd3ac049f435f6716e8b3e
tme <tme@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1998-02-12 00:52:05 +0000

    parser completion in OS_Builder.

--- src/imc/OS_Builder.cc
+++ src/imc/OS_Builder.cc
@@ -3,12 +3,203 @@
 // Thomas M. Evans
 // Mon Feb  9 16:16:07 1998
 //---------------------------------------------------------------------------//
-// @> 
+// @> OS_Builder class implementation file
 //---------------------------------------------------------------------------//
 
 #include "OS_Builder.hh"
 
+IMCSPACE
 
+//---------------------------------------------------------------------------//
+// constructors
+//---------------------------------------------------------------------------//
+// defined inline
+
+//---------------------------------------------------------------------------//
+// build mesh public member function
+//---------------------------------------------------------------------------//
+SP<OS_Mesh> OS_Builder::buildMesh()
+{
+  // declare smart pointers
+    SP<Coord_sys> coord;
+    SP<Layout> layout;
+    SP<OS_Mesh> return_mesh;
+
+  // call parser
+    parser();
+
+  // build mesh-independent objects
+    coord       = buildCoord();
+    layout      = buildLayout(*coord);
+
+  // build mesh
+    int dim = coord->getDim();
+    if (dim == 2)
+	return_mesh = build2DMesh(coord, *layout);
+    else if (dim == 3)
+	return_mesh = build3DMesh(coord, *layout);
+    return return_mesh;
+}
+
+//---------------------------------------------------------------------------//
+// parser member functions
+//---------------------------------------------------------------------------//
+void OS_Builder::parser()
+{
+  // open input file
+    const char *file = input_file.c_str();
+    ifstream input(file);
+    
+  // determine coord_sys
+    string keyword;
+    input >> keyword >> coord_system;
+
+  // call appropriate sub-parser
+    if (coord_system == "xy" || coord_system == "XY" || coord_system == "rz"
+	|| coord_system == "RZ")
+    {
+	fine_cells.resize(2);
+	coarse_edge.resize(2);
+	fine_edge.resize(2);
+	bnd_cond.resize(4);
+	parser2D(input);
+    }
+    else if (coord_system == "xyz" || coord_system == "XYZ")
+    {
+	fine_cells.resize(3);
+	coarse_edge.resize(3);
+	fine_edge.resize(3);
+	bnd_cond.resize(6);
+	parser3D(input);
+    }
+}
+
+void OS_Builder::parser2D(ifstream &in)
+{
+  // 2D parser
+
+    string keyword;
+    int data;
+    
+  // initialization block input
+    while (keyword != "end-init")
+    {
+	in >> keyword;
+	if (keyword == "xcoarse:" || keyword == "rcoarse:")
+	{
+	    in >> data;
+	    fine_cells[0].resize(data);
+	    coarse_edge[0].resize(data+1);  
+	}
+	if (keyword == "ycoarse:" || keyword == "zcoarse:")
+	{
+	    in >> data;
+	    fine_cells[1].resize(data);
+	    coarse_edge[1].resize(data+1);
+	}
+	if (keyword == "xfine:" || keyword == "rfine:")
+	{
+	    in >> data;
+	    fine_edge[0].resize(data+1);
+	}
+	if (keyword == "yfine:" || keyword == "zfine:")
+	{
+	    in >> data;
+	    fine_edge[1].resize(data+1);
+	}
+    }
+
+  // mesh block input
+    while (keyword != "end-mesh")
+    {
+	in >> keyword;
+	if (keyword == "xdim:" || keyword == "rdim:")
+	    for (int i = 0; i < coarse_edge[0].size(); i++)
+		in >> coarse_edge[0][i];
+	if (keyword == "xints:" || keyword == "rints:")
+	    for (int i = 0; i < fine_cells[0].size(); i++)
+		in >> fine_cells[0][i];
+	if (keyword == "ydim:" || keyword == "zdim:")
+	    for (int i = 0; i < coarse_edge[1].size(); i++)
+		in >> coarse_edge[1][i];
+	if (keyword == "yints:" || keyword == "zints:")
+	    for (int i = 0; i < fine_cells[1].size(); i++)
+		in >> fine_cells[1][i];
+    }
+}    
+
+void OS_Builder::parser3D(ifstream &in)
+{
+  // 3D parser
+
+    string keyword;
+    int data;
+    
+  // initialization block input
+    while (keyword != "end-init")
+    {
+	in >> keyword;
+	if (keyword == "xcoarse:")
+	{
+	    in >> data;
+	    fine_cells[0].resize(data);
+	    coarse_edge[0].resize(data+1);  
+	}
+	if (keyword == "ycoarse:")
+	{
+	    in >> data;
+	    fine_cells[1].resize(data);
+	    coarse_edge[1].resize(data+1);
+	}
+	if (keyword == "zcoarse:")
+	{
+	    in >> data;
+	    fine_cells[2].resize(data);
+	    coarse_edge[2].resize(data+1);
+	}
+	if (keyword == "xfine:")
+	{
+	    in >> data;
+	    fine_edge[0].resize(data+1);
+	}
+	if (keyword == "yfine:")
+	{
+	    in >> data;
+	    fine_edge[1].resize(data+1);
+	}
+	if (keyword == "zfine:")
+	{
+	    in >> data;
+	    fine_edge[2].resize(data+1);
+	}
+    }
+
+  // mesh block input
+    while (keyword != "end-mesh")
+    {
+	in >> keyword;
+	if (keyword == "xdim:")
+	    for (int i = 0; i < coarse_edge[0].size(); i++)
+		in >> coarse_edge[0][i];
+	if (keyword == "xints:")
+	    for (int i = 0; i < fine_cells[0].size(); i++)
+		in >> fine_cells[0][i];
+	if (keyword == "ydim:")
+	    for (int i = 0; i < coarse_edge[1].size(); i++)
+		in >> coarse_edge[1][i];
+	if (keyword == "yints:")
+	    for (int i = 0; i < fine_cells[1].size(); i++)
+		in >> fine_cells[1][i];
+	if (keyword == "zdim:")
+	    for (int i = 0; i < coarse_edge[2].size(); i++)
+		in >> coarse_edge[2][i];
+	if (keyword == "zints:")
+	    for (int i = 0; i < fine_cells[2].size(); i++)
+		in >> fine_cells[2][i];
+    }
+}  
+
+CSPACE
 
 //---------------------------------------------------------------------------//
 //                              end of OS_Builder.cc
--- src/imc/OS_Builder.hh
+++ src/imc/OS_Builder.hh
@@ -3,7 +3,7 @@
 // Thomas M. Evans
 // Mon Feb  9 16:16:07 1998
 //---------------------------------------------------------------------------//
-// @> 
+// @> OS_Builder class header file
 //---------------------------------------------------------------------------//
 
 #ifndef __imctest_OS_Builder_hh__
@@ -23,11 +23,20 @@
 //===========================================================================//
 
 #include "Names.hh"
+#include "Coord_sys.hh"
+#include "Layout.hh"
+#include "OS_Mesh.hh"
 #include <vector>
 #include <string>
+#include <iostream>
+#include <fstream>
 #include "SP.hh"
 
 IMCSPACE
+
+using std::vector;
+using std::string;
+using std::ifstream;
 
 class OS_Builder
 {
@@ -35,16 +44,20 @@ private:
   // data
     string input_file;
     string coord_system;
-    vector<int> zones;
-    vector<int> zone_cells;
-    vector<int> cells;
-    vector<double> zone_faces;
-    vector<string> bnd_cond
+  // number of fine cells per coarse cell
+    vector< vector<int> > fine_cells;
+  // coarse edges
+    vector< vector<double> > coarse_edge;
+  // fine cell edges
+    vector< vector<double> > fine_edge;
+  // boundary conditions
+    vector<string> bnd_cond;
 
   // Parser member functions
-    void parser();
+    void parser2D(ifstream &);
+    void parser3D(ifstream &);
   // build Layout helper functions
-    SP<Layout> buildLayout(const Coord_sys &coord)
+    SP<Layout> buildLayout(const Coord_sys &coord);
     void Assign2D(Layout &);
     void Assign3D(Layout &);
   // build Coord_sys helper functions
@@ -52,10 +65,16 @@ private:
     SP<Coord_sys> buildXY();
     SP<Coord_sys> buildXYZ();
   // build Mesh helper functions
-    SP<OS_Mesh> build2dMesh(SP<Coord_sys>, const Layout &);
+    SP<OS_Mesh> build2DMesh(SP<Coord_sys>, const Layout &);
     SP<OS_Mesh> build3DMesh(SP<Coord_sys>, const Layout &);
 public:
-    SP<OS_Mesh> buildMesh();
+    explicit OS_Builder(const string &infile) 
+	: input_file(infile), coord_system(""), fine_cells(0), 
+	  coarse_edge(0), fine_edge(0), bnd_cond(0)
+    {}
+    SP<OS_Mesh> buildMesh();    
+    void parser();
+};
 
 CSPACE
 
