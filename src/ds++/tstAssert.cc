81f395b8af8af98c4f478fe286354390eab07180
furnish <furnish@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1998-02-12 22:45:48 +0000

    Convert to work in the canonical way for Draco test articles.

--- src/ds++/tstAssert.cc
+++ src/ds++/tstAssert.cc
@@ -12,40 +12,147 @@ using namespace dsxx;
 #include <iostream.h>
 
 //---------------------------------------------------------------------------//
-// Basic test of the assert macro.
+// The way this test article works is that each of the DBC macros are tested
+// in a seperate function.  A falst condition is asserted using each macro,
+// and after this follows a throw.  Two catch clauses are available, one to
+// catch an assertion object, and one to catch anything else.  By comparing
+// the exception that is actually caught with the one that should be caught
+// given the DBC setting in force, we can determine whether each test passes
+// or fails.
+//---------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------//
+// Check the operation of the Require() macro.
 //---------------------------------------------------------------------------//
 
 void t1()
 {
     cout << "t1: ";
     try {
-	Assert(0);
-	cout << "fell through \"Assert(0)\"." << endl;
+	Require(0);
+	throw "Bogus!";
     }
-    catch( assertion& a ) {
-	cout << "a.what() = " << a.what() << endl;
+    catch( assertion& a )
+    {
+#if DBC & 1
+	cout << "passed\n";
+#else
+	cout << "failed\n";
+#endif
     }
-    catch(...) {
-	cout << "Big trouble in t1()!" << endl;
+    catch(...)
+    {
+#if DBC & 1
+	cout << "failed\n";
+#else
+	cout << "passed\n";
+#endif
     }
 }
 
 //---------------------------------------------------------------------------//
-// Basic test of the Insist macro.
+// Check the operation of the Check() macro.
 //---------------------------------------------------------------------------//
 
 void t2()
 {
     cout << "t2: ";
     try {
+	Check(0);
+	throw "Bogus!";
+    }
+    catch( assertion& a )
+    {
+#if DBC & 2
+	cout << "passed\n";
+#else
+	cout << "failed\n";
+#endif
+    }
+    catch(...)
+    {
+#if DBC & 2
+	cout << "failed\n";
+#else
+	cout << "passed\n";
+#endif
+    }
+}
+
+//---------------------------------------------------------------------------//
+// Check the operation of the Ensure() macro.
+//---------------------------------------------------------------------------//
+
+void t3()
+{
+    cout << "t3: ";
+    try {
+	Ensure(0);
+	throw "Bogus!";
+    }
+    catch( assertion& a )
+    {
+#if DBC & 4
+	cout << "passed\n";
+#else
+	cout << "failed\n";
+#endif
+    }
+    catch(...)
+    {
+#if DBC & 4
+	cout << "failed\n";
+#else
+	cout << "passed\n";
+#endif
+    }
+}
+
+//---------------------------------------------------------------------------//
+// Check the operation of the Assert() macro, which works like Check().
+//---------------------------------------------------------------------------//
+
+void t4()
+{
+    cout << "t4: ";
+    try {
+	Assert(0);
+	throw "Bogus!";
+    }
+    catch( assertion& a )
+    {
+#if DBC & 2
+	cout << "passed\n";
+#else
+	cout << "failed\n";
+#endif
+    }
+    catch(...)
+    {
+#if DBC & 2
+	cout << "failed\n";
+#else
+	cout << "passed\n";
+#endif
+    }
+}
+
+//---------------------------------------------------------------------------//
+// Basic test of the Insist() macro.
+//---------------------------------------------------------------------------//
+
+void t5()
+{
+    cout << "t5: ";
+    try {
 	Insist( 0, "You must be kidding!" );
-	cout << "fell  through \"Insist(0,...)\"." << endl;
+	throw "Bogus!";
     }
     catch( assertion& a ) {
-	cout << "a.what() = " << a.what() << endl;
+	cout << "passed\n";
     }
     catch(...) {
-	cout << "Big trouble in t2()!" << endl;
+	cout << "failed\n";
     }
 }
 
@@ -53,6 +160,9 @@ int main( int argc, char *argv[] )
 {
     t1();
     t2();
+    t3();
+    t4();
+    t5();
 }
 
 //---------------------------------------------------------------------------//
