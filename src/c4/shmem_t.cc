221522c67a0080e3b0413136c559c1d0f496b31e
furnish <furnish@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1998-02-18 23:49:44 +0000

    Template b/a s/r functions.

---
+++ src/c4/shmem_t.cc
@@ -0,0 +1,110 @@
+//----------------------------------*-C++-*----------------------------------//
+// shmem_t.cc
+// Geoffrey M. Furnish
+// Wed Feb 18 15:55:22 1998
+//---------------------------------------------------------------------------//
+// @> Template functions for the C4 MPI interface.
+//---------------------------------------------------------------------------//
+
+//#include "mpi_traits.hh"
+#include "c4_traits.hh"
+
+C4_NAMESPACE_BEG
+
+//---------------------------------------------------------------------------//
+// MPI send/receive calls (basic set)
+//
+// Synchronous:
+//	MPI_Send(void* buf, int count, MPI_Datatype datatype,
+//		 int dest, int tag, MPI_Comm comm);
+//	MPI_Recv(void* buf, int count, MPI_Datatype datatype,
+//		 int source, int tag, MPI_Comm comm, MPI_Status *status);
+//
+// Asynchronous:
+//	MPI_Isend(void* buf, int count, MPI_Datatype datatype,
+//		  int dest, int tag, MPI_Comm comm, MPI_Request *request);
+//	MPI_Irecv(void* buf, int count, MPI_Datatype datatype,
+//		  int source, int tag, MPI_Comm comm, MPI_Request *request);
+//---------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------//
+// Basic send and receive functions.
+//---------------------------------------------------------------------------//
+
+template<class T>
+int Send( const T *buf, int nels, int dest,
+	  int tag /*=c4_traits<T*>::Tag*/, int group /*=0*/ )
+{
+//     MPI_Send( (void *) buf, nels, mpi_traits<T>::element_type,
+// 	      dest, tag, MPI_COMM_WORLD );
+    return C4_SUCCESS;
+}
+
+template<class T>
+int Recv( T *buf, int nels, int source,
+	  int tag /*=c4_traits<T*>::Tag*/, int group /*=0*/ )
+{
+//     int cnt;
+//     MPI_Status status;
+//     MPI_Recv( buf, nels, mpi_traits<T>::element_type,
+// 	      source, tag, MPI_COMM_WORLD, &status );
+//     MPI_Get_count( &status, mpi_traits<T>::element_type, &cnt );
+//     return cnt;
+    return 1;
+}
+
+//---------------------------------------------------------------------------//
+// Asynchronous send and recive functions, returning a request handle.
+//---------------------------------------------------------------------------//
+
+template<class T>
+C4_Req SendAsync( const T *buf, int nels, int dest,
+		  int tag /*=c4_traits<T*>::Tag*/, int group /*=0*/ )
+{
+    C4_Req r;
+//     MPI_Isend( (void *) buf, nels, mpi_traits<T>::element_type,
+// 	       dest, tag, MPI_COMM_WORLD, &r.r );
+//     r.set();
+    return r;
+}
+
+template<class T>
+C4_Req RecvAsync( T *buf, int nels, int source,
+		  int tag /*=c4_traits<T*>::Tag*/, int group /*=0*/ )
+{
+    C4_Req r;
+//     MPI_Irecv( (void *) buf, nels, mpi_traits<T>::element_type,
+// 	       source, tag, MPI_COMM_WORLD, &r.r );
+//     r.set();
+    return r;
+}
+
+//---------------------------------------------------------------------------//
+// Asynchronous send and recive functions, using a provided request handle.
+//---------------------------------------------------------------------------//
+
+template<class T>
+void SendAsync( C4_Req& r, const T *buf, int nels, int dest,
+		int tag /*=c4_traits<T*>::Tag*/, int group /*=0*/ )
+{
+    Require( !r.inuse() );
+//     r.set();
+//     MPI_Isend( (void *) buf, nels, mpi_traits<T>::element_type>,
+// 	       dest, tag, MPI_COMM_WORLD, &r.r );
+}
+
+template<class T>
+void RecvAsync( C4_Req& r, T *buf, int nels, int source,
+		int tag /*=c4_traits<T*>::Tag*/, int group /*=0*/ )
+{
+    Require( !r.inuse() );
+//     r.set();
+//     MPI_Irecv( (void *) buf, nels, mpi_traits<T>::element_type>,
+// 	       source, tag, MPI_COMM_WORLD, &r.r );
+}
+
+C4_NAMESPACE_END
+
+//---------------------------------------------------------------------------//
+//                              end of shmem_t.cc
+//---------------------------------------------------------------------------//
