1a775da7b60072e566caeee24caaa7ef85f9c62c
tme <tme@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1998-03-13 23:23:37 +0000

    Update to testb.cc for checking particle tracking and geometry builds.

--- src/imc/test/testb.cc
+++ src/imc/test/testb.cc
@@ -3,65 +3,118 @@
 // Thomas M. Evans
 // Wed Feb 18 10:45:23 1998
 //---------------------------------------------------------------------------//
-// @> test driver for OS_Builder
+// @> test driver for Builder classes
 //---------------------------------------------------------------------------//
 
-#include "imctest/OS_Parser.hh"
+#include "imctest/OS_Interface.hh"
 #include "imctest/OS_Builder.hh"
 #include "imctest/OS_Mesh.hh"
 #include "imctest/Mat_State.hh"
 #include "imctest/Opacity_Builder.hh"
 #include "imctest/Opacity.hh"
+#include "imctest/Particle.hh"
 #include "ds++/SP.hh"
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <cassert>
+
+using IMC::OS_Interface;
+using IMC::OS_Builder;
+using IMC::OS_Mesh;
+using IMC::Mat_State;
+using IMC::Opacity_Builder;
+using IMC::Opacity;
+using IMC::Particle;
+using namespace std;
+
+void Builder_diagnostic(const OS_Mesh &mesh, const Mat_State<OS_Mesh> &mat,
+			const Opacity<OS_Mesh> &opacity)
+{
+  // do some diagnostic checks
+
+    ofstream output("try.dat");
+
+    output << "Coordinate System: " << mesh.Coord().Get_coord() << endl;
+    output << "Mesh Size: " << mesh.Num_cells() << endl;
+    output << endl;
+
+    for (int cell = 1; cell <= mesh.Num_cells(); cell++)
+	mesh.Print(output, cell);
+    output << endl;
+
+    output << mat;
+    output << endl;
+
+    output << opacity;
+    output << endl;
+
+    output << "Mesh:      " << mesh.Num_cells() << endl;
+    output << "Opacity:   " << opacity.Num_cells() << endl;
+    output << "Mat_State: " << mat.Num_cells() << endl;
+}
 
 main()
 {
-    using IMC::OS_Parser;
-    using IMC::OS_Builder;
-    using IMC::OS_Mesh;
-    using IMC::Mat_State;
-    using IMC::Opacity_Builder;
-    using IMC::Opacity;
-    using namespace std;
-
+  // declare geometry and material stuff
     SP<OS_Mesh> mesh;
     SP< Mat_State<OS_Mesh> > mat_state;
     SP< Opacity<OS_Mesh> > opacity;
 
-  // scoping blocks
+  // scoping blocks for build-stuff
     {
 	string infile;
 	cout << "Name the input file" << endl;
 	cin >> infile;
 
-      // run the Parser
-	SP<OS_Parser> parser = new OS_Parser(infile);
-	parser->Parser();
+      // run the interface parser
+	SP<OS_Interface> interface = new OS_Interface(infile);
+	interface->Parser();
 
       // initialize the mesh builder and build mesh
-	OS_Builder build(parser);
-	mesh = build.Build_Mesh();
+	OS_Builder os_build(interface);
+	mesh = os_build.Build_Mesh();
 
       // initialize the Opacity builder and build state 
-	Opacity_Builder<OS_Mesh> opacity_build(parser, mesh);
+	Opacity_Builder<OS_Mesh> opacity_build(interface, mesh);
 	mat_state = opacity_build.Build_Mat();
 	opacity   = opacity_build.Build_Opacity();
     }
 
-    cout << "Coordinate System: " << mesh->Coord().Get_coord() << endl;
-    cout << "Mesh Size: " << mesh->Num_cells() << endl;
-    for (int cell = 1; cell <= mesh->Num_cells(); cell++)
-	mesh->Print(cell);
+    Builder_diagnostic(*mesh, *mat_state, *opacity);
+
+  // transport a particle
+
+  // initialize particle
+    int seed = -18374;
+    Particle<OS_Mesh> particle(*mesh, seed, 1.0, 10.0);
+    assert (particle.Status());
+
+  // origin and source
+    
+    vector<double> origin(mesh->Coord().Get_dim());   
+    vector<double> source(mesh->Coord().Get_sdim());
+
+    for (int i = 0; i < origin.size(); i++)
+    {
+	cout << "Origin " << i+1 << ": ";
+	cin >> origin[i];
+    }
+    for (int i = 0; i < source.size(); i++)
+    {
+	cout << "Omega " << i+1 << ": ";
+	cin >> source[i];
+    }
     cout << endl;
+    
+  // source
+    particle.Source(origin, source, *mesh);
 
-    cout << *mat_state;
-    cout << *opacity;
+  // transport
+    particle.Transport(*mesh, *opacity);
 
-    cout << "Mesh:      " << mesh->Num_cells() << endl;
-    cout << "Opacity:   " << opacity->Num_cells() << endl;
-    cout << "Mat_State: " << mat_state->Num_cells() << endl;
+  // assert that particle is indeed dead
+    assert (!particle.Status());
 }
 
 //---------------------------------------------------------------------------//
