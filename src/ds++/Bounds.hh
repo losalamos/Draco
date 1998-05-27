cd8b6805b13adac6ebd956d80a37de1c0d0aa1c1
furnish <furnish@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1998-05-27 20:47:13 +0000

    Namespacify.  Don't know how this one got left out before.

--- src/ds++/Bounds.hh
+++ src/ds++/Bounds.hh
@@ -3,11 +3,24 @@
 // Geoffrey Furnish
 // Mon Jan 27 12:11:50 1997
 //---------------------------------------------------------------------------//
-// @> 
+// @> Class to represent index ranges for DS++ containers.
 //---------------------------------------------------------------------------//
 
 #ifndef __ds_Bounds_hh__
 #define __ds_Bounds_hh__
+
+#include "config.hh"
+
+NAMESPACE_DS_BEG
+
+//===========================================================================//
+// class Bounds - Index range specifier class
+
+// This class holds the inclusive bounds specifiers used to construct DS++
+// containers when something other than zero based indexing is desired.  For
+// example, Bounds(-1,3) represents an index range running from -1 through 3, 
+// or a total of 5 valid indexes.  
+//===========================================================================//
 
 class Bounds {
     int a, b;
@@ -17,6 +30,8 @@ class Bounds {
     int max() const { return b; }
     int len() const { return b - a + 1; }
 };
+
+NAMESPACE_DS_END
 
 #endif                          // __ds_Bounds_hh__
 
