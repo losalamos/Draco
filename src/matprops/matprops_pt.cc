77db17d09253ae5d4698d31d810ed7fdfaf4503c
rsqrd <rsqrd@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1998-05-29 17:11:01 +0000

    Added template instantiations needed specifically for this library.

---
+++ src/matprops/matprops_pt.cc
@@ -0,0 +1,33 @@
+//----------------------------------*-C++-*----------------------------------//
+// matprops_pt.cc
+// Randy M. Roberts
+// Thu May 28 14:56:12 1998
+//---------------------------------------------------------------------------//
+// @> Template instantiations for the matprops library.
+//---------------------------------------------------------------------------//
+
+#include "radphys/RadiationPhysics.cc"
+
+typedef double T1;
+
+template void XTM::RadiationPhysics::getElectIonCoupling<T1>(const T1 &,
+							     const T1 &,
+							     const T1 &,
+							     double,
+							     T1 &) const;
+
+template void XTM::RadiationPhysics::getElectronConductionCoeff<T1>(const T1 &,
+								    const T1 &,
+								    const T1 &,
+								    double,
+								    T1 &) const;
+
+template void XTM::RadiationPhysics::getIonConductionCoeff<T1>(const T1 &,
+							       const T1 &,
+							       const T1 &,
+							       double,
+							       T1 &) const;
+
+//---------------------------------------------------------------------------//
+//                              end of matprops_pt.cc
+//---------------------------------------------------------------------------//
