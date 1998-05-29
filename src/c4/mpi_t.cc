d2311cba30e97e55ab4907d204cd4e6d37dbc3ae
furnish <furnish@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1998-05-29 13:44:12 +0000

    Upgrade mpi_traits<T>::element_type to mpi_traits<T>::element_type(), and upgrade an old style cast to a new style cast.

--- src/c4/mpi_t.cc
+++ src/c4/mpi_t.cc
@@ -35,7 +35,7 @@ template<class T>
 int Send( const T *buf, int nels, int dest,
 	  int tag /*=c4_traits<T*>::Tag*/, int group /*=0*/ )
 {
-    MPI_Send( (void *) buf, nels, mpi_traits<T>::element_type,
+    MPI_Send( const_cast<T *>(buf), nels, mpi_traits<T>::element_type(),
 	      dest, tag, MPI_COMM_WORLD );
     return C4_SUCCESS;
 }
@@ -46,9 +46,9 @@ int Recv( T *buf, int nels, int source,
 {
     int cnt;
     MPI_Status status;
-    MPI_Recv( buf, nels, mpi_traits<T>::element_type,
+    MPI_Recv( buf, nels, mpi_traits<T>::element_type(),
 	      source, tag, MPI_COMM_WORLD, &status );
-    MPI_Get_count( &status, mpi_traits<T>::element_type, &cnt );
+    MPI_Get_count( &status, mpi_traits<T>::element_type(), &cnt );
     return cnt;
 }
 
@@ -61,7 +61,7 @@ C4_Req SendAsync( const T *buf, int nels, int dest,
 		  int tag /*=c4_traits<T*>::Tag*/, int group /*=0*/ )
 {
     C4_Req r;
-    MPI_Isend( (void *) buf, nels, mpi_traits<T>::element_type,
+    MPI_Isend( (void *) buf, nels, mpi_traits<T>::element_type(),
 	       dest, tag, MPI_COMM_WORLD, &r.r );
     r.set();
     return r;
@@ -72,7 +72,7 @@ C4_Req RecvAsync( T *buf, int nels, int source,
 		  int tag /*=c4_traits<T*>::Tag*/, int group /*=0*/ )
 {
     C4_Req r;
-    MPI_Irecv( (void *) buf, nels, mpi_traits<T>::element_type,
+    MPI_Irecv( (void *) buf, nels, mpi_traits<T>::element_type(),
 	       source, tag, MPI_COMM_WORLD, &r.r );
     r.set();
     return r;
@@ -88,7 +88,7 @@ void SendAsync( C4_Req& r, const T *buf, int nels, int dest,
 {
     Require( !r.inuse() );
     r.set();
-    MPI_Isend( (void *) buf, nels, mpi_traits<T>::element_type,
+    MPI_Isend( (void *) buf, nels, mpi_traits<T>::element_type(),
 	       dest, tag, MPI_COMM_WORLD, &r.r );
 }
 
@@ -98,7 +98,7 @@ void RecvAsync( C4_Req& r, T *buf, int nels, int source,
 {
     Require( !r.inuse() );
     r.set();
-    MPI_Irecv( (void *) buf, nels, mpi_traits<T>::element_type,
+    MPI_Irecv( (void *) buf, nels, mpi_traits<T>::element_type(),
 	       source, tag, MPI_COMM_WORLD, &r.r );
 }
 
@@ -106,7 +106,7 @@ template<class T>
 void gsum( T& x )
 {
     T y = x;
-    MPI_Allreduce( &y, &x, 1, mpi_traits<T>::element_type,
+    MPI_Allreduce( &y, &x, 1, mpi_traits<T>::element_type(),
 		   MPI_SUM, MPI_COMM_WORLD );
 }
 
@@ -114,7 +114,7 @@ template<class T>
 void gmin( T& x )
 {
     T y = x;
-    MPI_Allreduce( &y, &x, 1, mpi_traits<T>::element_type,
+    MPI_Allreduce( &y, &x, 1, mpi_traits<T>::element_type(),
 		   MPI_MIN, MPI_COMM_WORLD );
 }
 
@@ -122,7 +122,7 @@ template<class T>
 void gmax( T& x )
 {
     T y = x;
-    MPI_Allreduce( &y, &x, 1, mpi_traits<T>::element_type,
+    MPI_Allreduce( &y, &x, 1, mpi_traits<T>::element_type(),
 		   MPI_MAX, MPI_COMM_WORLD );
 }
 
