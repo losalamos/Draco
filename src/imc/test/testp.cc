5e60cdead946bd0520cef895bc30e573b34446c4
tme <tme@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1998-06-03 02:05:21 +0000

    Tests of parallelism go on unabatted.

--- src/imc/test/testp.cc
+++ src/imc/test/testp.cc
@@ -3,7 +3,7 @@
 // Thomas M. Evans
 // Mon Apr 13 17:31:21 1998
 //---------------------------------------------------------------------------//
-// @> test executable to try out parallelism in IMCTEST.
+// @> test executable to try out parallelism in IMCTEST
 //---------------------------------------------------------------------------//
 
 #include "imctest/OS_Interface.hh"
@@ -14,6 +14,8 @@
 #include "imctest/Opacity.hh"
 #include "imctest/Parallel_Builder.hh"
 #include "imctest/Source_Init.hh"
+#include "imctest/Particle_Buffer.hh"
+#include "imctest/Particle.hh"
 #include "rng/Rnd_Control.hh"
 #include "ds++/SP.hh"
 #include "ds++/Assert.hh"
@@ -33,6 +35,8 @@ using IMC::Opacity_Builder;
 using IMC::Opacity;
 using IMC::Parallel_Builder;
 using IMC::Source_Init;
+using IMC::Particle_Buffer;
+using IMC::Particle;
 using RNG::Rnd_Control;
 using namespace std;
 using namespace C4;
@@ -135,57 +139,57 @@ int main(int argc, char *argv[])
 	  // run the interface parser
 	    SP<OS_Interface> interface = new OS_Interface(infile);
 	    interface->parser();
-            cout << "Read input file on host " << mynode << endl;
+            cout << ">> Read input file on host " << mynode << endl;
 
 	  // initialize the mesh builder and build mesh
 	    OS_Builder os_build(interface);
 	    mesh = os_build.build_Mesh();
-            cout << "Built mesh on host " << mynode << endl;
+            cout << ">> Built mesh on host " << mynode << endl;
 
 	  // initialize the Opacity builder and build state 
 	    Opacity_Builder<OS_Mesh> opacity_build(interface, mesh);
 	    mat_state = opacity_build.build_Mat();
 	    opacity   = opacity_build.build_Opacity();
-            cout << "Built opacities on host " << mynode << endl;
+            cout << ">> Built opacities on host " << mynode << endl;
 
 	  // do the source initialization
 	    sinit = new Source_Init<OS_Mesh>(interface, mesh);
 	    sinit->initialize(mesh, opacity, mat_state, rcon, 1);
-            cout << "Initialized source on host " << mynode << endl;
+            cout << ">> Initialized source on host " << mynode << endl;
             cout << endl;
 	}
 
       // make parallel builder object to do send/receives of objects
-	SP<Parallel_Builder<OS_Mesh> > pcomm;
+	SP< Parallel_Builder<OS_Mesh> > pcomm;
+	SP< Particle_Buffer<Particle<OS_Mesh> > > buffer;
     
 	if (!mynode)
 	{
 	  // make parallel builder object to do my mesh decomposition
-	  // 	    pcomm = new Parallel_Builder<OS_Mesh>(*mesh, *sinit);
- 	    pcomm = new Parallel_Builder<OS_Mesh>();
-	    pcomm->parallel_params(*mesh, *sinit);
-	    Check (pcomm);
-	    Check (mesh);
-	    Check (opacity);
+ 	    pcomm = new Parallel_Builder<OS_Mesh>(*mesh, *sinit);
+
+          // make Particle buffer
+            buffer  = new Particle_Buffer<Particle<OS_Mesh> >(*mesh, *rcon);
+
+          // send out objects
             pcomm->send_Mesh(*mesh);
 	    pcomm->send_Opacity(*opacity);
 	    Builder_diagnostic(*mesh, *mat_state, *opacity);
-	    
-	  // print out salient quantities from Parallel_Builder
-//          for (int i = 0; i < nodes(); i++)
-//              cout << setw(10) << i << setw(10) 
-//                   << pcomm->cells_per_proc[i].size() << endl;
 	}
 	
   	if (mynode)
   	{	
   	  // make parallel builder object to receive objects
   	    pcomm = new Parallel_Builder<OS_Mesh>();
-	    Check (pcomm);
+
+          // get mesh and opacity
   	    mesh    = pcomm->recv_Mesh();
   	    opacity = pcomm->recv_Opacity(mesh);
-	    Check (mesh);
-	    Check (opacity);
+
+          // make a particle buffer on this node
+            buffer  = new Particle_Buffer<Particle<OS_Mesh> >(*mesh, *rcon);
+
+          // get the source for this node
   	}
      
   	if (mesh) 
@@ -197,11 +201,11 @@ int main(int argc, char *argv[])
 	cout << "Dumbass, you screwed up: " << ass.what() << endl;
 	return 1;
     }
-//     catch(...)
-//     {
-// 	cout << "HELP ME" << endl;
-// 	return 1;
-//     }
+    catch(...)
+    {
+	cout << "HELP ME" << endl;
+	return 1;
+    }
 
   // c4 end
     C4::Finalize();
