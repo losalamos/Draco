a6d76c972350757a3039fe31641fa406d2852f3e
tme <tme@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1998-06-26 20:34:39 +0000

    Test functions for Sprng.

---
+++ src/rng/Sprng.cc
@@ -0,0 +1,38 @@
+//----------------------------------*-C++-*----------------------------------//
+// Sprng.cc
+// Thomas M. Evans
+// Fri Jun 26 07:41:48 1998
+//---------------------------------------------------------------------------//
+// @> Sprng random number class implementation file
+//---------------------------------------------------------------------------//
+
+#include "rng/Sprng.hh"
+#include <cmath>
+#include <iostream>
+
+RNGSPACE
+
+// stl components
+using std::fabs;
+
+//---------------------------------------------------------------------------//
+// diagnostic tests
+//---------------------------------------------------------------------------//
+// calculate the average for n deviates, should succeed
+
+bool Sprng::avg_test(int n, double eps)
+{
+    double avg = 0.0;
+    for (int i = 1; i <= n; i++)
+	avg += ran();
+    double result = avg / n - 0.5;
+    if (fabs(result) >= eps)
+	return false;
+    return true;
+}
+
+CSPACE
+
+//---------------------------------------------------------------------------//
+//                              end of Sprng.cc
+//---------------------------------------------------------------------------//
