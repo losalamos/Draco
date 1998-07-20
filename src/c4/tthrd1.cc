12d1883475ac4b463c3c616669f3c5c26066361a
furnish <furnish@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1998-07-20 15:59:34 +0000

    Test program for demonstrating some of C4's burgeoning support for threaded programming.

---
+++ src/c4/tthrd1.cc
@@ -0,0 +1,62 @@
+//----------------------------------*-C++-*----------------------------------//
+// tthrd1.cc
+// Geoffrey Furnish
+// Mon Jul 13 22:48:24 1998
+//---------------------------------------------------------------------------//
+// @> 
+//---------------------------------------------------------------------------//
+
+#include "c4/ThreadGroup.cc"
+#include "c4/ThreadGroupMember.hh"
+
+#include <iostream>
+
+void z( int& x )
+{
+    x++;
+}
+
+class X : public C4::ThreadGroupMember
+{
+  public:
+    void c4_run_thread()
+    {
+        int x = 1000000 * (rand() & 7);
+        int y = 0;
+
+        for( int i=0; i < x; i++ )
+            z( y );
+
+        printf( "Hello #1 from thread member %d of %d.\n", thread, threads );
+
+        gsync();
+
+        x = 1000000 * (rand() & 7);
+
+        for( int i=0; i < x; i++ )
+            z( y );
+
+        printf( "Hello #2 from thread member %d of %d.\n", thread, threads );
+    }
+};
+
+int main( int argc, char *argv[] )
+{
+    using namespace  std;
+    using namespace  C4;
+
+    cout << "Initiating thread test program.\n";
+
+    {
+        ThreadGroup<X> tgx( 5 );
+        cout << "Thread group is active.\n";
+    }
+
+    fflush( stdout );
+
+    cout << "Thread group has gone out of scope.\n";
+}
+
+//---------------------------------------------------------------------------//
+//                              end of tthrd1.cc
+//---------------------------------------------------------------------------//
