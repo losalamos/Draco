a400d04f7db4370ed02222484c8833255dc09d90
tme <tme@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1998-09-16 20:17:38 +0000

    Added a more complete black-box style test problem for AMR codes.

--- src/imc/test/testamr.cc
+++ src/imc/test/testamr.cc
@@ -22,6 +22,7 @@
 #include <algorithm>
 #include <cstdio>
 #include <vector>
+#include <cmath> 
 
 using IMC::AMR_Interface;
 using IMC::AMR_Builder;
@@ -45,8 +46,12 @@ int main(int argc, char *argv[])
     try
     {
       // material temp
-	double tev[6] = {1,1,1,1,1,1};
-	for (int cycle = 0; cycle < 10; cycle++)
+	double tev[6]  = {10,1e-2,1e-2,10,1e-2,1e-2};
+	double factor  = .20;
+	double dt      = .000001;
+	double max_dt  = .01;
+	double elapsed = 0.0;
+	for (int cycle = 0; cycle < 250; cycle++)
 	{
 	    
 	  // let's give a mesh definition, RAGE style
@@ -58,28 +63,106 @@ int main(int argc, char *argv[])
 			     0,0,1,0,1,1,1,1,1,1,0,1,0,0,2,0,1,2,1,1,2,1,0,2,
 			     1,0,1,1,1,1,2,1,1,2,0,1,1,0,2,1,1,2,2,1,2,2,0,2,
 			     2,0,1,2,1,1,3,1,1,3,0,1,2,0,2,2,1,2,3,1,2,3,0,2};
+	    for (int i = 0; i < 144; i++)
+		vert[i] = vert[i] / 10.0;
 
 	  // other data
-	    int b_proc[]       = {0};
-	    int b_cell[]       = {0};
-	    int global_cell[]  = {1,2,3,4,5,6};
-	    double dedt[6]     = {.3,.3,.3,.3,.3,.3};
-	    double rho[6]      = {3,3,3,3,3,3};
-	    double opacity[6]  = {100,100,100,100,100,100};
-	    double rev[6]      = {1,1,1,1,1,1};
-	    double t4_slope[6] = {0};
+	    int b_proc[]        = {0};
+	    int b_cell[]        = {0};
+	    int global_cell[]   = {1,2,3,4,5,6};
+	    double dedt[6]      = {.3,.3,.3,.3,.3,.3};
+	    double rho[6]       = {3,3,3,3,3,3};
+	    double opacity[6]   = {100,100,100,100,100,100};
+	    double rev[6]       = {10,1e-2,1e-2,10,1e-2,1e-2};
+	    double t4_slope[18] = {0};
 	    
 	  // variables
 	    int numcells    = 6;
 	    int g_numcells  = 6;
 	    int numbcells   = 0;
 	    double implicit = 1.0;
-	    double dt       = .001;
 	    double dnpdt    = 0;
 	    int npnom       = 1000;
 	    int npmax       = 1000;
 	    int seed        = 9347593;
 	    int buffer      = 1000;
+
+	  // calculate t4-slopes
+	    double f1;
+	    double f2;
+	    int t4i = 0;
+
+	  // cell 1
+	  // x
+	    f1 = pow(tev[0], 4);
+	    f2 = (pow(tev[0], 4) + pow(tev[1], 4)) / 2;
+	    t4_slope[t4i++] = (f2 - f1) / (.1 - 0);
+	  // y
+	    t4_slope[t4i++] = 0;
+	  // z
+	    f2 = (pow(tev[0], 4) + pow(tev[3], 4)) / 2;
+	    t4_slope[t4i++] = (f2 - f1) / (.1 - 0);
+
+	  // cell 2
+	  // x
+	    f1 = (pow(tev[0], 4) + pow(tev[1], 4)) / 2;
+	    f2 = (pow(tev[1], 4) + pow(tev[2], 4)) / 2;
+	    t4_slope[t4i++] = (f2 - f1) / (.2 - .1);
+	  // y
+	    t4_slope[t4i++] = 0;
+	  // z
+	    f1 = pow(tev[1], 4);
+	    f2 = (pow(tev[1], 4) + pow(tev[4], 4)) / 2;
+	    t4_slope[t4i++] = (f2 - f1) / (.1 - 0);
+
+	  // cell 3
+	  // x
+	    f1 = (pow(tev[1], 4) + pow(tev[2], 4)) / 2;
+	    f2 = pow(tev[2], 4);
+	    t4_slope[t4i++] = (f2 - f1) / (.3 - .2);
+	  // y
+	    t4_slope[t4i++] = 0;
+	  // z
+	    f1 = pow(tev[2], 4);
+	    f2 = (pow(tev[2], 4) + pow(tev[5], 4)) / 2;
+	    t4_slope[t4i++] = (f2 - f1) / (.1 - 0);
+
+	  // cell 4
+	  // x
+	    f1 = pow(tev[3], 4);
+	    f2 = (pow(tev[3], 4) + pow(tev[4], 4)) / 2;
+	    t4_slope[t4i++] = (f2 - f1) / (.1 - 0);
+	  // y
+	    t4_slope[t4i++] = 0;
+	  // z
+	    f1 = (pow(tev[0], 4) + pow(tev[3], 4)) / 2;
+	    f2 = pow(tev[3], 4);
+	    t4_slope[t4i++] = (f2 - f1) / (.2 - .1);
+
+	  // cell 5
+	  // x
+	    f1 = (pow(tev[3], 4) + pow(tev[4], 4)) / 2;
+	    f2 = (pow(tev[4], 4) + pow(tev[5], 4)) / 2;
+	    t4_slope[t4i++] = (f2 - f1) / (.2 - .1);
+	  // y
+	    t4_slope[t4i++] = 0;
+	  // z
+	    f1 = (pow(tev[1], 4) + pow(tev[4], 4)) / 2;
+	    f2 = pow(tev[4], 4);
+	    t4_slope[t4i++] = (f2 - f1) / (.2 - .1);
+
+	  // cell 6
+	  // x
+	    f2 = pow(tev[5], 4);
+	    f1 = (pow(tev[4], 4) + pow(tev[5], 4)) / 2;
+	    t4_slope[t4i++] = (f2 - f1) / (.3 - .2);
+	  // y
+	    t4_slope[t4i++] = 0;
+	  // z
+	    f1 = (pow(tev[2], 4) + pow(tev[5], 4)) / 2;
+	    f2 = pow(tev[5], 4);
+	    t4_slope[t4i++] = (f2 - f1) / (.2 - .1);
+	    Check (t4i == 18);
 	    
 	  // return arrays
 	    double e_dep[6]   = {0};
@@ -88,7 +171,6 @@ int main(int argc, char *argv[])
 	  // initialize
 	    for (int i = 0; i < numcells; i++)
 		opacity[i] = opacity[i] / (tev[i] * tev[i] * tev[i]);
-	    double elapsed = IMC::Global::max(0.0, (cycle - 1) * dt);
 
 	  // call rage_imc
 	    rage_imc_(&cycle, &numcells, &g_numcells, vert, lay, &numbcells,
@@ -98,11 +180,19 @@ int main(int argc, char *argv[])
 	    
 	  // update temp
 	    cout << " *** RESULTS FOR CYCLE " << cycle << " ***" << endl;
+	    cout << "  ** Elapsed Time : " << elapsed << endl;
+	    cout << "  ** Time Step    : " << dt << endl;
 	    for (int i = 0; i < numcells; i++)
 	    {
 		tev[i] += e_dep[i] / dedt[i];
 		cout << i+1 << "\t" << tev[i] << "\t" << rad_den[i] << endl;
 	    }
+	    cout << endl;
+	    
+	  // increment timestep
+	    if (cycle > 0 && dt < max_dt)
+		dt += dt * factor;
+	    elapsed += dt;
 	}
     }
     catch (const dsxx::assertion &ass)
