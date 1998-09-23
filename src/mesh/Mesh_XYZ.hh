422b2e71246778de6674d7a39b046b727c6e45cd
rsqrd <rsqrd@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1998-09-23 17:24:46 +0000

    Added get_volumes().

--- src/mesh/Mesh_XYZ.hh
+++ src/mesh/Mesh_XYZ.hh
@@ -619,6 +619,7 @@ class Mesh_XYZ : private XYZ_Mapper
     const dsxx::Mat1<double>& get_zA() const { return zA; }
 
     const ccsf& get_vc() const { return vc; }
+    const ccsf& get_volumes() const { return get_vc(); }
 
     const int *get_diag_offsets() const { return diags; }
 
