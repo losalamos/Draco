1bd679f4de08668563a35ef2fc18f0b01d4f18f7
pautz <pautz@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1999-01-06 23:28:56 +0000

    Upgraded the Mat2 container to be a Random Access Container.  Added a test program for regression testing to confirm that fact.

--- src/ds++/Mat.hh
+++ src/ds++/Mat.hh
@@ -446,8 +446,17 @@ class Mat2 {
     T *v;
 
   public:
+    typedef T value_type;
+    typedef T& reference;
+    typedef const T& const_reference;
+    typedef T* pointer;
+    typedef const T* const_pointer;
+    typedef typename Allocator::difference_type difference_type;
+    typedef typename Allocator::size_type size_type;
     typedef typename Allocator::iterator iterator;
     typedef typename Allocator::const_iterator const_iterator;
+    typedef typename Allocator::reverse_iterator reverse_iterator;
+    typedef typename Allocator::const_reverse_iterator const_reverse_iterator;
 
 // Accessors
 
@@ -463,12 +472,22 @@ class Mat2 {
     iterator       end()         { return v + offset(xmax(),ymax()) + 1; }
     const_iterator end() const   { return v + offset(xmax(),ymax()) + 1; }
 
+    reverse_iterator       rbegin()       { return reverse_iterator(end()); }
+    const_reverse_iterator rbegin() const
+    { return const_reverse_iterator(end()); }
+
+    reverse_iterator       rend()         { return reverse_iterator(begin()); }
+    const_reverse_iterator rend() const
+    { return const_reverse_iterator(begin()); }
+
     int get_xmin() const { return xmin; }
     int get_xlen() const { return xlen; }
     int get_ymin() const { return ymin; }
     int get_ylen() const { return ylen; }
 
-    int size() const { return nx() * ny(); }
+    size_type size() const { return nx() * ny(); }
+    size_type max_size () const { return alloc.max_size(); }
+    bool empty() const { return (this->size() == 0); }
 
 // For backward compatibility.
     int nx() const { return xlen; }
@@ -557,6 +576,88 @@ class Mat2 {
 	}
 
 	return *this;
+    }
+
+    void swap ( Mat2& m )
+    {
+        int itemp;
+        bool btemp;
+        Allocator atemp;
+        T* ptemp;
+
+        itemp = xmin;
+        xmin = m.xmin;
+        m.xmin = itemp;
+
+        itemp = xlen;
+        xlen = m.xlen;
+        m.xlen = itemp;
+
+        itemp = ymin;
+        ymin = m.ymin;
+        m.ymin = itemp;
+
+        itemp = ylen;
+        ylen = m.ylen;
+        m.ylen = itemp;
+
+        btemp = may_free_space;
+        may_free_space = m.may_free_space;
+        m.may_free_space = btemp;
+
+        atemp = alloc;
+        alloc = m.alloc;
+        m.alloc = atemp;
+
+        ptemp = v;
+        v = m.v;
+        m.v = ptemp;
+    }
+
+// Boolean operators
+
+    bool operator==( const Mat2& m ) const
+    {
+        if (this == &m)
+            return true;
+
+        if ( m.size() != this->size() )
+            return false;
+
+        const_iterator miter = m.begin();
+        for (const_iterator iter = this->begin(); iter != this->end(); ++iter)
+        {
+            if (*iter != *miter) return false;
+            ++miter;
+        }
+
+        return true;
+    }
+
+    bool operator!=( const Mat2& m ) const
+    {
+        return !(*this == m);
+    }
+
+    bool operator<( const Mat2& m ) const
+    {
+        return lexicographical_compare(this->begin(), this->end(), m.begin(), 
+				       m.end());
+    }
+
+    bool operator>( const Mat2& m ) const
+    {
+        return (m < *this);
+    }
+
+    bool operator<=( const Mat2& m ) const
+    {
+        return !(m < *this);
+    }
+
+    bool operator>=( const Mat2& m ) const
+    {
+        return !(*this < m);
     }
 
 // Mathematical support
