cee9e317657b0540e14d0ebcccbb7ea3f1086344
rsqrd <rsqrd@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1999-02-24 23:30:57 +0000

    Added way to run tests in scalar only mode. This is important for packages below the c4 level.
    Also add $Id$ to configure.in files.
    
    Moved template instantiations to Solon's 3T/test directory, as
    per the new policy of not instantiating on the MT in the package libraries,
    only in packages that explicitly use the instantiations.

--- src/radphys/radphys_pt.cc
+++ src/radphys/radphys_pt.cc
@@ -5,9 +5,10 @@
 //---------------------------------------------------------------------------//
 // @> Template instantiations for the radphyslibrary.
 //---------------------------------------------------------------------------//
+// $Id$
+//---------------------------------------------------------------------------//
 
 #include "RadiationPhysics.t.hh"
-#include "mesh/Mesh_XYZ.hh"
 
 namespace XTM
 {
@@ -34,90 +35,6 @@ namespace XTM
 
  template void RadiationPhysics::getPlanck(const double &TElectron,
 					   double &planckian) const;
-
- // Instantiate the classes using Mesh_XYZ
- 
- typedef Mesh_XYZ MT;
-
- template<>
- class ContainerTraits<MT::ccsf >
- {
-   public:
-     typedef MT::ccsf::iterator iterator;
-     typedef MT::ccsf::const_iterator const_iterator;
-
-     static inline iterator begin(MT::ccsf &a)
-     {
-	 return a.begin();
-     }
-
-     static inline const_iterator begin(const MT::ccsf &a)
-     {
-	 return a.begin();
-     }
-
-     static inline iterator end(MT::ccsf &a)
-     {
-	 return a.end();
-     }
-
-     static inline const_iterator end(const MT::ccsf &a)
-     {
-	 return a.end();
-     }
-
-     static inline bool conformal(const MT::ccsf &a,
-			     const MT::ccsf &b)
-     {
-	 const MT &msha = a.get_Mesh();
-	 const MT &mshb = b.get_Mesh();
-
-	 return msha == mshb;
-     }
- };
-
- template<>
- class ContainerTraits<MT::fcdsf >
- {
-   public:
-     typedef MT::fcdsf::iterator iterator;
-     typedef MT::fcdsf::const_iterator const_iterator;
-
-     static inline iterator begin(MT::fcdsf &a)
-     {
-	 return a.begin();
-     }
-
-     static inline const_iterator begin(const MT::fcdsf &a)
-     {
-	 return a.begin();
-     }
-
-     static inline iterator end(MT::fcdsf &a)
-     {
-	 return a.end();
-     }
-
-     static inline const_iterator end(const MT::fcdsf &a)
-     {
-	 return a.end();
-     }
-     
-     static inline bool conformal(const MT::fcdsf &a,
-				  const MT::fcdsf &b)
-     {
-	 const MT &msha = a.get_Mesh();
-	 const MT &mshb = b.get_Mesh();
-	 
-	 return msha == mshb;
-     }
- };
-
- template void RadiationPhysics::getPlanck(const MT::ccsf &TElectron,
-					   MT::ccsf &planckian) const;
- template void RadiationPhysics::
- getPlanckTemperatureDerivative(const MT::ccsf &TElectron,
-				MT::ccsf &dplanckdT) const;
 
 } // end namespace XTM
 
