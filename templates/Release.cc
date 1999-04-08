42dce6bf3ec566571ed846c68b9fdd8b2ed9c286
tme <tme@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1999-04-08 15:40:04 +0000

    Update to Release.cc template.

--- templates/Release.cc
+++ templates/Release.cc
@@ -11,16 +11,18 @@
 
 namespace rtt_<spkg>
 {
-    using std::string;
 
-  // function definition for Release, define the local version number for
-  // this library in the form <spkg>_#.#.# in pkg_version variable
-    const string release()
-    {
-	string pkg_release = "<spkg>_<start>#.#.#";
-	return pkg_release;
-    }
+using std::string;
+
+// function definition for Release, define the local version number for
+// this library in the form <spkg>_#.#.# in pkg_version variable
+const string release()
+{
+    string pkg_release = "<spkg>_<start>#.#.#";
+    return pkg_release;
 }
+
+}  // end of rtt_<spkg>
 
 //---------------------------------------------------------------------------//
 //                              end of Release.cc
