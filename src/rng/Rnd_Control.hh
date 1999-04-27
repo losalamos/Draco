4851766a11f3a3b36d8a0c5aed4ef682c547fe58
tme <tme@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1999-04-27 00:15:18 +0000

    Some reformatting of comments.

--- src/rng/Rnd_Control.hh
+++ src/rng/Rnd_Control.hh
@@ -2,6 +2,7 @@
 // Rnd_Control.hh
 // Thomas M. Evans
 // Wed Apr 29 16:08:58 1998
+// $Id$
 //---------------------------------------------------------------------------//
 // @> Rnd_Control header file
 //---------------------------------------------------------------------------//
@@ -30,37 +31,37 @@ namespace rtt_rng
 
 class Rnd_Control 
 {
-private:
-  // seed for initialization of random number streams
+  private:
+    // seed for initialization of random number streams
     int seed;
-  // total number of streams
+    // total number of streams
     int number;
-  // number of current stream
+    // number of current stream
     int streamnum;
-  // control parameter for stream inits
+    // control parameter for stream inits
     int parameter;
-  // size of packed stream state
+    // size of packed stream state
     int size;
 
-public:
-  // constructor
+  public:
+    // constructor
     Rnd_Control(int, int = 1000000000, int = 0, int = 1);
 
-  // start a new random number stream
+    // start a new random number stream
     Sprng get_rn();
     Sprng get_rn(int);
 
-  // spawn a new random number stream
+    // spawn a new random number stream
     Sprng spawn(const Sprng &) const;
 
-  // query for the number of random streams
+    // query for the number of random streams
     int get_num() const { return streamnum; }
     void set_num(int num) { streamnum = num; }
 
-  // query size of packed random number state
+    // query size of packed random number state
     int get_size() const { return size; }
 
-  // get the seed value and total number of current streams set here
+    // get the seed value and total number of current streams set here
     int get_seed() const { return seed; }
     int get_number() const { return number; }
 };
