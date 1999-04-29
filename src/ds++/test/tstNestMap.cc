f0fa0c7f3dbaee03c82c61abd2aa31fa08fdf1a4
pautz <pautz@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1999-04-29 16:43:13 +0000

    Added test code for the "NestMap" data structure.

---
+++ src/ds++/test/tstNestMap.cc
@@ -0,0 +1,91 @@
+//----------------------------------*-C++-*----------------------------------//
+// tstNestMap.cc
+// Shawn Pautz
+// Tue Mar 30 08:22:51 1999
+// $Id$
+//---------------------------------------------------------------------------//
+// @> 
+//---------------------------------------------------------------------------//
+
+#include "../NestMap.t.hh"
+#include <iostream>
+
+using rtt_ds::NestMap;
+using std::cout;
+using std::endl;
+
+bool passed = true;
+
+void version(const std::string &progname)
+{
+    std::string version = "1.0.0";
+    cout << progname << ": version " << version << endl;
+}
+
+int main( int argc, char *argv[] )
+{
+    for (int arg=1; arg < argc; arg++)
+    {
+	if (std::string(argv[arg]) == "--version")
+	{
+	    version(argv[0]);
+	    return 0;
+	}
+    }
+    
+    cout << "Initiating test of the NestMap class.\n";
+
+    NestMap<std::string, int> intTree;
+
+    intTree.open("one", 1);   // stack: 1
+    intTree.close();          // stack:
+    intTree.open("one", 1);   // stack: 1
+    intTree.close();          // stack:
+    intTree.open("two", 2);   // stack: 2
+    intTree.close();          // stack:
+
+    intTree.open("one", 1);            // stack: 1
+    intTree.open("one one", 11);       // stack: 1 11
+    intTree.open("one one one", 111);  // stack: 1 11 111
+    intTree.close();                   // stack: 1 11
+    intTree.close();                   // stack: 1
+    intTree.open("one two", 12);       // stack: 1 12
+    intTree.close();                   // stack: 1
+    intTree.open("one three", 13);     // stack: 1 13
+    intTree.close();                   // stack: 1
+    intTree.close();                   // stack:
+    intTree.open("three", 3);          // stack: 3
+    intTree.open("three three", 33);   // stack: 3 33
+    intTree.close();                   // stack: 3
+    intTree.close();                   // stack:
+
+    cout << "\nDumping objects" << endl;
+    for (NestMap<std::string, int>::iterator iter = intTree.begin();
+	 iter != intTree.end(); ++iter)
+    {
+	cout << (*iter).first << endl;
+    }
+
+// Print the status of the test.
+
+    cout << endl;
+    cout <<     "***********************************" << endl;
+    if (passed) 
+    {
+        cout << "**** NestMap Self Test: PASSED ****" << endl;
+    }
+    else
+    {
+        cout << "**** NestMap Self Test: FAILED ****" << endl;
+    }
+    cout <<     "***********************************" << endl;
+    cout << endl;
+
+    cout << "Done testing NestMap class.\n";
+
+    return 0;
+}
+
+//---------------------------------------------------------------------------//
+//                              end of tstNestMap.cc
+//---------------------------------------------------------------------------//
