25d09062eedc534309f47fb694f8636ad3b5433a
tme <tme@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1999-05-19 17:14:34 +0000

    Wrapped dynarray instantiations in the dsxx namespace.  This satisfies compilation with more ISO-picky compilers.

--- src/ds++/DynArray_pt.cc
+++ src/ds++/DynArray_pt.cc
@@ -4,38 +4,42 @@
 // 14 January 1997
 //---------------------------------------------------------------------------//
 
+#include <iostream>
 #include "DynArray.t.hh"
 
-using namespace dsxx;
+namespace dsxx
+{
 
 template class DynArray<int>;
 template std::ostream& operator<<( std::ostream& os, 
-				  const DynArray<int>& d );
+				   const DynArray<int>& d );
 
 template class DynArray<long>;
 template std::ostream& operator<<( std::ostream& os, 
-				  const DynArray<long>& d );
+				   const DynArray<long>& d );
 
 template class DynArray<char>;
 template std::ostream& operator<<( std::ostream& os, 
-				  const DynArray<char>& d );
+				   const DynArray<char>& d );
 
 template class DynArray<float>;
 template std::ostream& operator<<( std::ostream& os, 
-				  const DynArray<float>& d );
+				   const DynArray<float>& d );
 
 template class DynArray<double>;
 template std::ostream& operator<<( std::ostream& os, 
-	  	                  const DynArray<double>& d );
+				   const DynArray<double>& d );
 
 #include <string>
 
 template class DynArray<std::string>;
 template std::ostream& operator<<( std::ostream& os, 
-			          const DynArray<std::string>& d );
+				   const DynArray<std::string>& d );
 
 // instantiate class DynArray<String>
 // instantiate function String ostream& operator<<( ostream& os, const DynArray<String>& d )
 
 template class DynArray<DynArray<int> >;
 template class DynArray<DynArray<char> >;
+
+}
