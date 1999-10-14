b5023f791648098360e57c3df4ddb47b11f0877a
mcghee <mcghee@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
1999-10-14 15:50:31 +0000

    Added a shell script to count the lines of Draco code used by a client system. Some cleanup on the scripts which support reuse_loc.

--- tools/get_include_tree.sh
+++ tools/get_include_tree.sh
@@ -17,12 +17,34 @@
 #  DESCRIPTION
 #       get_include_tree.sh looks through the .d dependency files generated
 #       as a result of a configure and make, finding the package and
-#       files names that are used.
+#       files names that are used. This script is intended to be used in
+#       a fully configured target directory.
 #
 #  ========================================================================  #
 
+#Get command line arguments, if any
+if [ $# = 0 ]
+then
+  TMP="."
+elif [ $# = 1 ]
+then
+  TMP="$1"
+else
+  TMP="$1"
+  echo "Warning -- multiple command line arguments found,"
+  echo "           using first argument and ignoring others"
+  echo
+fi
+
+#Check for existence of the directory to be scanned
+if [ ! -d $TMP ];
+then
+ echo $TMP " is not a directory."
+ exit 1
+fi
+
 # Look through the depend files and extract the .hh files
-dependFiles=`find $1 -name '*.d' -print`
+dependFiles=`find $TMP -name '*.d' -print`
 hhFiles=`cat $dependFiles | grep '.hh' | awk '{print $4}' | sort | uniq`
 
 # Loop though the .hh files to get the package and file name
@@ -38,3 +60,4 @@ do
 done |
 sort | uniq
 
+#  ================ end of get_include_files.sh =========================  #
