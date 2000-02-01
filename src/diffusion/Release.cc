ff54910b08a1f38e73d65fd530e2a818b328dd00
rsqrd <rsqrd@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2000-02-01 16:25:55 +0000

    diffusion-1_1_0

---
+++ src/diffusion/Release.cc
@@ -0,0 +1,35 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   diffusion/Release.cc
+ * \author Randy M. Roberts
+ * \date   Tue Feb  1 09:24:50 2000
+ * \brief  Release function implementation for diffusion library
+ */
+//---------------------------------------------------------------------------//
+// $Id$
+//---------------------------------------------------------------------------//
+
+#include "Release.hh"
+
+namespace rtt_diffusion
+{
+
+using std::string;
+
+/*!  
+ * \return string of the release number
+ *
+ * Function definition for Release, define the local version number for
+ * this library in the form diffusion-\#_\#_\# in pkg_release variable 
+ */
+const string release()
+{
+    string pkg_release = "@(#)diffusion-1_1_0";
+    return pkg_release;
+}
+
+}  // end of rtt_diffusion
+
+//---------------------------------------------------------------------------//
+//                             end of Release.cc
+//---------------------------------------------------------------------------//
