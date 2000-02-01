0fe1a568d65746516f7b2c6b395df8897d2e6093
rsqrd <rsqrd@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2000-02-01 21:25:19 +0000

    Added Release files.

---
+++ src/radphys/Release.cc
@@ -0,0 +1,35 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   radphys/Release.cc
+ * \author Randy M. Roberts
+ * \date   Tue Feb  1 09:30:08 2000
+ * \brief  Release function implementation for radphys library
+ */
+//---------------------------------------------------------------------------//
+// $Id$
+//---------------------------------------------------------------------------//
+
+#include "Release.hh"
+
+namespace rtt_radphys
+{
+
+using std::string;
+
+/*!  
+ * \return string of the release number
+ *
+ * Function definition for Release, define the local version number for
+ * this library in the form radphys-\#_\#_\# in pkg_release variable 
+ */
+const string release()
+{
+    string pkg_release = "@(#)radphys-1_1_0";
+    return pkg_release;
+}
+
+}  // end of rtt_radphys
+
+//---------------------------------------------------------------------------//
+//                             end of Release.cc
+//---------------------------------------------------------------------------//
---
+++ src/radphys/Release.hh
@@ -0,0 +1,43 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   radphys/Release.hh
+ * \author Randy M. Roberts
+ * \date   Tue Feb  1 09:30:08 2000
+ * \brief  Release function for the radphys library
+ */
+//---------------------------------------------------------------------------//
+// $Id$
+//---------------------------------------------------------------------------//
+
+#ifndef __radphys_Release_hh__
+#define __radphys_Release_hh__
+
+//===========================================================================//
+/*!
+ * \page radphys_overview Overview of the radphys package
+ * \version
+ * \author
+ * 
+ * 
+ */
+//===========================================================================//
+/*!
+ * \namespace rtt_radphys
+ *
+ * \brief Namespace that contains the radphys package classes and variables.
+ *
+ */
+//===========================================================================//
+
+#include <string>
+
+namespace rtt_radphys 
+{
+    const std::string release();
+}
+
+#endif                          // __radphys_Release_hh__
+
+//---------------------------------------------------------------------------//
+//                           end of radphys/Release.hh
+//---------------------------------------------------------------------------//
