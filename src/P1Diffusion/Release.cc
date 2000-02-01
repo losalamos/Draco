89591f6deb71637f767b8eca42dc7b509321efdc
rsqrd <rsqrd@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2000-02-01 16:18:44 +0000

    P1Diffusion-1_2_0 release.

--- src/P1Diffusion/Release.cc
+++ src/P1Diffusion/Release.cc
@@ -24,7 +24,7 @@ using std::string;
  */
 const string release()
 {
-    string pkg_release = "@(#)P1Diffusion-1_0_0";
+    string pkg_release = "@(#)P1Diffusion-1_2_0";
     return pkg_release;
 }
 
