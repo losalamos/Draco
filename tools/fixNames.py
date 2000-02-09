04eb4fe995ab844719d9a282539f27f198485bdb
tme <tme@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2000-02-09 00:06:57 +0000

    Final version of fixNames script.

--- tools/fixNames.py
+++ tools/fixNames.py
@@ -2,7 +2,7 @@
 ## fixNames.py
 ## Thomas M. Evans
 ## Tue Feb  8 15:34:20 2000
-## Time-stamp: <00/02/08 16:39:21 tme>
+## Time-stamp: <00/02/08 16:55:03 tme>
 ###############################################################################
 ##---------------------------------------------------------------------------##
 ## fixNames
@@ -17,23 +17,26 @@ import re
 
 ##---------------------------------------------------------------------------##
 
-dirlist = glob.glob("*.hh")
+hhdirlist = glob.glob("*.hh")
+ccdirlist = glob.glob("*.cc")
+dirlist   = [hhdirlist, ccdirlist]
 
-for file in dirlist:
-    # open file
-    file_contents = open(file).read()
+for files in dirlist:
+    for file in files:
+        # open file
+        file_contents = open(file).read()
 
-    # find substitute expressions
-    (match, mc)   = re.subn(" dsxx::", " rtt_dsxx::", file_contents)
-    (match2, mc2) = re.subn(" dsxx;", " rtt_dsxx;", match)
+        # find substitute expressions
+        (match, mc)   = re.subn(" dsxx::", " rtt_dsxx::", file_contents)
+        (match2, mc2) = re.subn(r'namespace\s+dsxx', "namespace rtt_dsxx", match)
 
-    # output modified files and count
-    count = mc + mc2
+        # output modified files and count
+        count = mc + mc2
     
-    if count > 0:
-        new_file = open(file, 'w')
-        new_file.write(match2)
-        print "Replaced %d expressions in %s" % (count, file)
+        if count > 0:
+            new_file = open(file, 'w')
+            new_file.write(match2)
+            print "Replaced %d expressions in %s" % (count, file)
     
 ###############################################################################
 ##                            end of fixNames.py
