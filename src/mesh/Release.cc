adaf38ef67208b243bac49d07134c6e5061e67f5
rsqrd <rsqrd@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2000-02-09 17:50:11 +0000

    This release has changed dsxx to rtt_dsxx namespace.

--- src/mesh/Release.cc
+++ src/mesh/Release.cc
@@ -18,7 +18,7 @@ using std::string;
 // this library in the form mesh-#_#_# in pkg_version variable
 const string release()
 {
-    string pkg_release = "@(#)mesh-1_5_0";
+    string pkg_release = "@(#)mesh-1_6_0";
     return pkg_release;
 }
 
