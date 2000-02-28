90c053db5da062a057b6b81843410d7fa159c45a
rsqrd <rsqrd@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2000-02-28 15:58:20 +0000

    Added comments.
    Added recognition of --core argument.  This prevents try block usage,
    in order to receive core dumps.

--- src/UnitTestFrame/TestApp.hh
+++ src/UnitTestFrame/TestApp.hh
@@ -3,7 +3,7 @@
  * \file   UnitTestFrame/TestApp.hh
  * \author Randy M. Roberts
  * \date   Fri Feb 25 07:34:16 2000
- * \brief  
+ * \brief  The abstract base from which unit test applications can be derived.
  */
 //---------------------------------------------------------------------------//
 // $Id$
@@ -28,7 +28,10 @@ class PassFailStream;
 //===========================================================================//
 /*!
  * \class TestApp
- *
+ * This class forms the abstract base from which unit test applications
+ * can be derived.  Unit tests derived from this class must supply the
+ * name() and version() methods, as well as the runTest() method, which will
+ * be called to perform the actual unit test.
  */
 // revision history:
 // -----------------
@@ -41,70 +44,204 @@ class TestApp
 
     // NESTED CLASSES AND TYPEDEFS AND FRIEND CLASSES
 
+    /*!
+     * The PassFailStream class makes use of several protected methods
+     * of TestApp objects, inorder to perform its functions.
+     */
+    
     friend class PassFailStream;
 
     // DATA
 
+    /*!
+     * The static theSPTestApp smart pointer is an attempt to enforce
+     * the Singleton pattern.
+     */
+    
     static rtt_dsxx::SP<TestApp> theSPTestApp;
 
+    /*!
+     * The argument list, defined by argc and argv in the constructor call,
+     * are stored in a list of std::string's.
+     */
+    
     std::list<std::string> argList_m;
+
+    /*!
+     * This message list contains all of the pass/fail messages.
+     * Pass and fail messages are not sent directly to the ostream,
+     * but are stored in the message list for later processing.
+     */
+    
     std::list<std::string> messageList_m;
+
+    /*!
+     * A flag to determine whether this unit test passed or failed.
+     */
     
     bool passed_m;
+
+    /*!
+     * The ostream to which pass/fail messages should be written.
+     */
+    
     std::ostream &os_m;
 
   public:
 
     // CREATORS
+
+    /*!
+     * The ctor for the unit test base class.  The argc and argv represent
+     * the command line arguments.  They will be used to create a list of
+     * command line argument std::string's.  The os_in argument refers to
+     * the ostream to which pass/fail messages are written.
+     */
     
     TestApp(int &argc,  char *argv[], std::ostream &os_in = std::cerr);
     
     //Defaulted: TestApp(const TestApp &rhs);
 
+    /*!
+     * The dtor is virtual, since derived class' dtor's must be called
+     * via base class references or pointers.
+     */
+    
     virtual ~TestApp() { /* empty */ }
 
     // MANIPULATORS
     
     //Defaulted: TestApp& operator=(const TestApp &rhs);
 
+    /*!
+     * The run method is the public interface used to run the unit test(s).
+     * The run method, in turn, calls the protected virtual runTest() method.
+     * If you are using the mainDriver's main(argc,argv) then this method
+     * is called automatically (outside of any try blocks).
+     * The return value is printed to the pass/fail message ostream.
+     * If --version is supplied in the arguments, run() returns the version
+     * info and does not run runTest.
+     */
+    
     std::string run();
 
     // PROTECTED MANIPULATORS
     
   protected:
 
+    /*!
+     * The run method is the protected virtual method used to run the unit
+     * test(s).
+     * This method is called within the public run() method.
+     * The derived class must supply its own version of this class.
+     * The return value is usually returned by run().
+     */
+    
     virtual std::string runTest() = 0;
 
+    /*!
+     * Get your grubby little hands on the argument list.
+     */
+    
     std::list<std::string> &argList() { return argList_m; }
+
+    /*!
+     *             !!!YOUR MESSAGE HERE!!!
+     *
+     * Add your own messages, just like the pass/fail messages
+     * added by the PassFailStream objects.  If you are using the
+     * mainDriver framework then these messages will be printed
+     * to the ostream.
+     */
     
     void addMessage(const std::string &message)
     {
 	messageList_m.push_back(message);
     }
+
+    /*!
+     * Protected method to change the pass/fail status of the test.
+     */
+    
+    bool setPassed(bool flag)
+    {
+	bool tmp = passed_m;
+	passed_m = flag;
+	return tmp;
+    }
+    
+    /*!
+     * Now the magic...
+     * The pass() method is for use within the overloaded runTest() methods.
+     * This method returns a PassFailStream just aching to have data output
+     * onto them via the operator<< "inserter" (Stroustrup page 608).
+     * The pass() method gives you a PassFailStream that says a subtest passed.
+     */
     
     PassFailStream pass(const std::string &str = "");
+
+    /*!
+     * The fail() method is for use within the overloaded runTest() methods.
+     * This method returns a PassFailStream just aching to have data output
+     * onto them via the operator<< "inserter" (Stroustrup page 608).
+     * The fail() method gives you a PassFailStream that says a subtest failed.
+     */
+    
     PassFailStream fail(const std::string &str = "");
 
   public:
 
     // ACCESSORS
 
+    /*!
+     * The ostream to which pass/fail messages should be written.
+     */
+    
     std::ostream &os() const { return os_m; }
 
+    /*!
+     * The pass/fail state of this unit test.
+     */
+    
     bool passed() const { return passed_m; }
 
+    /*!
+     * The pass/fail message list.
+     */
+    
     const std::list<std::string> &messageList() const { return messageList_m; }
 
+    /*!
+     * A convenience utility that retrieves the argument after the specified
+     * argument, i.e. foo --a x --b y --z --t
+     * with a call to getNextArg("--b") returns "y".
+     */
+    
     std::string getNextArg(const std::string &arg) const;
 
+    /*!
+     * The name of the unit test.  Must be supplied by derived class.
+     */
+    
     virtual std::string name() const = 0;
 
+    /*!
+     * The version of the unit being tested.  Must be supplied by the
+     * derived class.
+     */
+    
     virtual std::string version() const = 0;
 
   public:
     
     // STATIC CREATOR
 
+    /*!
+     * The public static method of accessing the **one** object of the class
+     * derived from this class.  Used to enforce the Singleton pattern.
+     * This method calls the static private create method.
+     */
+    
     static TestApp &theTestApp(int &argc, char *argv[],
 			       std::ostream &os_in = std::cerr)
     {
@@ -113,6 +250,12 @@ class TestApp
 	return *theSPTestApp;
     }
 
+    /*!
+     * The public static method of accessing the **one** object of the class
+     * derived from this class.  Used to enforce the Singleton pattern.
+     * This method calls the static private create method.
+     */
+    
     static TestApp &theTestApp()
     {
 	int argc = 0;
@@ -126,6 +269,19 @@ class TestApp
 
     // STATIC IMPLEMENTATION
     
+    /*!
+     * When setting up a unit test the author of the test must **define**
+     * this static method that creates a new object of the derived class,
+     * and returns a base class smart pointer to it.
+     * For Example, if MyTestApp is derived from TestApp, then in MyTestApp.cc
+     * (ignoring namespaces)...
+
+     * SP<TestApp> TestApp::create(int &argc, char *argv[], ostream& os_in)
+     * {
+     *    return SP<TestApp>(new MyTestApp(argc, argv, os_in));
+     * }
+     */
+    
     static rtt_dsxx::SP<TestApp> create(int &argc, char *argv[],
 					std::ostream &os_in);
 
@@ -133,13 +289,6 @@ class TestApp
 
     // IMPLEMENTATION
 
-    bool setPassed(bool flag)
-    {
-	bool tmp = passed_m;
-	passed_m = flag;
-	return tmp;
-    }
-    
 };
 
 } // end namespace rtt_UnitTestFrame
--- src/UnitTestFrame/mainDriver.cc
+++ src/UnitTestFrame/mainDriver.cc
@@ -4,6 +4,12 @@
  * \author Randy M. Roberts
  * \date   Fri Feb 25 10:12:53 2000
  * \brief  Main driver for the UnitTestFrame
+ *
+ * This file is used to provide the main program that drives unit tests
+ * within the unit test framework.  The user of this framework need only
+ * derive a class from TestApp, supply any pure virtual methods specified
+ * by TestApp, provide a definition for TestApp::create(), and link in
+ * this library.
  */
 //---------------------------------------------------------------------------//
 // $Id$
@@ -18,37 +24,75 @@
 
 int main(int argc, char *argv[])
 {
+    // This is the call to kick off C4.
+    
     C4::Init(argc, argv);
+
+    // Decide wether we want to use try blocks or cause core
+    // dumps. The --core flag on the command line determines that core dumps,
+    // instead of try blocks will be used.
+    
+    bool coreDump = false;
+    
+    for (int i=1; i<argc; ++i)
+	if (std::string(argv[i]) == "--core")
+	    coreDump = true;
 
     std::string status;
 
     using rtt_UnitTestFrame::TestApp;
 
-    try
+    if (coreDump)
     {
+	try
+	{
+	    // Create and access the TestApp derived class singleton.
+	    
+	    TestApp &theTestApp = TestApp::theTestApp(argc, argv, std::cout);
+	}
+	catch(rtt_dsxx::assertion& a)
+	{
+	    status = std::string("Caught Assertion in Test Construction: ")
+		+ a.what();
+	}
+	catch(std::exception& a)
+	{
+	    status = std::string("Caught Exception in Test Construction: ")
+		+ a.what();
+	}
+    }
+    else
+    {
+	// Create and access the TestApp derived class singleton.
+	
 	TestApp &theTestApp = TestApp::theTestApp(argc, argv, std::cout);
     }
-    catch(rtt_dsxx::assertion& a)
-    {
-	status = std::string("Caught Assertion in Test Construction: ")
-	    + a.what();
-    }
-    catch(std::exception& a)
-    {
-	status = std::string("Caught Exception in Test Construction: ")
-	    + a.what();
-    }
 
+    // This access of the TestApp derived class object does **not** create
+    // a new one... it's a singleton for goodness sake!
+    
     TestApp &theTestApp = TestApp::theTestApp();
+
+    // Let's run the unit test.
+    // The base class run method will check for "--core" and decide whether
+    // to run the test within a try block.
+    
     status = theTestApp.run();
+
+    // Copy all of the generated pass/fail messages to the pass/fail ostream.
     
     std::copy(theTestApp.messageList().begin(),
 	      theTestApp.messageList().end(),
 	      std::ostream_iterator<std::string>(theTestApp.os(), "\n"));
 
+    // If the run method returned a string let's copy it to the pass/fail
+    // ostream.
+    
     if (status.size() > 0)
 	theTestApp.os() << status << std::endl;
 
+    // This is the call to kick out C4.
+    
     C4::Finalize();
 }
 
