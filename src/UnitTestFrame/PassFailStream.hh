92d854ec17bf366c3881fde16a6f79b5c03be5cc
rsqrd <rsqrd@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2000-03-06 17:11:57 +0000

    Added ability to use std ios manipulators with PassFailStream's.

--- src/UnitTestFrame/PassFailStream.hh
+++ src/UnitTestFrame/PassFailStream.hh
@@ -13,6 +13,7 @@
 #define __UnitTestFrame_PassFailStream_hh__
 
 #include "TestApp.hh"
+#include <iosfwd>
 #include <sstream>
 
 namespace rtt_UnitTestFrame
@@ -20,7 +21,7 @@ namespace rtt_UnitTestFrame
  
 //===========================================================================//
 /*!
- * \class PassFailStream
+ * \class PassFailStreamBase
  *
  */
 // revision history:
@@ -29,7 +30,76 @@ namespace rtt_UnitTestFrame
 // 
 //===========================================================================//
 
-class PassFailStream
+template<class Ch, class Tr>
+class PassFailStreamBase
+{
+
+    // NESTED CLASSES AND TYPEDEFS
+
+    // DATA
+
+    std::ostringstream ost_m;
+    
+  public:
+
+    // CREATORS
+    
+    PassFailStreamBase()
+    {
+	// empty
+    }
+    
+    //Defaulted: PassFailStreamBase(const PassFailStreamBase &rhs);
+
+    //Defaulted: ~PassFailStreamBase()
+
+    // MANIPULATORS
+    
+    //Defaulted: PassFailStreamBase& operator=(const PassFailStreamBase &rhs);
+
+    template<class T>
+    PassFailStreamBase &operator<<(const T& val)
+    {
+	ost() << val;
+	return *this;
+    }
+
+    // Handle Manipulators
+    
+    PassFailStreamBase &operator<<(std::basic_ostream<Ch,Tr>
+				   &(*f)(std::basic_ostream<Ch,Tr> &))
+    {
+	f(ost());
+	return *this;
+    }
+
+    PassFailStreamBase &operator<<(std::ios_base &(*f)(std::ios_base &))
+    {
+	f(ost());
+	return *this;
+    }
+
+    PassFailStreamBase &operator<<(std::basic_ios<Ch,Tr>
+				   &(*f)(std::basic_ios<Ch,Tr> &))
+    {
+	f(ost());
+	return *this;
+    }
+
+    // ACCESSORS
+
+  protected:
+
+    // PROTECTED MANIPULATORS
+
+    std::ostringstream &ost() { return ost_m; }
+   
+  private:
+    
+    // IMPLEMENTATION
+};
+
+class PassFailStream : public PassFailStreamBase<char, std::char_traits<char> >
 {
 
     // NESTED CLASSES AND TYPEDEFS
@@ -38,48 +108,39 @@ class PassFailStream
 
     TestApp &testApp;
     const bool stateToSet;
-    std::ostringstream ost;
-    
+
   public:
 
     // CREATORS
     
     PassFailStream(TestApp &testApp_in, const std::string &str,
 		   bool stateToSet_in)
-	: testApp(testApp_in), stateToSet(stateToSet_in)
+	: PassFailStreamBase<char, std::char_traits<char> >(),
+	  testApp(testApp_in), stateToSet(stateToSet_in)
     {
-	ost << "**** " << testApp.name();
+	ost() << "**** " << testApp.name();
 	if (str.size() != 0)
 	{
-	    ost << "(" << str << ")";
+	    ost() << "(" << str << ")";
 	}
-	ost << " Test: "
+	ost() << " Test: "
 	    << (stateToSet ? "PASSED" : "FAILED")
 	    << ": ****\n\t";
 
 	if (stateToSet == false)
 	    testApp.setPassed(false);
     }
-    
+
     //Defaulted: PassFailStream(const PassFailStream &rhs);
 
     ~PassFailStream()
     {
-	testApp.addMessage(ost.str());
+	testApp.addMessage(ost().str());
     }
-
+    
     // MANIPULATORS
     
     //Defaulted: PassFailStream& operator=(const PassFailStream &rhs);
-
-    template<class T>
-    PassFailStream &operator<<(const T& val)
-    {
-	ost << val;
-	return *this;
-    }
-
-    // ACCESSORS
 
   private:
     
