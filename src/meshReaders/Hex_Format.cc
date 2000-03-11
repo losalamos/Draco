9f879bfa12c022255e1829a7a9a250e6ac3615b4
mcghee <mcghee@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2000-03-11 01:34:29 +0000

    Added Doxygen comments for Mesh_Reader base class. Corrected several bugs in Hex_Format reader.

--- src/meshReaders/Hex_Format.cc
+++ src/meshReaders/Hex_Format.cc
@@ -37,13 +37,24 @@ Hex_Format::Hex_Format(std::string filename)
     meshfile >> npoints >> ncells >> nvrtx >> nvrpf >> ndim >> nvb_faces
 	     >> nrb_faces >> nmat;
 
+    std::cout << "npoints= " << npoints << std::endl;
+    std::cout << "ncells= " << ncells << std::endl;
+    std::cout << "nvrtx= " << nvrtx << std::endl;
+    std::cout << "nvrpf= " << nvrpf << std::endl;
+    std::cout << "ndim= " << ndim  << std::endl;
+    std::cout << "nvb_faces= " << nvb_faces << std::endl;
+    std::cout << "nrb_faces= " <<  nrb_faces << std::endl;
+    std::cout << "nmat= " <<  nmat << std::endl;
+
     // Read the point coordinates data.
     point_coords.resize(npoints);
+    std::cout << "Capacity of Point Coords: " << point_coords.capacity() << std::endl;
+    std::cout << "Size of Point Coords: " << point_coords.size() << std::endl;
     for (int i=0; i<npoints; i++)
     {
 	point_coords[i].resize(ndim);
 	if (ndim == 1)
-	    meshfile >> point_coords[i][0]; 
+	    meshfile >> point_coords[i][0];
 	else if (ndim == 2)
 	    meshfile >> point_coords[i][0] >> point_coords[i][1];
 	else if (ndim == 3)
@@ -53,44 +64,73 @@ Hex_Format::Hex_Format(std::string filename)
 	    Insist(false,"Dimension index out of range!");  
     }
 
+    std::cout << "Read coordinates." << std::endl;
+
     // Read in the mesh connectivity.
     ipar.resize(ncells);
     for (int i=0; i<ncells; i++)
     {
 	ipar[i].resize(nvrtx);
-	meshfile >> ipar[i][0] >> ipar[i][1] >> ipar[i][2] >> ipar[i][3]
-		 >> ipar[i][4] >> ipar[i][5] >> ipar[i][6] >> ipar[i][7];
+	if (ndim == 1) 
+	    meshfile >> ipar[i][0] >> ipar[i][1];
+	else if (ndim ==2)
+	    meshfile >> ipar[i][0] >> ipar[i][1] >> ipar[i][2] >> ipar[i][3];
+	else if (ndim ==3)
+	    meshfile >> ipar[i][0] >> ipar[i][1] >> ipar[i][2] >> ipar[i][3]
+		     >> ipar[i][4] >> ipar[i][5] >> ipar[i][6] >> ipar[i][7];
+	else
+	    Insist(false,"Dimension index out of range!");
 	for (int j=0; j<nvrtx; j++)
-		   ipar[i][j] = ipar[i][j]-1;
+	    ipar[i][j] = ipar[i][j]-1;
     }
-    
+    std::cout << "Read elements." << std::endl;
+
     // Read in the mesh interior-region data.
     imat_index.resize(ncells);
     for (int i=0; i<ncells; i++)
 	meshfile >> imat_index[i];
-    
+    std::cout << "Read material flag." << std::endl;
+
     // Read in the mesh vacuum boundary data.
     ipar_vb.resize(nvb_faces);
     irgn_vb_index.resize(nvb_faces);
     for (int i=0; i<nvb_faces; i++)
     {
 	ipar_vb[i].resize(nvrpf);
-	meshfile >> ipar_vb[i][0] >> ipar_vb[i][1] >>ipar_vb[i][2] >>
-	    ipar_vb[i][3] >> irgn_vb_index[i];
+	if (ndim == 1)
+	    meshfile >> ipar_vb[i][0] >> irgn_vb_index[i];
+	else if (ndim ==2)
+	    meshfile >> ipar_vb[i][0] >> ipar_vb[i][1] >> irgn_vb_index[i];
+	else if (ndim ==3)
+	    meshfile >> ipar_vb[i][0] >> ipar_vb[i][1] >>ipar_vb[i][2] >>
+		ipar_vb[i][3] >> irgn_vb_index[i];
+	else
+	    Insist(false,"Dimension index out of range!");
 	for (int j=0; j<nvrpf; j++) 
 	    ipar_vb[i][j] = ipar_vb[i][j]-1;
     }
+    std::cout << "Read vacuum boundary data." << std::endl;
 
-    // Read in the mesh reflective boudnary data.
+    // Read in the mesh reflective boundary data.
     ipar_rb.resize(nrb_faces);
     for (int i=0; i<nrb_faces; i++)
     {
 	ipar_rb[i].resize(nvrpf);
-	meshfile >> ipar_rb[i][0] >> ipar_rb[i][1] >>ipar_rb[i][2] >>
-	    ipar_vb[i][3] ;
+	if (ndim ==1)
+	    meshfile >> ipar_rb[i][0];
+	else if(ndim ==2)
+	    meshfile >> ipar_rb[i][0] >> ipar_rb[i][1];
+	else if (ndim == 3)
+	    meshfile >> ipar_rb[i][0] >> ipar_rb[i][1] >>ipar_rb[i][2] >>
+		ipar_vb[i][3];
+	else
+	    Insist(false,"Dimension index out of range!");
 	for (int j=0; j<nvrpf; j++) 
-	    ipar_vb[i][j] = ipar_vb[i][j]-1;
+	    ipar_rb[i][j] = ipar_rb[i][j]-1;
     }
+    std::cout << "Read reflective boundary data." << std::endl;
+
+    Ensure (invariant());
 }
 
 std::vector<std::vector<int> > Hex_Format::get_element_nodes() const
--- src/meshReaders/Mesh_Reader.hh
+++ src/meshReaders/Mesh_Reader.hh
@@ -32,6 +32,17 @@ namespace rtt_meshReaders
 // 
 //===========================================================================//
 
+/*!
+ * \brief Base class for the RTT mesh readers. 
+ *
+ * This class provides the template from which all other
+ * mesh readers inherit. It provides a standard intefrace
+ * to mesh information for all clients. The interface
+ * supports structured and unstructured meshes. Both
+ * so called "AMR" or "hanging-node" meshes and C0 connectivity
+ * meshes can be described.
+ *
+ */
 class Mesh_Reader 
 {
 
@@ -58,14 +69,86 @@ class Mesh_Reader
 
     // ACCESSORS
 
+    /*!
+     * \brief Provides node coordinates for all the nodes in the
+     * mesh.
+     *
+     * Not all the nodes returned herein have to be used to define 
+     * a mesh element. For example some nodes may be children, dudded,
+     * or tracer nodes. The operator [i][j] returns
+     * node i, coordinate j.
+     *
+     */
     virtual std::vector<std::vector<double> > get_node_coords() const = 0;
+
+    /*!
+     * \brief Provides the units (inches, feet, cm, meters, etc.) of the
+     *        node coordinates. 
+     */
     virtual std::string get_node_coord_units() const = 0;
+
+    /*!
+     * \brief Returns node numbers of the mesh elements.
+     *
+     * Node numbers are 0 based and refer to the nodes 
+     * returned by the Mesh_Reader.get_node_coords() method. This
+     * information determines the connectivity of the mesh. It
+     * uniquely defines all the elements in the mesh, and their
+     * relationship to each other. Any mix of element types is
+     * allowed including multple dimensions. The elements
+     * may be spatially disjoint. The operator [i][j] returns
+     * cell i, node j.
+     *
+     */
     virtual std::vector<std::vector<int> > get_element_nodes() const = 0;
+
+    /*!
+     * \brief Returns the type of all the elements in the mesh. 
+     *
+     */
     virtual std::vector<Element_Definition::Element_Type> get_element_types() 
         const = 0;
+
+    /*!
+     * \brief Returns node sub-sets.
+     *
+     * Returned node numbers are 0 based and refer to the nodes 
+     * returned by the Mesh_Reader.get_node_coords() method.
+     * This method provides a capability to flag certain sub-sets of the
+     * nodes returned by the Mesh_Reader.get_node_coords() method. This
+     * can be useful for flagging certain nodes or sets of nodes for
+     * some special treatement such as edits or sources. The string
+     * key to the map provides a unique and hopefully descriptive name
+     * for each node sub-set.
+     */
     virtual std::map<std::string, std::set<int> > get_node_sets() const = 0;
-    virtual std::map<std::string, std::set<int> > get_element_sets() const = 0;
+
+    /*!
+     * \brief Returns element sub-sets.
+     *
+     * Returned element numbers are 0 based and refer to the elements
+     * returned by the Mesh_Reader.get_element_nodes() method.
+     * This method provides a capability to flag certain sub-sets of the
+     * elements returned by the Mesh_Reader.get_element_nodes() method. This
+     * can be useful for flagging certain elements or sets of elements for
+     * some special treatement such as edits, sources, material assignments,
+     * boudndary conditions, etc. The string
+     * key to the map provides a unique and hopefully descriptive name
+     * for each element sub-set.
+     */
+    virtual std::map<std::string, std::set<int> > get_element_sets() const =
+    0;
+
+    /*!
+     * \brief Returns the title of the mesh.
+     *
+     */
     virtual std::string get_title() const = 0;
+
+    /*!
+     * \brief Provides a check on the integrity of the mesh data.
+     *
+     */
     virtual bool invariant() const = 0;
 
   private:
