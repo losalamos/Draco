bcee72a66bb5fde9328232a6c21bfdeec38dcf61
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2000-03-29 00:49:47 +0000

    *** empty log message ***

--- src/quadrature/test/tQuadrature.cc
+++ src/quadrature/test/tQuadrature.cc
@@ -15,21 +15,19 @@
 #include "UnitTestFrame/PassFailStream.hh"
 
 #include <iostream>
-using std::cout;
-using std::endl;
 #include <vector>
 #include <sstream>
+
+using rtt_dsxx::SP;
+using rtt_quadrature::QuadCreator;
+using rtt_quadrature::Quadrature;
 
 // Unit Test Frame Stuff
 //----------------------------------------
 namespace rtt_UnitTestFrame {
-// what does this do?
-    rtt_dsxx::SP<TestApp> TestApp::create( int &argc, char *argv[],
-				       std::ostream& os_in ) 
-	{
-	    using rtt_dsxx::SP;
+    SP<TestApp> TestApp::create( int &argc, char *argv[],
+				 std::ostream& os_in ) {
 	    using rtt_quadrature_test::tQuadrature;
-	    
 	    return SP<TestApp> ( new tQuadrature( argc, argv, os_in ));
     }
 } // end namespace rtt_UnitTestFrame
@@ -42,6 +40,8 @@ namespace rtt_quadrature_test {
 
 using std::vector;
 using std::string;
+using std::cout;
+using std::endl;
 
 tQuadrature::tQuadrature( int argc, char *argv[], std::ostream& os_in )
     : rtt_UnitTestFrame::TestApp( argc, argv, os_in )
@@ -64,11 +64,13 @@ string tQuadrature::runTest()
 {
     // double precesion values will be tested for correctness against this
     // tolerance. 
-    static const double TOL = 1.0e-10; 
+    const double TOL = 1.0e-10; 
 
     // create an object that is responsible for creating quadrature objects.
-    rtt_quadrature::QuadCreator QuadratureCreator;
-
+    QuadCreator QuadratureCreator;
+//     pass(" Instantiate QuadCreator") 
+// 	<< "Successfully instantiated a QuadCreator object." << endl;
+    
     // we will only look at S4 Sets in this test.
     int sn_order = 4;
 
@@ -85,14 +87,15 @@ string tQuadrature::runTest()
     // 0   GaussLeg   1D Gauss Legendre
     // 1   LevelSym   3D Level Symmetric
 
-    rtt_quadrature::QuadCreator::Qid
-	qid[nquads] = { rtt_quadrature::QuadCreator::GaussLeg,
-		   rtt_quadrature::QuadCreator::LevelSym  };
+    QuadCreator::Qid qid[nquads] = { QuadCreator::GaussLeg,
+				     QuadCreator::LevelSym  };
 
     // mu0 holds mu for the first direction for each quadrature set tested.
     double mu0[nquads] = { 0.8611363116,
-		      -0.350021174581541 };
+			  -0.350021174581541 };
     
+    SP<Quadrature> spQuad;
+
     // loop over quadrature types to be tested.
 
     for ( int ix = 0; ix < nquads; ++ix ) {
@@ -102,32 +105,32 @@ string tQuadrature::runTest()
 	    fail() << "Setting QuadCreator::Qid enumeration failed.";
 	    break;
 	} else {
-	    // Create the quadrature object.
-	    rtt_dsxx::SP<rtt_quadrature::Quadrature> quad 
-		= QuadratureCreator.QuadCreate( qid[ix], sn_order );
+	    // Instantiate the quadrature object.
+	    spQuad = QuadratureCreator.QuadCreate( qid[ix], sn_order ); 
+
 	    // print the name of the quadrature set that we are testing.
-	    cout << "\nTesting the "  << quad->name() 
+	    cout << "\nTesting the "  << spQuad->name() 
 		 << "Quadrature set." << endl;
-	    cout << "   Sn Order         = " << quad->getSnOrder() << endl;
-	    cout << "   Number of Angles = " << quad->getNumAngles() << endl;
+	    cout << "   Sn Order         = " << spQuad->getSnOrder() << endl;
+	    cout << "   Number of Angles = " << spQuad->getNumAngles() << endl;
 
 	    // If the object was constructed sucessfully then we continue
 	    // with the tests.
-	    if ( ! quad )
+	    if ( ! spQuad )
 		fail() << "QuadCreator failed to create a new quadrature set.";
 	    else {
 		// get the mu vector
-		vector<double> mu = quad->getMu();
+		vector<double> mu = spQuad->getMu();
 		// get the omega vector for direction m=1.
-		vector<double> omega_1 = quad->getOmega(1);
-		if ( mu.size() != quad->getNumAngles() )
+		vector<double> omega_1 = spQuad->getOmega(1);
+		if ( mu.size() != spQuad->getNumAngles() )
 		    fail() << "The direction vector has the wrong length.";
 		else if ( fabs( mu[0] + mu0[ix] ) >= TOL ) 
 		    fail() << "mu[0] has the wrong value."; 
 		else if ( fabs( mu[1] - omega_1[0] ) >= TOL )
 		    fail() << "mu[1] != omega_1[0].";
 		else {
-		    quad->display();
+		    spQuad->display();
 		    cout << endl << endl; // end of this quadrature type
 		}
 	    }
