064f3ff0811b439e3395c400d91fbf9f9db201d6
rsqrd <rsqrd@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2000-04-07 20:25:17 +0000

    Removed the reliance of pcg_DB.hh on .nml_pcg.hh to find its internal state variables.  I made them explicit instead.  I also added a constructor to initialize the variables by a way other than through nml.
    This does not remove the nml completely, just supplements it.

--- src/PCGDiffusionSolver/pcg_DB.hh
+++ src/PCGDiffusionSolver/pcg_DB.hh
@@ -29,16 +29,39 @@ class pcg_DB {
 
     rtt_dsxx::String name;
 
-// Pick up the namelist variables.
-
-#include ".nml_pcg.hh"
-
-// Pick up the auxiliary variables.
+    int itmeth;
+    int nout;
+    int levout;
+    int itsmax;
+    int ntest;
+    int iqside;
+    int iuinit;
+    int ns1;
+    int ns2;
+    int ickstg;
+    int iuexac;
+    int idot;
+    int istats;
+    double ctimer;
+    double rtimer;
+    double flopsr;
+    double zeta;
+    double alpha;
 
 // Methods.
 
   public:
     pcg_DB( rtt_dsxx::String _name ) : name(_name) {}
+
+    pcg_DB(int itmeth_in, int levout_in, int itsmax_in, double zeta_in,
+	   double alpha_in)
+	: itmeth(itmeth_in), nout(6), levout(levout_in), itsmax(itsmax_in),
+	  ntest(-1), iqside(0), iuinit(1), ns1(10), ns2(10), ickstg(-1),
+	  iuexac(0), idot(1), istats(0), ctimer(0.0), rtimer(0.0),
+	  flopsr(0.0), zeta(zeta_in), alpha(alpha_in), name("pcg_DB")
+    {
+	// empty
+    }
 
     rtt_dsxx::String Name() const { return name; }
 
