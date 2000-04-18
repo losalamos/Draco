6bc6bc605ca459f73b7d280b8027316e10da17f2
rsqrd <rsqrd@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2000-04-18 22:29:46 +0000

    New Release number.

--- src/mesh/Release.cc
+++ src/mesh/Release.cc
@@ -18,7 +18,7 @@ using std::string;
 // this library in the form mesh-#_#_# in pkg_version variable
 const string release()
 {
-    string pkg_release = "@(#)mesh-1_7_0";
+    string pkg_release = "@(#)mesh-1_8_0";
     return pkg_release;
 }
 
