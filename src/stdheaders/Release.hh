9d7eced1527e06d0c66577287444f24a227194e1
msqrd <msqrd@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2000-06-21 23:56:09 +0000

    new package addition - stdheaders

---
+++ src/stdheaders/Release.hh
@@ -0,0 +1,43 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   stdheaders/Release.hh
+ * \author Michelle L. Murillo
+ * \date   Wed Jun 21 17:46:55 2000
+ * \brief  Release function for the stdheaders library
+ */
+//---------------------------------------------------------------------------//
+// $Id$
+//---------------------------------------------------------------------------//
+
+#ifndef __stdheaders_Release_hh__
+#define __stdheaders_Release_hh__
+
+//===========================================================================//
+/*!
+ * \page stdheaders_overview Overview of the stdheaders package
+ * \version
+ * \author
+ * 
+ * 
+ */
+//===========================================================================//
+/*!
+ * \namespace rtt_stdheaders
+ *
+ * \brief Namespace that contains the stdheaders package classes and variables.
+ *
+ */
+//===========================================================================//
+
+#include <string>
+
+namespace rtt_stdheaders 
+{
+    const std::string release();
+}
+
+#endif                          // __stdheaders_Release_hh__
+
+//---------------------------------------------------------------------------//
+//                           end of stdheaders/Release.hh
+//---------------------------------------------------------------------------//
