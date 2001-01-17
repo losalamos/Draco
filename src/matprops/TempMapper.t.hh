9110f3f6dffa71067254a49af30b3f95c57fff6e
rsqrd <rsqrd@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2001-01-17 20:48:24 +0000

    Fixed TempMapper bug that resulted in boundary face temperatures being "averaged" with the non-existant opposite face's temperature.

--- src/matprops/TempMapper.t.hh
+++ src/matprops/TempMapper.t.hh
@@ -89,21 +89,47 @@ namespace rtt_matprops
      MT::fcdsf::const_iterator fTit = faceTempsAvg.begin();
      MT::fcdsf::iterator ait = alpha.begin();
 
+     // Set up a mask that will tell me where the boundary faces are.
+     // On these faces I will leave the temperatures unchanged, i.e.
+     // Set alpha = 1.0 for that face.
+
+     MT::fcdif boundaryFaces(fCtor);
+     
+     { // scoping
+
+         MT::bsif boundary(fCtor);
+         boundary = 1;
+         MT::gather(boundaryFaces, boundary, MT::OpAssign());
+         
+     } // scoping
+
+     MT::fcdif::const_iterator bit = boundaryFaces.begin();
+     
      while (aFTit != adjacentFaceTemp.end())
      {
-	 double Tavg = *fTit;
-	 double Thigh = std::max(*aFTit, *fTit);
-	 double Tlow = std::min(*aFTit, *fTit);
-
 	 double &alpha = *ait;
 
-	 using std::pow;
-	 alpha = gamma*pow(Thigh, power) + (1.0 - gamma)*pow(Tlow, power);
-	 alpha = pow(alpha, 1.0/power);
-	 alpha /= Tavg;
+         const bool onBoundary = (*bit == 1);
+
+         if (onBoundary)
+         {
+             alpha = 1.0;
+         }
+         else
+         {
+             double Tavg = *fTit;
+             double Thigh = std::max(*aFTit, *fTit);
+             double Tlow = std::min(*aFTit, *fTit);
+
+             using std::pow;
+             alpha = gamma*pow(Thigh, power) + (1.0 - gamma)*pow(Tlow, power);
+             alpha = pow(alpha, 1.0/power);
+             alpha /= Tavg;
+         }
 
 	 aFTit++;
 	 fTit++;
+         bit++;
 	 ait++;
      }
  }
