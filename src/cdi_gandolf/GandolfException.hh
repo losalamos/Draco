bf39da6a58f1205290c54e3cc747bf209ab1aaff
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2001-03-23 17:59:05 +0000

    o A complete rewrite of the GandolfException class.
    o New exception objects now inherit directly from std::exception and
      use standard interface calls (e.g. "exception.what()").
    
    o Users can now "catch" std::exceptions and still pick up exceptions
      thrown by this class.
    
    o If users "catch" GandolfException exceptions a detailed message will
      be available inlcuding a text error message correlating to integer
      error codes returned by the Gandolf library.

--- src/cdi_gandolf/GandolfException.hh
+++ src/cdi_gandolf/GandolfException.hh
@@ -13,210 +13,319 @@
 #define __cdi_gandolf_GandolfException_hh__
 
 #include <string>
-//#include <stdexcept>
-
-// I really should inherit from std::exception and use standard C++
-// exception symantecs (e.g. errorObj.what()).  However, I don't
-// really understand how to make exceptions work.  I need a better
-// reference to learn from. (-kt)
+#include <stdexcept>
 
 namespace rtt_cdi_gandolf
 {
  
-//===========================================================================//
-/*!
- * \class GandolfException
- *
- * \brief This class handles exceptions thrown by GandolfOpacity when
- *        it calls the Gandolf library functions.
- *
- * This class provides an exception data type.  When an exception is
- *     thrown in the cdi_gandolf package a GandolfException object is
- *     created and may be trapped in the calling routine by using a
- *     try/catch block when a GandolfFile or GandolfOpacity object is
- *     first created or when the client fetches an opacity value(s)
- *     by using one of GandolfOpacities accessor functions.  
- *
- *     There is one base exception class (GandolfException) and 5
- *     derived exception classes (gkeysException, gchgridsException,
- *     ggetgrayException, ggetmgException and gmatidsException).  
- *
- * \example cdi_gandolf/test/tGandolfFile.cc
- * \example cdi_gandolf/test/tCDIGandolf.cc
- * \example cdi_gandolf/test/tGandolfWithCDI.cc
- *
- * These two examples demonstrate how the try/catch blocks may be used 
- * to trap errors in the cdi_gandolf package.  They also demonstrate
- * how the calling program can extract information about the exception.
- *
- */
-//===========================================================================//
-
-class GandolfException //: public std::exception
-{
-  protected:    
-
+    //========================================================================
     /*!
-     * \brief The integer error code returned by the Gandolf function.
-     */
-    const int errorCode;
-    /*!
-     * \brief The name of the Gandolf function that generated the
-     *        error.
-     */
-    const std::string gandolfFunctionName;
-
-  public:
-
-    // CREATORS
-
-    /*!
-     * \brief Standard GandolfException constructor.
+     * \class GandolfException
      *
-     * \param gandolfFunctionName
-     * \param errorCode
-     */    
-    GandolfException( std::string gandolfFunctionName, int errorCode )
-	: gandolfFunctionName( gandolfFunctionName ), errorCode( errorCode )
+     * \brief This class handles exceptions thrown by GandolfOpacity when
+     *        it calls the Gandolf library functions.
+     *
+     * This class provides an Gandolf exception data object derived
+     * from std::exception.  Additionally, there are Gandolf exception
+     * objects (derived from GandolfException) for each function that
+     * Gandolf provides.  There is one base exception class
+     * (GandolfException) and 5 derived exception classes
+     * (gkeysException, gchgridsException, ggetgrayException,
+     * ggetmgException and gmatidsException).
+     *
+     * When an exception is thrown in the cdi_gandolf package a
+     * XXXException (see list above for what XXX might stand for)
+     * object is created and may be trapped in the calling routine by
+     * using a try/catch block when a GandolfFile or GandolfOpacity
+     * object is first created or when the client fetches an opacity
+     * value(s) by using one of GandolfOpacities accessor functions.
+     *
+     * \example cdi_gandolf/test/tGandolfFile.cc
+     * \example cdi_gandolf/test/tCDIGandolf.cc
+     * \example cdi_gandolf/test/tGandolfWithCDI.cc
+     *
+     * These examples demonstrate how the try/catch blocks may be used
+     * to trap errors in the cdi_gandolf package.  They also
+     * demonstrate how the calling program can extract information
+     * about the exception.
+     *  
+     */
+    //========================================================================
+
+    class GandolfException : public std::exception
     { 
-	// empty
+      private:
+
+	/*!
+	 * \brief The name of the Gandolf library function that
+	 *	 returned an error.
+	 */
+	const std::string gandolfFunctionName;
+
+	/*!
+	 * \brief A summary message that describes the error that was
+	 *	 caught.
+	 */
+	mutable char* message;
+	
+      protected:
+	
+	/*!
+	 * \brief The Gandolf error code that was returned from the
+	 *	 FORTRAN library.
+	 */
+ 	const int errorCode;
+	
+      public:
+
+	// CREATORS
+
+	/*!
+	 * \brief The standard GandolfExceptoin constuctor.
+	 *
+	 * When a specific error is thrown (i.e.: gmatidException),
+	 * the base class is also instantiated through this 
+	 * constructor.  This constructor, in turn, instantiates 
+	 * std::exception.
+	 *
+	 * The throw() at the end of these declarations are exception
+	 * specifiers.  I have not limited the types of exceptions
+	 * that may be caught by this object (hense the empty parens).
+	 * See Lippman and Lajoie, "C++ Primer" p. 1042ff for more
+	 * information.
+  	 *
+	 * \param what_arg A simple description of the exception.  In
+	 *        some cases this may simply be the name of the
+	 *        Gandolf function that failed.  In other cases it
+	 *        might contain a detailed description of the error.
+	 *        This string is used in the construction of
+	 *        std::exception.
+	 * \param gandolfFunctionName This optional argument is a
+	 *        string holds the name of the Gandolf function that
+	 *        failed.
+	 * \param errorCode This optional argument is the integer
+	 *        error code returned by Gandolf.  GandolfExcetion can
+	 *        translate this into a string message. 
+	 */
+	GandolfException( const std::string& what_arg,
+			  std::string gandolfFunctionName = "unknown",
+			  int errorCode = -99 ) throw();
+	
+	/*!
+	 * \brief The standard GandolfException destructor.
+	 *
+	 * \sa what()
+	 *
+	 * The destructor frees memory that may have been allocated by
+	 * the accessor what().  If what was never called then
+	 * "message" was never allocated and no deallocation is
+	 * required.
+	 */
+	virtual ~GandolfException() throw();
+	
+	// ACCESSORS
+
+	/*!
+	 * \brief GandolfException overrides the default
+	 *        std::exception.what() function so that a more
+	 *        detailed message may be returned. 
+	 * 
+	 * \sa The actual text message returned is generated by
+	 *     getErrorSummary(). 
+	 * \sa ~GandolfException(): Memory allocated by this routine
+	 *     is freed in the class destrcutor.
+	 *
+	 * This function returns the contents of the data member
+	 * "message".  If message is empty a call to errorSummary() is
+	 * made to fill "message" with descriptive text.  If required
+	 * memory is allocated for the data member "message".
+	 * Deallocation is completed in the destructor
+	 * ~GandolfException().
+	 */
+	virtual const char* what() const throw();
+
+	/*!
+	 * \brief Returns the numeric error code supplied by the
+	 *        Gandolf function. 
+	 */
+	int getErrorCode() const { return errorCode; };
+
+	/*!
+	 * \brief Returns a brief message that is correlated to the
+	 * 	  specific error code returned by Gandolf.
+	 *
+	 * This is a virtual function that is overridden by the
+	 * derived Gandolf exception classes.  Each of derived classes
+	 * can decode the numeric Gandolf error code into a short
+	 * descriptive string.
+	 */
+	virtual std::string getErrorMessage() const;
+
+	/*!
+	 * \brief Returns the name of the Gandolf function that
+	 *	  returned an error.
+	 */
+	const std::string& getGandolfFunctionName() const 
+	{ 
+	    return gandolfFunctionName; 
+	};
+
+	/*!
+	 * \brief Returns a detailed message containing the Gandolf
+	 *        function that failed, the actual error code returned
+	 *        and a brief description of the error.
+	 */
+	std::string errorSummary() const;
     };
 
+
+    //========================================================================
     /*!
-     * \brief Standard GandolfException destructor.
+     * \class gkeysException
      *
-     *     We require a virtual destructor to avoid potential memory
-     *     leaks that may occur when derived objects are destroyed
-     *     without first destroying this parent object.
+     * \brief Derived from GandolfException this class encapsulates
+     *        errors returned by the Gandolf Function gkeys().
+     * 
+     * \sa The class description for GandolfException for additional
+     *     comments.
+     *
+     * There are two possible constructors.  One simply takes a user
+     * supplied error message.  The second takes the integer supplied
+     * Gandolf error code and looks up the associated error message.
+     *
+     * getErrorMessage() overrides the base class and provides the
+     * ability to lookup an integer error code and find its associated
+     * error message.
      */
-    ~GandolfException() {};
+    //========================================================================
 
-    // ACCESSORS
+    class gkeysException : public GandolfException
+    {
+      public:
+	
+	// CREATORS
 
-    /*!
-     * \brief Accessor to return the name of the Gandolf function
-     *        where the error was generated.
-     */
-    std::string getGandolfFunctionName() const
-    { 
-	return gandolfFunctionName;
+	gkeysException( const std::string& what_arg );
+	gkeysException( int gkeysError );
+	~gkeysException() throw();
+	virtual std::string getErrorMessage() const;
     };
-    
+       
+   //========================================================================
     /*!
-     * \brief Accessor to return the integer error code generated by
-     *        the Gandolf function.
+     * \class gchgridsException
+     *
+     * \brief Derived from GandolfException this class encapsulates
+     *        errors returned by the Gandolf Function gchgrids().
+     * 
+     * \sa The class description for GandolfException for additional
+     *     comments.
+     *
+     * There are two possible constructors.  One simply takes a user
+     * supplied error message.  The second takes the integer supplied
+     * Gandolf error code and looks up the associated error message.
+     *
+     * getErrorMessage() overrides the base class and provides the
+     * ability to lookup an integer error code and find its associated
+     * error message.
      */
-    int getErrorCode() const { return errorCode; }; 
+    //========================================================================
 
+    class gchgridsException : public GandolfException
+    {
+      public:
+	gchgridsException( const std::string& what_arg );
+	gchgridsException( int gchgridsError );
+	~gchgridsException() throw();
+	virtual std::string getErrorMessage() const;
+    };
+
+    //========================================================================
     /*!
-     * \brief This function returns a string that describes this
-     *        exception.
+     * \class ggetgrayException
+     *
+     * \brief Derived from GandolfException this class encapsulates
+     *        errors returned by the Gandolf Function ggetgray().
+     * 
+     * \sa The class description for GandolfException for additional
+     *     comments.
+     *
+     * There are two possible constructors.  One simply takes a user
+     * supplied error message.  The second takes the integer supplied
+     * Gandolf error code and looks up the associated error message.
+     *
+     * getErrorMessage() overrides the base class and provides the
+     * ability to lookup an integer error code and find its associated
+     * error message.
      */
-    std::string errorMessage() const ;
+    //========================================================================
 
+    class ggetgrayException : public GandolfException
+    {
+      public:
+	ggetgrayException( const std::string& what_arg );
+	ggetgrayException( int ggetgrayError );
+	~ggetgrayException() throw ();
+	virtual std::string getErrorMessage() const;
+    };
+
+    //========================================================================
     /*!
-     * \brief This function returns a short paragraph that summarizes
-     *         all of the known error information associated with this 
-     *         exception.
+     * \class ggetmgException
+     *
+     * \brief Derived from GandolfException this class encapsulates
+     *        errors returned by the Gandolf Function ggetmg().
+     * 
+     * \sa The class description for GandolfException for additional
+     *     comments.
+     *
+     * There are two possible constructors.  One simply takes a user
+     * supplied error message.  The second takes the integer supplied
+     * Gandolf error code and looks up the associated error message.
+     *
+     * getErrorMessage() overrides the base class and provides the
+     * ability to lookup an integer error code and find its associated
+     * error message.
      */
-    std::string errorSummary() const;
+    //========================================================================
 
-};
+     class ggetmgException : public GandolfException
+    {
+      public:
+	ggetmgException( const std::string& what_arg );
+	ggetmgException( int ggetmgError );
+	~ggetmgException() throw();
+	virtual std::string getErrorMessage() const;
+    };
 
-// --------------------- //
-// gkeys Exception Class //
-// --------------------- //
+    //========================================================================
+    /*!
+     * \class gmatidsException
+     *
+     * \brief Derived from GandolfException this class encapsulates
+     *        errors returned by the Gandolf Function gmatids().
+     * 
+     * \sa The class description for GandolfException for additional
+     *     comments.
+     *
+     * There are two possible constructors.  One simply takes a user
+     * supplied error message.  The second takes the integer supplied
+     * Gandolf error code and looks up the associated error message.
+     *
+     * getErrorMessage() overrides the base class and provides the
+     * ability to lookup an integer error code and find its associated
+     * error message.
+     */
+    //========================================================================
 
-class gkeysException : public GandolfException
-{
-
-  public:
-
-    // CONSTRUCTOR
-
-    gkeysException( int errorCode );
-    
-    // ACCESSORS
-
-    std::string errorMessage() const;
-    
-};
-
-// ------------------------ //
-// gchgrids Exception Class //
-// ------------------------ //
-
-class gchgridsException : public GandolfException
-{
-
-  public:
-
-    // CONSTRUCTOR
-
-    gchgridsException( int errorCode );
-    
-    // ACCESSORS
-
-    std::string errorMessage() const;
-    
-};
-
-// ------------------------ //
-// ggetgray Exception Class //
-// ------------------------ //
-
-class ggetgrayException : public GandolfException
-{
-
-  public:
-
-    // CONSTRUCTOR
-
-    ggetgrayException( int errorCode );
-    
-    // ACCESSORS
-
-    std::string errorMessage() const;
-    
-};
-
-// ---------------------- //
-// ggetmg Exception Class //
-// ---------------------- //
-
-class ggetmgException : public GandolfException
-{
-
-  public:
-
-    // CONSTRUCTOR
-
-    ggetmgException( int errorCode );
-    
-    // ACCESSORS
-
-    std::string errorMessage() const;
-    
-};
-
-// ----------------------- //
-// gmatids Exception Class //
-// ----------------------- //
-
-class gmatidsException : public GandolfException
-{
-
-  public:
-
-    // CONSTRUCTOR
-
-    gmatidsException( int errorCode );
-    
-    // ACCESSORS
-
-    std::string errorMessage() const;
-    
-};
+    class gmatidsException : public GandolfException
+    {
+      public:
+	gmatidsException( const std::string& what_arg );
+	gmatidsException( int gmatidsError );
+	~gmatidsException() throw();
+	virtual std::string getErrorMessage() const;
+    };
 
 } // end namespace rtt_cdi_gandolf
 
