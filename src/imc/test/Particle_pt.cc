633f2e05e336d31b58cf4845ffc6660e8d4efb74
mwbuksas <mwbuksas@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2001-06-14 21:20:17 +0000

    Restored version 1.1

--- src/imc/test/Particle_pt.cc
+++ src/imc/test/Particle_pt.cc
@@ -1,5 +1,5 @@
 //----------------------------------*-C++-*----------------------------------//
-/*!
+/*! 
  * \file   imc/test/Particle_pt.cc
  * \author Thomas M. Evans
  * \date   Fri Aug 18 16:14:04 2000
@@ -12,25 +12,20 @@
 #include "mc/OS_Mesh.hh"
 #include "rng/Rnd_Control.hh"
 #include "../Particle.t.hh"
-#include "../Analog_Particle.t.hh"
 #include "../Particle_Buffer.t.hh"
 
 namespace rtt_imc
 {
 
-typedef rtt_mc::OS_Mesh              MT;
-typedef rtt_imc::Particle<MT>        PT;
-typedef rtt_imc::Analog_Particle<MT> APT;
-typedef rtt_rng::Rnd_Control         RC; 
+typedef rtt_mc::OS_Mesh       MT;
+typedef rtt_imc::Particle<MT> PT;
+typedef rtt_rng::Rnd_Control  RC;
 
 template class Particle<MT>;
-template class Analog_Particle<MT>;
 
 template class Particle_Buffer<PT>;
-template class Particle_Buffer<APT>;
 
 template Particle_Buffer<PT>::Particle_Buffer(const MT &, const RC &);
-template Particle_Buffer<APT>::Particle_Buffer(const MT &, const RC &);
 
 } // end namespace rtt_imc
 
