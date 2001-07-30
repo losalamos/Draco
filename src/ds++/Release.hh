0a78a321d15a9907a8553d11712ef4573b2b7d8e
tme <tme@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2001-07-30 20:33:39 +0000

    Comment change to Release.

--- src/ds++/Release.hh
+++ src/ds++/Release.hh
@@ -1,8 +1,8 @@
 //----------------------------------*-C++-*----------------------------------//
 /*! 
- *  \file ds++/Release.hh
+ *  \file   ds++/Release.hh
  *  \author Randy M. Roberts
- *  \date Thu Jul 15 09:31:44 1999
+ *  \date   Thu Jul 15 09:31:44 1999
  *  \brief  Header file for ds++ library release function.
  */
 //===========================================================================//
@@ -12,39 +12,37 @@
 #ifndef __ds_Release_hh__
 #define __ds_Release_hh__
 
-//===========================================================================//
-
 #include <string>
-/*!
- *
- * \brief RTT "Data Structures in C++" (DS++)  namespace.
- *
- */
-namespace rtt_dsxx 
-{
-/*!
- * \brief  Gets the release number for the ds++ package. 
- * \return release number as a string in the form "ds-\#_\#_\#".
- * 
- * This can be used to get exact version information in codes that 
- * use ds++.
- *
- */
-    const std::string release();
-}
 
-#endif                          // __ds_Release_hh__
-
+//===========================================================================//
 /*!
  * \page ds++-overview Overview of the Data Structures in C++ (DS++) Package
  *
- * \version 1_0_0
+ * \version 1_4_0
  *
  * This package provides data structures and other miscellaneous support
  * for the Draco system. Classes for matrices, design-by-contract, 
  * smart-pointers, and a variety of other utilities are included.
  *
  */
+//===========================================================================//
+/*!
+ * \namespace rtt_dsxx
+ *
+ * \brief RTT "Data Structures in C++" (DS++)  namespace.
+ *
+ */
+//===========================================================================//
+
+namespace rtt_dsxx 
+{
+
+//! Query package for the release number.
+const std::string release();
+
+} // end of rtt_ds++
+
+#endif                          // __ds_Release_hh__
 
 //---------------------------------------------------------------------------//
 //                              end of ds++/Release.hh
