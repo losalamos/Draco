46b57f304d6efbb4a33372c4bb2e1e5f2eb51f3c
tmonster <tmonster@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2001-09-05 17:26:28 +0000

    Added scattering tests to check the robustness of the normalization of the direction cosines. ========================================================================== Made checks consistent on magnitude and magnitude^2 of the direction cosines.  The epsilon on soft_equiv to unity is 1.0e-10 and 1.0e-5 for magnitude and magnitude^2, respectively, since magnitude^2 = omega-dot-omega is farther away from unity.
    Made corresponding changes in the Mesh Types.

--- src/mc/test/tstCoord.cc
+++ src/mc/test/tstCoord.cc
@@ -27,6 +27,7 @@ using rtt_mc::Coord_sys;
 using rtt_mc::XYCoord_sys;
 using rtt_mc::XYZCoord_sys; 
 using rtt_mc::global::soft_equiv;
+using rtt_mc::global::dot;
 
 using rtt_dsxx::SP;
 
@@ -91,6 +92,100 @@ void testXYZ(Coord_sys &xyz)
 
 //---------------------------------------------------------------------------//
 
+// test the direction cosines from initial sampling through several
+// scatters. 
+
+void test_scatters(Coord_sys &xyz)
+{
+    // direction vector
+    vector<double> direction(3, 0.0);
+
+    // 
+    // test scatter back and forth along the x-axis
+    //
+
+    direction[0] = 1.0;
+    direction[1] = 0.0;
+    direction[2] = 0.0;
+
+    for (int i = 0; i < 1000; i++)
+    {
+	xyz.calc_omega(-1.0, 0.0, direction);
+	
+	if (!soft_equiv(direction[0], pow(-1.0,i+1), 1.e-10)) ITFAILS;
+	if (!soft_equiv(direction[1],           0.0, 1.e-10)) ITFAILS;
+	if (!soft_equiv(direction[2],           0.0, 1.e-10)) ITFAILS;
+
+	if (!soft_equiv(sqrt(dot(direction,direction)),1.0,1.e-10)) ITFAILS; 
+    }
+
+    // 
+    // test scatter back and forth along the y-axis
+    //
+
+    direction[0] = 0.0;
+    direction[1] = 1.0;
+    direction[2] = 0.0;
+
+    for (int i = 0; i < 1000; i++)
+    {
+	xyz.calc_omega(-1.0, 0.0, direction);
+	
+	if (!soft_equiv(direction[0],           0.0, 1.e-10)) ITFAILS;
+	if (!soft_equiv(direction[1], pow(-1.0,i+1), 1.e-10)) ITFAILS;
+	if (!soft_equiv(direction[2],           0.0, 1.e-10)) ITFAILS;
+
+	if (!soft_equiv(sqrt(dot(direction,direction)),1.0,1.e-10)) ITFAILS; 
+    }
+
+    // 
+    // test scatter back and forth along the z-axis
+    //
+
+    direction[0] = 0.0;
+    direction[1] = 0.0;
+    direction[2] = 1.0;
+
+    for (int i = 0; i < 1000; i++)
+    {
+	xyz.calc_omega(-1.0, 0.0, direction);
+	
+	if (!soft_equiv(direction[0],           0.0, 1.e-10)) ITFAILS;
+	if (!soft_equiv(direction[1],           0.0, 1.e-10)) ITFAILS;
+	if (!soft_equiv(direction[2], pow(-1.0,i+1), 1.e-10)) ITFAILS;
+
+	if (!soft_equiv(sqrt(dot(direction,direction)),1.0,1.e-10)) ITFAILS; 
+    }
+
+    //
+    // begin with an isotropic direction and scatter many times (robustness) 
+    //
+
+    // make a Sprng random number object
+    Sprng x = rndc.get_rn(10);
+
+    // get an initial direction
+    direction = xyz.sample_dir("isotropic", x);
+
+    // make several scatters through a constant angle
+    for (int i = 0; i < 1000; i++)
+	xyz.calc_omega(0.5,0.75,direction);
+
+    //
+    // begin anew along the z-axis, and scatter many times (robustness)
+    //
+
+    direction[0] = 0.0;
+    direction[1] = 0.0;
+    direction[2] = 1.0;
+
+    // make several scatters through a constant angle
+    for (int i = 0; i < 1000; i++)
+	xyz.calc_omega(0.5,0.75,direction);
+}
+
+//---------------------------------------------------------------------------//
+
 int main(int argc, char *argv[])
 {  
     C4::Init(argc, argv);
@@ -111,22 +206,35 @@ int main(int argc, char *argv[])
 	    return 0;
 	}
 
-    // fill up the random number array
-    make_ran();
-
-    // make some Coord_sys stuff
-    XYCoord_sys xy;
-    XYZCoord_sys xyz;
-    SP<XYCoord_sys> sxy(new XYCoord_sys());
-    SP<XYZCoord_sys> sxyz(new XYZCoord_sys());
-
-    // test XY
-    testXY(xy);
-    testXY(*sxy);
-
-    // test XYZ
-    testXYZ(xyz);
-    testXYZ(*sxyz);
+    try
+    {
+	// fill up the random number array
+	make_ran();
+	
+	// make some Coord_sys stuff
+	XYCoord_sys xy;
+	XYZCoord_sys xyz;
+	SP<XYCoord_sys> sxy(new XYCoord_sys());
+	SP<XYZCoord_sys> sxyz(new XYZCoord_sys());
+	
+	// test XY
+	testXY(xy);
+	testXY(*sxy);
+	
+	// test XYZ
+	testXYZ(xyz);
+	testXYZ(*sxyz);
+	
+	// test direction cosines with scattering
+	test_scatters(xyz);
+    }
+    catch (rtt_dsxx::assertion &ass)
+    {
+        std::cout << "Test: assertion failure at line "
+		  << ass.what() << std::endl;
+        C4::Finalize();
+        return 1;
+    }
 
     // status of test
     cout << endl;
