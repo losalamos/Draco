79471d2a972c2e0ac983e43a9c1ac2396fd5292f
tme <tme@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2001-10-17 22:33:42 +0000

    pcgWrap package.  This was formally linalg.  R. Lowrie cleaned it up some: added rtt_pcgWrap namespace, got rid of some code.  This is a temporary solver package because (A) pcg is no longer supported, and (B) the package is a bloody mess.  We will be developing a new set of linear solver interfaces in the near future.
    QED

---
+++ src/pcgWrap/Release.hh
@@ -0,0 +1,56 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   pcgWrap/Release.hh
+ * \author Rob Lowrie and Tom Evans
+ * \date   Thu Jul 15 09:44:12 1999
+ * \brief  Release function for pcgWrap library.
+ */
+//---------------------------------------------------------------------------//
+// $Id$
+//---------------------------------------------------------------------------//
+
+#ifndef __pcgWrap_Release_hh__
+#define __pcgWrap_Release_hh__
+
+//===========================================================================//
+/*!
+ * \page pcgWrap_overview Overview of the pcgWrap package
+ * \version 1_0_0
+ * \author Rob Lowrie and Dave Nystrom
+ *
+ * pcgWrap is a rather messy interface to the Preconditioned Conjugate
+ * Gradient library (pcg).  Because the pcg package is no longer actively
+ * maintained, pcgWrap is an essentially "dead" package.  It will be
+ * superceded by interfaces to the UPS linear solver packages.
+ *
+ * Needless to say, to use pcgWrap, libpcg must be linked.  This can be added
+ * for testing purposes during configure with the --with-pcg and
+ * --with-pcg-lib options.  Also, pcg lib depends on lapack, for non-default
+ * behavior use the --with-lapack and --with-lapack-lib options to
+ * configure. 
+ *
+ * Because this package does not have long for this world it is not
+ * extensively documented.  For guidance in its use (if you need it
+ * temporarily) see R. Lowrie <lowrie@lanl.gov>.
+ *
+ */
+//===========================================================================//
+/*!
+ * \namespace rtt_pcgWrap
+ *
+ * \brief Namespace that contains the pcgWrap package classes and variables.
+ */
+//===========================================================================//
+
+#include <string>
+
+namespace rtt_pcgWrap
+{
+    const std::string release();
+}
+
+#endif                          // __pcgWrap_Release_hh__
+
+//---------------------------------------------------------------------------//
+//                              end of pcgWrap/Release.hh
+//---------------------------------------------------------------------------//
