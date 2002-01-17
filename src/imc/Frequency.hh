92cf6f77ef63276e3ae4df7d89ce764bf276574c
tmonster <tmonster@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2002-01-17 01:25:50 +0000

    Replaced Frequency with Gray_Frequency and Multigroup_Frequency, types on which we will template.

--- src/imc/Frequency.hh
+++ src/imc/Frequency.hh
@@ -3,7 +3,7 @@
  * \file   imc/Frequency.hh
  * \author Todd J. Urbatsch
  * \date   Thu Jan 10 14:05:09 2002
- * \brief  Frequency class definition.
+ * \brief  Gray_Frequency and Multigroup_Frequency class definitions.
  */
 //---------------------------------------------------------------------------//
 // $Id$
@@ -21,18 +21,20 @@ namespace rtt_imc
  
 //===========================================================================//
 /*!
- * \class Frequency
+ * \class Multigroup_Frequency 
  *
- * \brief The Frequency class 
+ * \brief The Multigroup_Frequency class type.
  *
  */
 // revision history:
 // -----------------
 // 0) 10 Jan 2002 : original
+// 1) 16 Jan 2002 : replaced Frequency with Gray_Frequency and
+//                  Multigroup_Frequency, types on which we will template.
 // 
 //===========================================================================//
 
-class Frequency 
+class Multigroup_Frequency 
 {
 
   public:
@@ -43,34 +45,30 @@ class Frequency
 
   private:
 
-    // Boolean indicator for gray frequency treatment.
-    bool gray;
-
     // Frequency group boundaries.
     sf_double group_boundaries;
 
   public:
 
     // Constructor.
-    explicit Frequency(const sf_double & grp_bnds = sf_double())
+    explicit Multigroup_Frequency(const sf_double & grp_bnds = sf_double())
 	: group_boundaries(grp_bnds)
     {
-	// Set the boolean gray indicator.
-	gray = (group_boundaries.size() == 0);
+	// Require at least one group
+	Require (group_boundaries.size() > 1);
 
 	// Loose check on positivity/monotonicity of mg group boundaries. 
-	Require (gray?true:
-		 group_boundaries.front() < group_boundaries.back());
-	Require (gray?true:group_boundaries.front() >= 0.0);
+	Require (group_boundaries.front() <  group_boundaries.back());
+	Require (group_boundaries.front() >= 0.0);
     }
 
     // >>> Accessors.
     
     //! Query whether frequency is gray.
-    bool is_gray() const { return gray; }
+    bool is_gray() const { return false; }
 
     //! Query whether frequency is multigroup.
-    bool is_multigroup() const { return !gray; }
+    bool is_multigroup() const { return true; }
 
     //! Get number of frequency groups.
     int get_num_groups() const { return group_boundaries.size()-1; }
@@ -86,7 +84,7 @@ class Frequency
     pair_doubles get_group_boundaries(const int group) const
     {
 	// Require multigroup.
-	Check (!gray);
+	Check (group_boundaries.size() > 1);
 
 	// Require a valid group number on input.
 	Check (group > 0);
@@ -100,6 +98,133 @@ class Frequency
 			    group_boundaries[group]); 
     }
 
+    //! Find a frequency group, in [1,G], given a frequency [keV]. 
+    int find_group_given_a_freq(const double hnu) const
+    {
+	// The freqency must be multigroup.
+	Require (group_boundaries.size() > 1);
+
+	// Get the number of groups.
+	int num_grps = group_boundaries.size() - 1;
+	Require (num_grps > 0);
+
+	// Return error if hnu is outside group boundaries.
+	if (hnu < group_boundaries[0] || hnu > group_boundaries[num_grps])
+	    return -1;
+
+	// Binary search on groups. The search is successful when either 
+	// 1) the frequency is within the group bounds or 
+	// 2) the low and high search bounds are the same.
+	int grp_lo  = 1;
+	int grp_hi  = num_grps;
+	int grp_try = 0.5 * (grp_lo + grp_hi);
+
+	while (grp_lo != grp_hi)
+	{
+	    // check validity of group index
+	    Check (grp_try > 0 && grp_try <= num_grps);
+
+	    // move up low search bound if frequency is above this group
+	    // and split the new difference to get next estimate.
+	    if (hnu > group_boundaries[grp_try])
+	    {
+		grp_lo  = grp_try + 1;
+		grp_try = 0.5 * (grp_lo + grp_hi);
+	    }
+
+	    // move down high search bound if frequency is below this group
+	    // and split the new difference to get next estimate.
+	    else if (hnu < group_boundaries[grp_try-1])
+	    {
+		grp_hi  = grp_try - 1;
+		grp_try = 0.5 * (grp_lo + grp_hi);
+	    }
+
+	    // otherwise, we have the correct group; return
+	    else
+		return grp_try;
+	}
+
+	// Check consistency of search params.
+	Check (grp_lo == grp_hi && grp_try == grp_hi);
+	Check (grp_try > 0      && grp_try <= num_grps);
+
+	// check that the group actually contains the frequency.
+	Check (hnu >= get_group_boundaries(grp_try).first);
+	Check (hnu <= get_group_boundaries(grp_try).second);
+
+	// return the group number (in [1,G])
+	return grp_try;
+    }
+};
+ 
+//===========================================================================//
+/*!
+ * \class Gray_Frequency 
+ *
+ * \brief The Gray_Frequency class type.
+ *
+ */
+// revision history:
+// -----------------
+// 0) 10 Jan 2002 : original
+// 1) 16 Jan 2002 : replaced Frequency with Gray_Frequency and
+//                  Multigroup_Frequency, types on which we will template.
+// 
+//===========================================================================//
+
+class Gray_Frequency 
+{
+
+  public:
+
+    // Useful typedefs.
+    typedef std::vector<double>        sf_double;
+    typedef std::pair<double,double>   pair_doubles;
+
+  private:
+
+    // Frequency group boundaries.
+    sf_double group_boundaries;
+
+  public:
+
+    // Constructor.
+    explicit Gray_Frequency(const sf_double & grp_bnds = sf_double())
+	: group_boundaries(grp_bnds)
+    {
+	// Check that there are no group bounds.
+	Insist (group_boundaries.size() == 0, 
+		"Non-null group bnds given to Gray_Frequency!");
+    }
+
+    // >>> Accessors.
+    
+    //! Query whether frequency is gray.
+    bool is_gray() const { return true; }
+
+    //! Query whether frequency is multigroup.
+    bool is_multigroup() const { return false; }
+
+    //! Get number of frequency groups.
+    int get_num_groups() const { return 0; }
+
+    //! Get number of frequency group boundaries.
+    int get_num_group_boundaries() const { return 0; }
+
+    //! Get all group boundaries.
+    sf_double get_group_boundaries() const { return group_boundaries; }
+
+    //! Find a frequency group, in [1,G], given a frequency [keV]. 
+    int find_group_given_a_freq(const double hnu) const { return 0; }
+
+    //! Get both low and high frequency group boundaries [keV].  Usage is
+    //  get_group_boundaries.first and get_group_boundaries.second.
+    pair_doubles get_group_boundaries(const int group) const
+    {
+	// Return a pair of double zeros.
+	return pair_doubles(0.0, 0.0); 
+    }
 };
 
 } // end namespace rtt_imc
