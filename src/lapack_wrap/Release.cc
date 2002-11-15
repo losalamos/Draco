4a4e59e7f7d62a2f75c4f602e0db683fd79469b5
tme <tme@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2002-11-15 02:13:02 +0000

    Prep for release 1_0_0 of lapack_wrap.

--- src/lapack_wrap/Release.cc
+++ src/lapack_wrap/Release.cc
@@ -24,7 +24,7 @@ using std::string;
  */
 const string release()
 {
-    string pkg_release = "@(#)lapack_wrap-0_0_0";
+    string pkg_release = "@(#)lapack_wrap-1_0_0";
     return pkg_release;
 }
 
