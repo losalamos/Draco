04d2e2ebae1771e1877912b8577543b0a1230a8a
lowrie <lowrie@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2003-01-22 15:27:18 +0000

    Added intervals() accessor, which returns the number of start()/stop() time intervals.
    Added interval sums to print(), whenever intervals() > 1.
    
    Documentation rewording.

--- src/c4/Timer.cc
+++ src/c4/Timer.cc
@@ -23,16 +23,28 @@ void Timer::print(std::ostream &out, int p) const
 {
     using std::setw;
     using std::endl;
-    using std::cout;
     using std::ios;
     
     out.setf(ios::fixed, ios::floatfield);
     out.precision(p);
     out << endl;
+    
+    if ( num_intervals > 1 )
+	out << "LAST INTERVAL: " << endl;
+
     out << setw(20) << "WALL CLOCK TIME: " << wall_clock() << endl;
     out << setw(20) << "  USER CPU TIME: " << user_cpu() << endl;
     out << setw(20) << "SYSTEM CPU TIME: " << system_cpu() << endl;
     out << endl;
+    
+    if ( num_intervals > 1 )
+    {
+	out << "OVER " << num_intervals << " INTERVALS: " << endl;
+	out << setw(20) << "WALL CLOCK TIME: " << sum_wall_clock() << endl;
+	out << setw(20) << "  USER CPU TIME: " << sum_user_cpu() << endl;
+	out << setw(20) << "SYSTEM CPU TIME: " << sum_system_cpu() << endl;
+	out << endl;
+    }
 }
 
 } // end namespace rtt_c4
--- src/c4/Timer.hh
+++ src/c4/Timer.hh
@@ -62,44 +62,50 @@ class Timer
     // Flag determining if timer is currently on.
     bool timer_on;
 
-    // sum of wall clock time between starts and stops
+    // sum of wall clock time over all intervals.
     double sum_wall;
 
-    // sum of system clock time between starts and stops
+    // sum of system clock time over all intervals.
     double sum_system;
 
-    // sum of system clock time between starts and stops
+    // sum of system clock time over all intervals.
     double sum_user;
+
+    // number of time intervals.
+    int num_intervals;
 
   public:
     //! Constructor.
     Timer() : timer_on(false), clock_tick(sysconf(_SC_CLK_TCK)) { reset(); }
 
-    //! Set the beginning of time cycle.
+    //! Set the beginning of the time interval.
     inline void start();
 
-    //! Set the end of time cycle.
+    //! Set the end of the time interval.
     inline void stop();
 
-    //! Return the wall clock time in seconds.
+    //! Return the wall clock time in seconds, for the last interval.
     inline double wall_clock() const;
 
-    //! Return the system cpu time in seconds.
+    //! Return the system cpu time in seconds, for the last interval.
     inline double system_cpu() const;
 
-    //! Return the user cpu time in seconds.
+    //! Return the user cpu time in seconds, for the last interval.
     inline double user_cpu() const;
 
-    //! Return the wall clock time in seconds, summed over starts and stops.
+    //! Return the wall clock time in seconds, summed over all intervals.
     double sum_wall_clock() const { Require(! timer_on); return sum_wall; }
 
-    //! Return the system cpu time in seconds, summed over starts and stops.
+    //! Return the system cpu time in seconds, summed over all intervals.
     double sum_system_cpu() const { Require(! timer_on); return sum_system; }
 
-    //! Return the user cpu time in seconds, summed over starts and stops.
+    //! Return the user cpu time in seconds, summed over all intervals.
     double sum_user_cpu() const { Require(! timer_on); return sum_user; }
 
-    //! Reset the sums of times.
+    //! Return the number of time intervals used in the sums.
+    int intervals() const { Require(! timer_on); return num_intervals; }
+
+    //! Reset the interval sums.
     inline void reset();
 
     //! Print out a timing report.
@@ -123,7 +129,8 @@ inline std::ostream& operator<<(std::ostream &out, const Timer &t)
 void Timer::start()
 {
     Require(! timer_on);
-    timer_on = true; 
+    timer_on = true;
+    ++num_intervals;
     begin    = times(&tms_begin);
 }
 
@@ -132,8 +139,8 @@ void Timer::start()
 void Timer::stop()
 {
     Require(timer_on);
-    timer_on = false; 
     end      = times(&tms_end);
+    timer_on = false; 
 
     sum_wall   += wall_clock();
     sum_system += system_cpu();
@@ -172,9 +179,10 @@ void Timer::reset()
 {
     Require(! timer_on);
 
-    sum_wall   = 0.0;
-    sum_system = 0.0;
-    sum_user   = 0.0;
+    num_intervals = 0;
+    sum_wall      = 0.0;
+    sum_system    = 0.0;
+    sum_user      = 0.0;
 }
 
 } // end namespace rtt_c4
