423d379efecfc1cfdd3b82f3d6978f9aebdb637a
tme <tme@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2003-03-14 19:46:31 +0000

    Prep for imc-3_3_0 release.

--- src/imc/Release.cc
+++ src/imc/Release.cc
@@ -24,7 +24,7 @@ using std::string;
  */
 const string release()
 {
-    string pkg_release = "@(#)imc-3_2_0";
+    string pkg_release = "@(#)imc-3_3_0";
     return pkg_release;
 }
 
--- src/imc/Release.hh
+++ src/imc/Release.hh
@@ -9,30 +9,34 @@
 // $Id$
 //---------------------------------------------------------------------------//
 
-#ifndef __imc_Release_hh__
-#define __imc_Release_hh__
+#ifndef rtt_imc_Release_hh
+#define rtt_imc_Release_hh
 
 //===========================================================================//
 /*!
  * \page imc_overview Overview of the IMC package
- * \version 2_2_0
+ * \version 3_3_0
  * \author Tom Evans, Todd Urbatsch, Mike Buksas
-
+ *
  * The imc package is a set of class that are used to perform cell-centered,
  * Fleck and Cummings Implicit Monte Carlo transport.  The imc package has
- * capabilities for two types of parallel decompositions: (1) full
+ * capabilities for two types of parallel decompositions: (1) full domain
  * replication; (2) full domain decomposition.  The primary components of the
  * imc package are:
-
+ *
  * \arg Transporter the imc transport solver;
  * \arg Particle the imc particle class;
  * \arg Source the imc source class;
  * \arg Mat_State the imc material state class;
  * \arg Opacity the imc opacity class;
  * \arg Tally the imc tally calss;
-
+ *
+ * The frequency treatment is determined, in most cases, upon the Frequency
+ * Type (FT) template parameter.  This parameter can be set to Gray_Frequency
+ * or Multigroup_Frequency.  Classes that depend on FT use specialization to
+ * perform the correct gray or multigroup operations.
+ *
  * The imc package makes heavy use of the mc (rtt_mc) package.
-
  */
 //===========================================================================//
 /*!
@@ -53,7 +57,7 @@ namespace rtt_imc
     const std::string release();
 }
 
-#endif                          // __imc_Release_hh__
+#endif                          // rtt_imc_Release_hh
 
 //---------------------------------------------------------------------------//
 //                              end of Release.hh
