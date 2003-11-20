f46da1041ac6197abeaff5d78896f2f170d243d6
mwbuksas <mwbuksas@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2003-11-20 23:17:48 +0000

    Added --enable-plot2D option to UMCF's local configure options. This   allows for the selective enabling of code which uses the draco   plot2D package.
    Removed the dependence of the unit tests on plot2D. This is so the
      regression tests will compile (with luck). A means of selectively
      including the dependence on plot2D is needed.
    
    Also, minor changes in the makefile for the autodoc directory.
    
    Some changes in aclocal.m4 files appear to have been caused by getting
      the latest version of the /config directory.

--- tools/partisn.py
+++ tools/partisn.py
@@ -3,48 +3,8 @@
 ## Script to run variations on a partisn input file
 ##---------------------------------------------------------------------------##
 import os
-import re
 import sys
-import popen2
 
-class Options:
-    def __init__(self, given_commands):
-        self.run_umcf = 0
-        self.run_sn = 0
-        self.run_census = 0
-        self.output_name = 'rsp'
-        self.input_name  = 'test.inp'
-        self.commands = given_commands
-
-##---------------------------------------------------------------------------##
-## Parse_arguments: extract arguments
-##---------------------------------------------------------------------------##
-def parse_arguments(arguments):
-
-    import getopt
-
-    try:
-        options, commands = getopt.getopt(arguments, 'usbco:i:')
-    except getopt.GetoptError:
-        sys.exit('ERROR: Bad option or missing argument.')
-
-    trial = Options(commands)
-
-    for option in options:
-        if option[0] == '-u': trial.run_umcf = 1
-        if option[0] == '-s': trial.run_sn   = 1
-        if option[0] == '-b': trial.run_umcf = trial.run_sn = 1
-        if option[0] == '-c': trial.run_census = 1
-        if option[0] == '-o': trial.output_name = option[1]
-        if option[0] == '-i': trial.input_name  = option[1]
-
-    # Run umcf by default:
-    if trial.run_umcf==0 and trial.run_sn==0 and trial.run_census==0:
-        trial.run_umcf = 1
-
-    return trial
-
-    
 
 ##---------------------------------------------------------------------------##
 ## Create filter dictionary
@@ -60,6 +20,66 @@ def filter_dictionary(filter_list):
     return filters
 
 ##---------------------------------------------------------------------------##
+## Class Options:
+##
+## Parses the command-line arguments with the getopt module.
+## Extracts the following options and information:
+##   -u: Perform a PARTISN run with Uncle McFlux on.
+##   -s: Perform a PARTISN run with Ray-tracing off.
+##   -b: Perform both of the above (equivalent to -us)
+## None of the above is equivalent to -u (run UMCF)
+##   -c: Perform a special Census-enabled run of PARTISN
+##   -o: <name> Change the default output data file from rsp.dat to
+##       name.dat
+##   -i: <name> Change the input deck name from test.inp to name.inp 
+##
+## All other arguments are assumed to be keyword=value pairs and a
+## parsed into a dictionary.
+##---------------------------------------------------------------------------##
+
+
+class Options:
+    def __init__(self, arguments):
+        self.run_umcf = 0
+        self.run_sn = 0
+        self.run_census = 0
+        self.output_name = 'rsp'
+        self.input_name  = 'test.inp'
+
+        self.parse_arguments(arguments)
+
+##---------------------------------------------------------------------------##
+## Parse_arguments: extract arguments
+##---------------------------------------------------------------------------##
+    def parse_arguments(self, arguments):
+
+        import getopt
+
+        try:
+            options, partisn_commands = getopt.getopt(arguments, 'usbco:i:')
+        except getopt.GetoptError:
+            sys.exit('ERROR: Bad option or missing argument.')
+            
+        for option in options:
+            if option[0] == '-u': self.run_umcf = 1
+            if option[0] == '-s': self.run_sn   = 1
+            if option[0] == '-b': self.run_umcf = self.run_sn = 1
+            if option[0] == '-c': self.run_census = 1
+            if option[0] == '-o': self.output_name = option[1]
+            if option[0] == '-i': self.input_name  = option[1]
+            
+            # Run umcf by default:
+        if self.run_umcf==0 and self.run_sn==0 and self.run_census==0:
+            self.run_umcf = 1
+
+        # Make command dictionary
+        self.command_dict = filter_dictionary(partisn_commands)
+##---------------------------------------------------------------------------##
+## End of class Options
+##---------------------------------------------------------------------------##
+    
+
+##---------------------------------------------------------------------------##
 ## Transform line function
 ##
 ## Search and replace values in the given line
@@ -70,6 +90,8 @@ def filter_dictionary(filter_list):
 # value. Return the line with the subsistutions.
 # 
 def transform_line(line, dict):
+    import re
+    
     for key, val in dict.items():
 
         # Search for 'key='<value> preceeded and followed by
@@ -89,6 +111,8 @@ def transform_line(line, dict):
 ## Execute partisn command and move output data file
 ##---------------------------------------------------------------------------##
 def execute_partisn(dir, input_lines, output_name):
+
+    import popen2
 
     executable = dir + '/partisn'
     data_file  = dir + "rsp.dat"
@@ -125,14 +149,11 @@ if __name__=="__main__":
     working_directory   = "/home/mwbuksas/work/partisn_test_problem/"
 
     # Parse the command line options:
-    options = parse_arguments(sys.argv[1:])
+    options = Options(sys.argv[1:])
 
     # Read the input file
     input_lines = open(working_directory +
                        options.input_name).readlines()
-
-    # Get the command-line dictionary of commands
-    dictionary = filter_dictionary(options.commands)
 
     ## Do a regular umcf partisn run:
     ## ------------------------------
@@ -140,9 +161,9 @@ if __name__=="__main__":
     if options.run_umcf:
         print "Executing PARTISN with UMCF"
 
-        dictionary['fcsrc'] = 'umcflux'
-        umcf_lines = [transform_line(line, dictionary) for line in
-                      input_lines]
+        options.command_dict['fcsrc'] = 'umcflux'
+        umcf_lines = [transform_line(line, options.command_dict) \
+                      for line in input_lines]
 
         result_file = working_directory + options.output_name + "_umcf.dat"
         execute_partisn(regular_partisn_dir, umcf_lines, result_file)
@@ -154,8 +175,9 @@ if __name__=="__main__":
     if options.run_sn:
         print "Executing PARTISN without UMCF"
 
-        dictionary['fcsrc'] = 'no'
-        sn_lines = [transform_line(line, dictionary) for line in input_lines]
+        options.command_dict['fcsrc'] = 'no'
+        sn_lines = [transform_line(line, options.command_dict) \
+                    for line in input_lines]
         
         result_file = working_directory + options.output_name + "_sn.dat"
         execute_partisn(regular_partisn_dir, sn_lines, result_file)
@@ -172,9 +194,9 @@ if __name__=="__main__":
             print "Removing old census file"
             os.remove(census_file)
             
-        dictionary['fcsrc'] = 'umcflux'
-        umcf_lines = [transform_line(line, dictionary) for line in
-                      input_lines]
+        options.command_dict['fcsrc'] = 'umcflux'
+        umcf_lines = [transform_line(line, options.command_dict) \
+                      for line in input_lines]
 
         result_file = working_directory + options.output_name + "_census.dat"
         execute_partisn(census_partisn_dir, umcf_lines, result_file)
