be2994d3420c609a080e8a79cffa1c76351e7733
lowrie <lowrie@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2004-01-21 22:53:06 +0000

    Moved big inline function definitions into *.i.hh.

--- src/ds++/Vector_Lite.hh
+++ src/ds++/Vector_Lite.hh
@@ -71,223 +71,155 @@ class Vector_Lite
 
     // CREATORS
 
-    /// Constructor based on a single element type.
-    /// Sets all values to \a u.
-    Vector_Lite(const T &u = T()) {
-	for ( size_type i = 0; i < N; i++ )
-	    d_U[i] = u;
-    }
+    // Constructor based on a scalar value.
+    inline Vector_Lite(const T &u = T());
 
-    /// Copy constructor.
-    Vector_Lite(const Vector_Lite &rhs) {
-	for ( size_type i = 0; i < N; i++ )
-	    d_U[i] = rhs.d_U[i];
-    }
+    // Copy constructor.
+    inline Vector_Lite(const Vector_Lite &rhs);
 
-    /// Constructor for N = 2.
-    Vector_Lite(const T &u0, const T &u1) {
- 	Require(N == 2); d_U[0] = u0; d_U[1] = u1;
-    }
+    // Constructor for N = 2.
+    inline Vector_Lite(const T &u0, const T &u1);
 
-    /// Constructor for N = 3.
-    Vector_Lite(const T &u0, const T &u1, const T &u2) {
-	Require(N == 3); d_U[0] = u0; d_U[1] = u1; d_U[2] = u2;
-    }
+    // Constructor for N = 3.
+    inline Vector_Lite(const T &u0, const T &u1, const T &u2);
 
-    /// Constructor for N = 4.
-    Vector_Lite(const T &u0, const T &u1, const T &u2,
-		const T &u3) {
-	Require(N == 4); d_U[0] = u0; d_U[1] = u1; d_U[2] = u2;
-	d_U[3] = u3;
-    }
+    // Constructor for N = 4.
+    inline Vector_Lite(const T &u0, const T &u1, const T &u2, const T &u3);
 
-    /// Constructor for N = 5.
-    Vector_Lite(const T &u0, const T &u1, const T &u2,
-                const T &u3, const T &u4) {
-	Require(N == 5); d_U[0] = u0; d_U[1] = u1; d_U[2] = u2;
-	d_U[3] = u3; d_U[4] = u4;
-    }
-
-    /// Constructor for N = 6.
-    Vector_Lite(const T &u0, const T &u1, const T &u2,
-		const T &u3, const T &u4, const T &u5) {
-	Require(N == 6); d_U[0] = u0; d_U[1] = u1; d_U[2] = u2;
-	d_U[3] = u3; d_U[4] = u4; d_U[5] = u5;
-    }
+    // Constructor for N = 5.
+    inline Vector_Lite(const T &u0, const T &u1, const T &u2,
+		       const T &u3, const T &u4);
 
     /// Destructor.
     ~Vector_Lite(void) { }
 
     // MANIPULATORS
 
-    /// Assignment to another Vector_Lite.
-    Vector_Lite &operator=(const Vector_Lite &rhs) {
-	if ( this == &rhs ) {
-	    return *this;
-	}
-	
-	for ( size_type i = 0; i < N; i++ )
-	    d_U[i] = rhs.d_U[i];
-	
-	return *this;
-    }
+    // Assignment to another Vector_Lite.
+    inline Vector_Lite &operator=(const Vector_Lite &rhs);
 
-    /// Assignment to type T.
-    Vector_Lite &operator=(const T rhs) {
-	for ( size_type i = 0; i < N; i++ )
-	    d_U[i] = rhs;
+    // Assignment to a scalar.
+    inline Vector_Lite &operator=(const T &rhs);
 
-	return *this;
-    }
+    // Comparison to another Vector_Lite.
+    inline bool operator==(const Vector_Lite &a) const;
 
-    /// Comparison to another Vector_Lite.
-    bool operator==(const Vector_Lite &a) const {
-	for ( size_type i = 0; i < N; i++ )
-	    if ( d_U[i] != a.d_U[i] ) return false;
-	return true;
-    }
+    // Basic arithmetic operations, vector right-hand side
 
-    // Basic arithmetic operations
+    inline Vector_Lite &operator+=(const Vector_Lite &a);
+    inline Vector_Lite &operator-=(const Vector_Lite &a);
+    inline Vector_Lite &operator*=(const Vector_Lite &a);
+    inline Vector_Lite &operator/=(const Vector_Lite &a);
 
-    Vector_Lite &operator+=(const Vector_Lite &a) {
-	for ( size_type i = 0; i < N; i++ )
-	    d_U[i] += a.d_U[i];
-	return *this;
-    }
+    // Basic arithmetic operations, scalar right-hand side
 
-    Vector_Lite &operator-=(const Vector_Lite &a) {
-	for ( size_type i = 0; i < N; i++ )
-	    d_U[i] -= a.d_U[i];
-	return *this;
-    }
-
-    Vector_Lite &operator*=(const Vector_Lite &a) {
-	for ( size_type i = 0; i < N; i++ )
-	    d_U[i] *= a.d_U[i];
-	return *this;
-    }
-
-    Vector_Lite &operator/=(const Vector_Lite &a) {
-	for ( size_type i = 0; i < N; i++ )
-	    d_U[i] /= a.d_U[i];
-	return *this;
-    }
-
-    Vector_Lite &operator+=(const T a) {
-	for ( size_type i = 0; i < N; i++ )
-	    d_U[i] += a;
-	return *this;
-    }
-
-    Vector_Lite &operator-=(const T a) {
-	for ( size_type i = 0; i < N; i++ )
-	    d_U[i] -= a;
-	return *this;
-    }
-
-    Vector_Lite &operator*=(const T a) {
-	for ( size_type i = 0; i < N; i++ )
-	    d_U[i] *= a;
-	return *this;
-    }
-
-    Vector_Lite &operator/=(const T a) {
-	for ( size_type i = 0; i < N; i++ )
-	    d_U[i] /= a;
-	return *this;
-    }
-
+    inline Vector_Lite &operator+=(const T &a);
+    inline Vector_Lite &operator-=(const T &a);
+    inline Vector_Lite &operator*=(const T &a);
+    inline Vector_Lite &operator/=(const T &a);
+    
     /// Returns true if \a i is a valid array index.
-    bool valid_index(const int i) const {
+    bool valid_index(const int i) const
+    {
 	return (i >= 0 && static_cast<size_type>(i) < N);
     }
 
     // ...overloaded versions.
     // We need to overload to avoid automatic
     // casts to either int or unsigned versions.
-    bool valid_index(const unsigned int i) const {
-	return (i < N);
-    }
+    bool valid_index(const unsigned int i) const { return (i < N); }
 
-    bool valid_index(const long int i) const {
-	return (i >= 0 && i < N);
-    }
+    bool valid_index(const long int i) const { return (i >= 0 && i < N); }
 
-    bool valid_index(const unsigned long int i) const {
-	return (i < N);
-    }
+    bool valid_index(const unsigned long int i) const { return (i < N); }
 
     // ACCESSORS
 
     // Indexing for int argument
 
-    reference operator()(const int i) {
+    reference operator()(const int i)
+    {
 	Require(valid_index(i)); return d_U[i];
     }
     
-    const_reference operator()(const int i) const {
+    const_reference operator()(const int i) const
+    {
 	Require(valid_index(i)); return d_U[i];
     }
     
-    reference operator[](const int i) {
+    reference operator[](const int i)
+    {
 	Require(valid_index(i)); return d_U[i];
     }
     
-    const_reference operator[](const int i) const {
+    const_reference operator[](const int i) const
+    {
 	Require(valid_index(i)); return d_U[i];
     }
 
     // Indexing for unsigned int argument
 
-    reference operator()(const unsigned int i) {
+    reference operator()(const unsigned int i)
+    {
 	Require(valid_index(i)); return d_U[i];
     }
     
-    const_reference operator()(const unsigned int i) const {
+    const_reference operator()(const unsigned int i) const
+    {
 	Require(valid_index(i)); return d_U[i];
     }
     
-    reference operator[](const unsigned int i) {
+    reference operator[](const unsigned int i)
+    {
 	Require(valid_index(i)); return d_U[i];
     }
     
-    const_reference operator[](const unsigned int i) const {
+    const_reference operator[](const unsigned int i) const
+    {
 	Require(valid_index(i)); return d_U[i];
     }
 
     // Indexing for long int argument
 
-    reference operator()(const long int i) {
+    reference operator()(const long int i)
+    {
 	Require(valid_index(i)); return d_U[i];
     }
     
-    const_reference operator()(const long int i) const {
+    const_reference operator()(const long int i) const
+    {
 	Require(valid_index(i)); return d_U[i];
     }
     
-    reference operator[](const long int i) {
+    reference operator[](const long int i)
+    {
 	Require(valid_index(i)); return d_U[i];
     }
     
-    const_reference operator[](const long int i) const {
+    const_reference operator[](const long int i) const
+    {
 	Require(valid_index(i)); return d_U[i];
     }
 
     // Indexing for unsigned long int argument
 
-    reference operator()(const unsigned long int i) {
+    reference operator()(const unsigned long int i)
+    {
 	Require(valid_index(i)); return d_U[i];
     }
     
-    const_reference operator()(const unsigned long int i) const {
+    const_reference operator()(const unsigned long int i) const
+    {
 	Require(valid_index(i)); return d_U[i];
     }
     
-    reference operator[](const unsigned long int i) {
+    reference operator[](const unsigned long int i)
+    {
 	Require(valid_index(i)); return d_U[i];
     }
     
-    const_reference operator[](const unsigned long int i) const {
+    const_reference operator[](const unsigned long int i) const
+    {
 	Require(valid_index(i)); return d_U[i];
     }
 
--- src/ds++/Vector_Lite.i.hh
+++ src/ds++/Vector_Lite.i.hh
@@ -17,6 +17,299 @@ namespace rtt_dsxx
 {
 
 //---------------------------------------------------------------------------//
+// member functions
+//---------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------//
+/*!
+ * \brief Constructor based on a scalar value.
+ *
+ * Initializes all values to \a u.  Note that this ctor also acts as the
+ * default ctor.
+ * 
+ * \param u     Scalar value.
+ */
+template <class T, size_t N>
+Vector_Lite<T, N>::
+Vector_Lite(const T &u)
+{
+    for ( size_type i = 0; i < N; i++ )
+    {
+	d_U[i] = u;
+    }
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * \brief Copy constructor.
+ * 
+ * \param rhs   Value to copy.
+ */
+template <class T, size_t N>
+Vector_Lite<T, N>::
+Vector_Lite(const Vector_Lite &rhs)
+{
+    for ( size_type i = 0; i < N; i++ )
+    {
+	d_U[i] = rhs.d_U[i];
+    }
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * \brief Constructor for N = 2.
+ * 
+ * \param u0 1st element.    
+ * \param u1 2nd element.
+ */
+template <class T, size_t N>
+Vector_Lite<T, N>::
+Vector_Lite(const T &u0,
+	    const T &u1)
+{
+    Require(N == 2);
+    d_U[0] = u0;
+    d_U[1] = u1;
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * \brief Constructor for N = 3.
+ * 
+ * \param u0 1st element.    
+ * \param u1 2nd element.
+ * \param u2 3rd element.
+ */
+template <class T, size_t N>
+Vector_Lite<T, N>::
+Vector_Lite(const T &u0,
+	    const T &u1,
+	    const T &u2)
+{
+    Require(N == 3);
+    d_U[0] = u0;
+    d_U[1] = u1;
+    d_U[2] = u2;
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * \brief Constructor for N = 4.
+ * 
+ * \param u0 1st element.    
+ * \param u1 2nd element.
+ * \param u2 3rd element.
+ * \param u3 4th element.
+ */
+template <class T, size_t N>
+Vector_Lite<T, N>::
+Vector_Lite(const T &u0,
+	    const T &u1,
+	    const T &u2,
+	    const T &u3)
+{
+    Require(N == 4);
+    d_U[0] = u0;
+    d_U[1] = u1;
+    d_U[2] = u2;
+    d_U[3] = u3;
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * \brief Constructor for N = 5.
+ * 
+ * \param u0 1st element.    
+ * \param u1 2nd element.
+ * \param u2 3rd element.
+ * \param u3 4th element.
+ * \param u4 5th element.
+ */
+template <class T, size_t N>
+Vector_Lite<T, N>::
+Vector_Lite(const T &u0,
+	    const T &u1,
+	    const T &u2,
+	    const T &u3,
+	    const T &u4)
+{
+    Require(N == 5);
+    d_U[0] = u0;
+    d_U[1] = u1;
+    d_U[2] = u2;
+    d_U[3] = u3;
+    d_U[4] = u4;
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * \brief Assignment to another Vector_Lite.
+ */
+template <class T, size_t N>
+Vector_Lite<T, N> &
+Vector_Lite<T, N>::
+operator=(const Vector_Lite<T, N> &rhs)
+{
+    if ( this == &rhs )
+    {
+	return *this;
+    }
+	
+    for ( size_type i = 0; i < N; i++ )
+    {
+	d_U[i] = rhs.d_U[i];
+    }
+	
+    return *this;
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * \brief Assignment to a scalar.
+ */
+template <class T, size_t N>
+Vector_Lite<T, N> &
+Vector_Lite<T, N>::
+operator=(const T &rhs)
+{
+    for ( size_type i = 0; i < N; i++ )
+    {
+	d_U[i] = rhs;
+    }
+	
+    return *this;
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * \brief Comparison to another Vector_Lite.
+ */
+template <class T, size_t N>
+bool
+Vector_Lite<T, N>::
+operator==(const Vector_Lite<T, N> &a) const
+{
+    for ( size_type i = 0; i < N; i++ )
+    {
+	if ( d_U[i] != a.d_U[i] ) return false;
+    }
+    
+    return true;
+}
+
+//---------------------------------------------------------------------------//
+// basic arithmetic member functions, vector right-hand side
+//---------------------------------------------------------------------------//
+
+template <class T, size_t N>
+Vector_Lite<T,N> &
+Vector_Lite<T, N>::
+operator+=(const Vector_Lite<T, N> &a)
+{
+    for ( size_type i = 0; i < N; i++ )
+    {
+	d_U[i] += a.d_U[i];
+    }
+    
+    return *this;
+}
+
+template <class T, size_t N>
+Vector_Lite<T,N> &
+Vector_Lite<T, N>::
+operator-=(const Vector_Lite<T, N> &a)
+{
+    for ( size_type i = 0; i < N; i++ )
+    {
+	d_U[i] -= a.d_U[i];
+    }
+    
+    return *this;
+}
+
+template <class T, size_t N>
+Vector_Lite<T,N> &
+Vector_Lite<T, N>::
+operator*=(const Vector_Lite<T, N> &a)
+{
+    for ( size_type i = 0; i < N; i++ )
+    {
+	d_U[i] *= a.d_U[i];
+    }
+    
+    return *this;
+}
+
+template <class T, size_t N>
+Vector_Lite<T,N> &
+Vector_Lite<T, N>::
+operator/=(const Vector_Lite<T, N> &a)
+{
+    for ( size_type i = 0; i < N; i++ )
+    {
+	d_U[i] /= a.d_U[i];
+    }
+    
+    return *this;
+}
+
+//---------------------------------------------------------------------------//
+// basic arithmetic member functions, scalar right-hand side
+//---------------------------------------------------------------------------//
+
+template <class T, size_t N>
+Vector_Lite<T,N> &
+Vector_Lite<T, N>::
+operator+=(const T &a)
+{
+    for ( size_type i = 0; i < N; i++ )
+    {
+	d_U[i] += a;
+    }
+    
+    return *this;
+}
+
+template <class T, size_t N>
+Vector_Lite<T,N> &
+Vector_Lite<T, N>::
+operator-=(const T &a)
+{
+    for ( size_type i = 0; i < N; i++ )
+    {
+	d_U[i] -= a;
+    }
+    
+    return *this;
+}
+
+template <class T, size_t N>
+Vector_Lite<T,N> &
+Vector_Lite<T, N>::
+operator*=(const T &a)
+{
+    for ( size_type i = 0; i < N; i++ )
+    {
+	d_U[i] *= a;
+    }
+    
+    return *this;
+}
+
+template <class T, size_t N>
+Vector_Lite<T,N> &
+Vector_Lite<T, N>::
+operator/=(const T &a)
+{
+    for ( size_type i = 0; i < N; i++ )
+    {
+	d_U[i] /= a;
+    }
+    
+    return *this;
+}
+
+//---------------------------------------------------------------------------//
 // free functions
 //---------------------------------------------------------------------------//
 
