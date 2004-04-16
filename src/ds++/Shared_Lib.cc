d2478d407c260e8deb54921d670e9d7d99082787
lowrie <lowrie@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2004-04-16 17:55:38 +0000

    Added Shared_Lib (yet again).

---
+++ src/ds++/Shared_Lib.cc
@@ -0,0 +1,102 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   ds++/Shared_Lib.cc
+ * \author Rob Lowrie
+ * \date   Thu Apr 15 20:44:39 2004
+ * \brief  Implementation of Shared_Lib.
+ * \note   Copyright © 2003 The Regents of the University of California.
+ */
+//---------------------------------------------------------------------------//
+// $Id$
+//---------------------------------------------------------------------------//
+
+#include "Shared_Lib.hh"
+#include "Assert.hh"
+#include <string>
+
+// Use ::dlopen, ::dlclose, ::dlerror, ::dlsym
+#include <dlfcn.h>
+
+namespace rtt_dsxx
+{
+
+//---------------------------------------------------------------------------//
+/*!
+  \brief Default constructor.
+
+  \param file_name File name of the shared lib.  If empty, open() must be
+  called in order to use a shared lib.
+*/
+Shared_Lib::Shared_Lib(const std::string &file_name)
+    : d_handle(0)
+    , d_file_name(file_name)
+{
+    if ( not file_name.empty() )
+    {
+	open(file_name);
+    }
+}
+
+//---------------------------------------------------------------------------//
+/*!
+  \brief Copy constructor.
+
+  This is implemented by opening a new handle to the shared file.
+*/
+Shared_Lib::Shared_Lib(const Shared_Lib &from)
+    : d_file_name(from.d_file_name)
+{
+    open(d_file_name);
+}
+
+//---------------------------------------------------------------------------//
+/*!
+  \brief Assignment.
+
+  This is implemented by opening a new handle to the shared file.
+*/
+Shared_Lib &Shared_Lib::operator=(const Shared_Lib &rhs)
+{
+    if ( this == &rhs )
+    {
+	return *this;
+    }
+
+    d_file_name = rhs.d_file_name;
+    open(d_file_name);
+
+    return *this;
+}
+
+//---------------------------------------------------------------------------//
+/*!
+  \brief Closes the shared library, if it is open.
+*/
+void Shared_Lib::close()
+{
+    if ( is_open() )
+    {
+	dlclose(d_handle);
+    }
+}
+
+//---------------------------------------------------------------------------//
+/*!
+  \brief Returns the handle to the shared library.
+*/
+void Shared_Lib::open(const std::string &file_name)
+{
+    Require(not file_name.empty());
+    
+    close();
+    d_handle = dlopen(file_name.c_str(), RTLD_LAZY);
+
+    Insist(d_handle, dlerror());
+}
+
+
+} // end namespace rtt_dsxx
+
+//---------------------------------------------------------------------------//
+//                 end of Shared_Lib.cc
+//---------------------------------------------------------------------------//
