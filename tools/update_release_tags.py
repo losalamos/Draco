1d316c4ede128ebb51501f4533e6ea165977ac2a
tme <tme@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2004-11-10 21:39:38 +0000

    Fixed an accidental overwrite.

--- tools/update_release_tags.py
+++ tools/update_release_tags.py
@@ -33,7 +33,7 @@ def get_dirs(homedir):
     for d in dirs:
         if os.path.isdir(d):
             os.chdir(d)
-            if os.path.isdir("autodoc"):
+            if os.path.isfile("Release.cc"):
                 rdirs.append(d)
             os.chdir(homedir)
 
