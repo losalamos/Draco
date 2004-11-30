ea38cb369b1836addeee4e31317d4ee078073233
tme <tme@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2004-11-30 18:02:15 +0000

    Moved syntax highlighting to level 3 from level 2.

--- environment/elisp/fl-keywords.el
+++ environment/elisp/fl-keywords.el
@@ -7,11 +7,12 @@
 (require 'font-lock)
 
 (defun add-draco-dbc-font-lock-keywords ()
+  (interactive)
   (setq font-lock-keywords
 	(append
 	 '(("\\<\\(Check\\|Ensure\\|Insist\\|Require\\)\\>"
-	    0 font-lock-warning-face t))
-	 c++-font-lock-keywords-2)))
+	    1 font-lock-warning-face t))
+	 c++-font-lock-keywords-3)))
 
 (provide 'fl-keywords)
 
