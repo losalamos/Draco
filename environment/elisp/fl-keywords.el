9737c659c5298e452ffcbf10cefe54e161acaa29
tme <tme@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2004-11-30 18:08:00 +0000

    You can now highlight DBC stuff by defining a color for kull-macro face:
    (custom-set-faces
      '(font-lock-kull-macros-face ((t (:foreground "magenta"))) t))
    
    This will highlight Require|Check|Ensure|Insist to the color of your
    choice.  (Use the Edit Faces menu option to change the color; its
    easier).
    
    QED

--- environment/elisp/fl-keywords.el
+++ environment/elisp/fl-keywords.el
@@ -6,12 +6,14 @@
 
 (require 'font-lock)
 
+(make-face 'font-lock-kull-macros-face)
+
 (defun add-draco-dbc-font-lock-keywords ()
   (interactive)
   (setq font-lock-keywords
 	(append
 	 '(("\\<\\(Check\\|Ensure\\|Insist\\|Require\\)\\>"
-	    1 font-lock-warning-face t))
+	    1 font-lock-kull-macros-face t))
 	 c++-font-lock-keywords-3)))
 
 (provide 'fl-keywords)
