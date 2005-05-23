696a3357efa267a877906ae9736bea12d4465b8f
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2005-05-23 20:45:21 +0000

    o Major update to Draco elisp package.
      To use:
    
         1. Edit ~/.xemacs/init.el and remove all old code for Draco elisp
            setup.
    
         2. Replace with one of the following elisp code blocks (replace
            paths with your custom locations as needed).
    
            a. Released (tested) elisp:
    
               (setq draco-env-dirs (list
                   "/codes/radtran/vendors/environment/"
                   "~/draco/environment/"
                   "~/.xemacs" ))
               (load-library
               "/codes/radtran/vendors/environment/elisp/draco-setup"))
    
            b. Early Adopters (always use latests elisp):
    
               (setq draco-env-dirs (list
                   "~/draco/environment/"
                   "/codes/radtran/vendors/environment/"
                   "~/.xemacs" ))
               (load-library "~/draco/environment/elisp/draco-setup"))
    
         3. Be sure to recompile all elisp code in your ~/.xemacs
            directory.
    
            a. cd ~/.xemacs
            b. xemacs . &  (starts XEmacs in dired mode)
    	c. Use the following sequence of keys to select elisp files
               and recmpile:
               % e (select files by extension; or use the menu "Regexp" ->
                   "Mark files with extension")
               el  (Mark all elisp files)
               B   (byte compile marked files)
    
         4. If you use (2b) then you need to compile your local draco
            elisp code using the steps provided by (3).
    
    o Notes:
    
      1. Draco-mode is NOT turned on for fundamental mode.
      2. The "compile-elisp" script doesn't appear to work anymore.  I
         will investigate this problem.
    
    Tracker:
    Status: Open[ ] Closed[ ] Pending[ ]

---
+++ environment/elisp/draco-global-keys.el
@@ -0,0 +1,49 @@
+;; ======================================================================
+;; draco-global-keys.el
+;; Kelly Thompson
+;; 8 Dec 2004
+;;
+;; $Id$
+;;
+;; Define some global key bindings
+;; 
+;; Usage: (require 'draco-global-keys)
+;; ======================================================================
+
+;; Kill default XEmacs key binding annoyances:
+
+(define-key global-map "\C-x\C-k" 'kill-buffer)
+(define-key global-map "\e?"      'help-for-help)
+(define-key global-map "\C-x?"    'describe-key-briefly)
+(define-key global-map "\C-z"     'undo)
+
+;; Force these onto the global map as well as the draco-mode-map
+(define-key global-map 'button3             'kill-region)
+(define-key global-map [(meta button3)]     'delete-rectangle)
+(define-key global-map [(control button1)]  'popup-buffer-menu)
+(define-key global-map [(control button2)]  'function-menu)
+(define-key global-map [(control button3)]  'popup-mode-menu)
+
+;; Buffer management
+(define-key global-map [(f7)]               'draco-save-and-kill-current-buffer)
+(define-key global-map [(shift f7)]         'delete-window)
+(define-key global-map [(control f7)]       'kill-this-buffer)
+(define-key global-map [(f8)]               'draco-toggle-previous-buffer)
+(define-key global-map [(control f8)]       'draco-find-companion-file)
+
+;; Mouse wheel for Lambda
+(if (getenv "HOST")
+    (setq machine-name (getenv "HOST"))
+(setq machine-name "none"))
+
+(if (string-match "lambda" machine-name)
+    (progn
+      (define-key global-map [(button4)] '(lambda () 
+					    (interactive)
+					    (scroll-down 5)))
+      (define-key global-map [(button5)] '(lambda () 
+					    (interactive)
+					    (scroll-up 5)))
+      ))
+
+(provide 'draco-global-keys)
