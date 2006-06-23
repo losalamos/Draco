7d62d7841f98e08940784408ae424669d8f7a84a
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2006-06-23 20:29:25 +0000

    o Replace Ylm() function with several variants:
      normalizedYlk
      realYlk
      complexYlk
      galerkinYlk
    
      also provide functions to encapsulate computation of normalization
      coefficient and the Associated Legendre polynomial.
    
      cPlk
      cPlkGalerkin
    
    o Significant update of doxygen documentation describes the various
      options.

--- src/special_functions/Ylm.hh
+++ src/special_functions/Ylm.hh
@@ -16,8 +16,43 @@
 namespace rtt_sf
 {
 
-//! Compute the spherical harmonic \f$ Y_{lm}(\theta,\phi) \f$ 
-double Ylm(unsigned l, int m, double theta, double phi);
+//! Compute the spherical harmonic coefficient multiplied by the appropriate Associated Legendre Polynomial \f$ c_{l,k}P_{l,k}(\mu) \f$.
+double cPlk( unsigned const l,
+             unsigned const m,
+             double   const mu );
+
+//! Compute Morel's Galerkin-quadrature spherical harmonic coefficient multiplied by the appropriate Associated Legendre Polynomial.
+double cPlkGalerkin( unsigned const l,
+                     unsigned const m,
+                     double   const mu,
+                     double   const sumwt );
+
+//! Compute the normalized spherical harmonic \f$ y_{l,k}(\theta,\phi) \f$.
+double normalizedYlk( unsigned const l,
+                      int      const m,
+                      double   const theta,
+                      double   const phi );
+
+//! Compute the real portion of the spherical harmonic \f$ Y_{l,k}(\theta,\phi) \f$.
+double realYlk( unsigned const l,
+                int      const m,
+                double   const theta,
+                double   const phi );
+                
+
+//! Compute the complex portion of the spherical harmonic \f$ Y_{l,k}(\theta,\phi) \f$.
+double complexYlk( unsigned const l,
+                   int      const m,
+                   double   const theta,
+                   double   const phi );
+
+//! Compute the spherical harmonic as used by Morel's Galerkin Quadrature paper.
+double galerkinYlk( unsigned const l,
+                    int      const m,
+                    double   const mu,
+                    double   const phi,
+                    double   const sumwt );
+
 
 } // end namespace rtt_sf
 
