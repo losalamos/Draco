46b3f604a3dad98fd5c7b99097c5d601389cd253
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2006-07-21 21:44:58 +0000

    o Covert unit test from a parallel test to a scalar test.

--- src/parser/test/tstToken.cc
+++ src/parser/test/tstToken.cc
@@ -3,20 +3,18 @@
  * \file   parser/test/tstToken.cc
  * \author Kent G. Budge
  * \date   Feb 18 2003
- * \brief  
+ * \brief  Unit test for the class rtt_parser::Token.
+ * \note   Copyright © 2006 Los Alamos National Security, LLC
  */
 //---------------------------------------------------------------------------//
 // $Id$
 //---------------------------------------------------------------------------//
 
-#include "parser_test.hh"
+#include "ds++/ScalarUnitTest.hh"
 #include "../Release.hh"
 #include "../Token.hh"
-#include "c4/global.hh"
-#include "c4/SpinLock.hh"
 
 using namespace std;
-
 using namespace rtt_parser;
 using namespace rtt_dsxx;
 
@@ -24,50 +22,50 @@ using namespace rtt_dsxx;
 // TESTS
 //---------------------------------------------------------------------------//
 
-void token_test()
+void token_test( UnitTest & ut )
 {
     Token end_token  (END,               "parser_test 1");
     Token other_token('$',               "parser_test 2");
     Token real_token (REAL,  "+1.56e-3", "parser_test 3");
 
-    if (end_token.Type()!=END) ITFAILS;
-    if (other_token.Type()!=OTHER) ITFAILS;
-    if (real_token.Type()!=REAL) ITFAILS;
+    if (end_token.Type()!=END) ut.failure(__LINE__);
+    if (other_token.Type()!=OTHER) ut.failure(__LINE__);
+    if (real_token.Type()!=REAL) ut.failure(__LINE__);
 
-    if (other_token.Text()!="$") ITFAILS;
-    if (real_token.Text()!="+1.56e-3") ITFAILS;
+    if (other_token.Text()!="$") ut.failure(__LINE__);
+    if (real_token.Text()!="+1.56e-3") ut.failure(__LINE__);
 
-    if (end_token.Location()!="parser_test 1") ITFAILS;
-    if (other_token.Location()!="parser_test 2") ITFAILS;
-    if (real_token.Location()!="parser_test 3") ITFAILS;
+    if (end_token.Location()!="parser_test 1") ut.failure(__LINE__);
+    if (other_token.Location()!="parser_test 2") ut.failure(__LINE__);
+    if (real_token.Location()!="parser_test 3") ut.failure(__LINE__);
 
-    if (!Is_Integer_Text("057133")) ITFAILS;
-    if (Is_Integer_Text("08223")) ITFAILS;
-    if (!Is_Integer_Text("663323")) ITFAILS;
-    if (!Is_Integer_Text("0x33a8")) ITFAILS;
-    if (!Is_Keyword_Text("This is a test")) ITFAILS;
-    if (Is_Keyword_Text("This is 1 test")) ITFAILS;
-    if (!Is_Real_Text("+1.56e-3")) ITFAILS;
-    if (Is_Real_Text("1.39d-3")) ITFAILS;
-    if (!Is_String_Text("\"This is a test.\"")) ITFAILS;
-    if (Is_String_Text("\"This is a test")) ITFAILS;
+    if (!Is_Integer_Text("057133")) ut.failure(__LINE__);
+    if (Is_Integer_Text("08223")) ut.failure(__LINE__);
+    if (!Is_Integer_Text("663323")) ut.failure(__LINE__);
+    if (!Is_Integer_Text("0x33a8")) ut.failure(__LINE__);
+    if (!Is_Keyword_Text("This is a test")) ut.failure(__LINE__);
+    if (Is_Keyword_Text("This is 1 test")) ut.failure(__LINE__);
+    if (!Is_Real_Text("+1.56e-3")) ut.failure(__LINE__);
+    if (Is_Real_Text("1.39d-3")) ut.failure(__LINE__);
+    if (!Is_String_Text("\"This is a test.\"")) ut.failure(__LINE__);
+    if (Is_String_Text("\"This is a test")) ut.failure(__LINE__);
 
     if (real_token == end_token)
     {
-	FAILMSG("unlike token equality test did NOT return false");
+	ut.failure("unlike token equality test did NOT return false");
     }
     else
     {
-	PASSMSG("unlike token equality test returned false");
+	ut.passes("unlike token equality test returned false");
     }
 
     if (real_token == real_token)
     {
-	PASSMSG("like token equality test did returned true");
+	ut.passes("like token equality test did returned true");
     }
     else
     {
-	FAILMSG("like token equality test did NOT return true");
+	ut.failure("like token equality test did NOT return true");
     }
 
     return;
@@ -77,45 +75,36 @@ void token_test()
 
 int main(int argc, char *argv[])
 {
-    rtt_c4::initialize(argc, argv);
-
-    // version tag
-    for (int arg = 1; arg < argc; arg++)
-	if (string(argv[arg]) == "--version")
-	{
-	    cout << argv[0] << ": version " << rtt_parser::release() 
-		 << endl;
-	    return 0;
-	}
-
     try
     {
-	// >>> UNIT TESTS
-	if ( rtt_c4::nodes() == 1 ) token_test();
+        ScalarUnitTest ut( argc, argv, release );
+	token_test( ut );
     }
-    catch (rtt_dsxx::assertion &ass)
+        catch( rtt_dsxx::assertion &err )
     {
-	cout << "While testing tstToken, " << ass.what()
-	     << endl;
-	return 1;
+        std::string msg = err.what();
+        if( msg != std::string( "Success" ) )
+        { cout << "ERROR: While testing " << argv[0] << ", "
+               << err.what() << endl;
+            return 1;
+        }
+        return 0;
     }
-
-    // status of test
-    cout << endl;
-    cout <<     "*********************************************" << endl;
-    if (rtt_parser_test::passed) 
+    catch (exception &err)
     {
-        cout << "**** tstToken Test: PASSED" 
-	     << endl;
+        cout << "ERROR: While testing " << argv[0] << ", "
+             << err.what() << endl;
+        return 1;
     }
-    cout <<     "*********************************************" << endl;
-    cout << endl;
 
-    rtt_c4::global_barrier();
+    catch( ... )
+    {
+        cout << "ERROR: While testing " << argv[0] << ", " 
+             << "An unknown exception was thrown" << endl;
+        return 1;
+    }
 
-    cout << "Done testing tstToken." << endl;
-
-    rtt_c4::finalize();
+    return 0;
 }   
 
 //---------------------------------------------------------------------------//
