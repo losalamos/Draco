b6d7ce87e449ed06bf6ad5b13bf365506e66b4a7
mwbuksas <mwbuksas@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2006-07-24 21:58:40 +0000

    Added a unit test for Power meta-function. Added descriptive documentation and eliminated an un-necesssary helper class.

--- src/special_functions/Power.hh
+++ src/special_functions/Power.hh
@@ -1,77 +1,81 @@
 //----------------------------------*-C++-*----------------------------------//
 /*!
- * \file   ds++/Power.hh
+ * \file   special_functions/Power.hh
  * \author Mike Buksas
  * \date   Thu Jul 20 17:23:31 2006
  * \brief  
  * \note   Copyright © 2006 Los Alamos National Security, LLC
- *
- * Long description.
+
+ Use meta-programming to generate an efficient routine to compute integer
+ powers. 
+
+ E.g.: Power<4>(x) computes the fourth power of x. The function constructed at
+ compile time should be equivalent to the following:
+
+ v1 = x * x
+ v2 = v1 * v1;
+ return v2;
+
+ Assuming that the compiler is inlining aggressively.
+
+ Likewise, Power<7>(x) should generate code equivalent to:
+
+ v1 = x * x
+ v2 = v1 * x;
+ v3 = v1 * v2;
+ return v3;
+
+ The meta-algorithm is based on the Russian Peasant Algorithm, modified to be
+ recursive, since this is required for template meta-programming in C++.
+ 
  */
 //---------------------------------------------------------------------------//
 // $Id$
 //---------------------------------------------------------------------------//
 
-#ifndef dsxx_Power_hh
-#define dsxx_Power_hh
+#ifndef special_functions_Power_hh
+#define special_functions_Power_hh
 
-namespace rtt_dsxx
+namespace rtt_sf
 {
+
+namespace {
+
+/* Protect the implementation detail of struct P from accidental usage
+ * outside of this file.
+ */
 
 template <int N, typename F>
-struct RP2
+struct P
 {
-
-    static double compute(F x, F p)
+    static F compute(F x, F p)
     {
-
         x *= x;
-        if ((N/2)*2 == N) return RP2<N/2, F>::compute(x, p);
-        else              return RP2<N/2, F>::compute(x, x*p);
-
+        if ((N/2)*2 == N) return P<N/2, F>::compute(x, p);
+        else              return P<N/2, F>::compute(x, x*p);
     }
-
 };
 
-
-template <int N, typename F>
-struct RP
+template <> template <typename F>
+struct P<0,F>
 {
-
-    static double compute(F x)
-    {
-
-        if ((N/2)*2 == N) return RP <N/2, F>::compute(x*x);
-        else              return RP2<N/2, F>::compute(x,x);
-
-    }
-
+    static F compute(F x, F p) { return p; }
 };
 
-template <>
-template <typename F>
-struct RP2<1, F>
-{
-    static double compute(F x, F p) { return x*x*p; }
-};
-    
-template <>
-template <typename F>
-struct RP2<0,F>
-{
-    static double compute(F x, F p) { return p; }
-};
+}
           
 
-
 template <int N, typename F>
-F Power(F x) { return RP<N, F>::compute(x); }
+F Power(F x) {
+    if ((N/2)*2 == N) return Power<N/2>(x*x);
+    else              return P<N/2,F>::compute(x,x);
+}
 
 
-} // end namespace rtt_dsxx
+} // end namespace rtt_sf
 
-#endif // dsxx_Power_hh
+#endif // special_functions_Power_hh
 
 //---------------------------------------------------------------------------//
-//              end of ds++/Power.hh
+//              end of special_functions/Power.hh
 //---------------------------------------------------------------------------//
