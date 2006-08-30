fbec861795dd08ac55ff2ac5e22f4334d5b21a54
tme <tme@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2006-08-30 15:34:38 +0000

    Removed unused temporary to keep PGI from complaining.

--- src/norms/Norms_Index.t.hh
+++ src/norms/Norms_Index.t.hh
@@ -88,7 +88,6 @@ void Norms_Index<Index_t>::comm(const size_t n)
     {
 	// Accumulate the results onto this node.
 	
-	double x;         // temporary for receives
 	Index_t pe_index; // temporary for index
 	
 	for ( size_t i = 0; i < num_nodes; ++ i )
