8b169b83e6a51bb063b646341be9574d8256bddf
warsa <warsa@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2007-03-30 15:07:34 +0000

    The dreaded "=" instead of "==" in an logical check was identified.

--- src/RTT_Format_Reader/Cells.cc
+++ src/RTT_Format_Reader/Cells.cc
@@ -103,6 +103,7 @@ void Cells::redefineCells()
 	vector_int node_map(cellDefs.get_node_map(this_cell_type));
 	Insist(node_map.size() == cellDefs.get_nnodes(this_cell_type),
 	       "Error in Cells redefinition.");
+
 	// Check to see if the nodes need to be rearranged for this cell type.
 	bool redefined = false;
 	for (int n = 0; n < node_map.size(); n ++)
@@ -110,12 +111,13 @@ void Cells::redefineCells()
 	    if (node_map[n] != n)
 	        redefined = true;
 	}
+
 	if (redefined)
 	{
 	    temp_nodes.resize(cellDefs.get_nnodes(this_cell_type));
 	    for (int c = 0; c < dims.get_ncells(); c++)
 	    {
-	        if (cellType[c] = this_cell_type)
+	        if (cellType[c] == this_cell_type)
 		{
 		    for (int n = 0; n < nodes[c].size(); n++)
 		        temp_nodes[node_map[n]] = nodes[c][n];
