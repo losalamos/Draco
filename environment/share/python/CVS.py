8638003fcdc91c4cd8aa4639d70a2967e5b35c0b
mwbuksas <mwbuksas@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2007-10-12 22:13:35 +0000

    'Pitch' is nearly done. It just needs some more testing. Release notice to come shortly.
    Refactored 'checkout' to move functionlity to CVS.py for re-use in
    pitch. Also modified Repo.py to support querying the "name" of the
    repository instead of just it's directory. This is to support filling
    in the argument for the "-d" and "-j" parameters of get_draco.

--- environment/share/python/CVS.py
+++ environment/share/python/CVS.py
@@ -1,3 +1,6 @@
+import exceptions, os
+import Verbosity, Utils
+
 """Package CVS
 
 Facilities for interacting with CVS in Python.
@@ -6,52 +9,213 @@ A Repository objects represents a specific CVS repository. It
 implements commands to check out modules from it.
 """
 
+##---------------------------------------------------------------------------##
 class ArgumentError(Exception):
     "An exception class for inconsistent combinations of arguments."
     pass
 
-import exceptions, os
 
-import Verbosity, Utils
+##---------------------------------------------------------------------------##
+class Tag:
+    """Represents the kinds of tags that we can apply to a package
+    when checking it out.
 
+    Converts itself to a string for composition of CVS commands
+
+    >>> print Tag('r', 'tag_name')
+    -r tag_name
+
+
+    >>> print Tag('x', 'tag_name')
+    Traceback (most recent call last):
+    ...
+    ArgumentError: Unrecognized tag kind: x
+
+
+    >>> t = Tag('D', 'yesterday')
+    >>> t.kind
+    'D'
+    >>> t.name
+    'yesterday'
+
+    >>> str(Tag())
+    '-r HEAD'
+
+    >>> t = Tag.build('some_branch')
+    >>> t.kind
+    'r'
+
+    >>> t = Tag.build('date:sometime')
+    >>> t.kind
+    'D'
+    >>> t.name
+    'sometime'
+
+    """
+
+    kinds = ['h', 'D', 'r']
+
+    # Add a head kind?
+    descriptors = {'date':'D','symbolic':'r'}
+
+    def __init__(self, kind="-r", name=""):
+
+        self.kind = kind
+        self.name = name
+
+        # If no name provided, no tag is intended.
+        if not self.name:
+            self.kind = 'h'
+
+        if (self.name and not self.kind):
+                raise ArgumentError("Must provide kind of tag.");
+
+        if self.kind not in self.kinds:
+            raise ArgumentError(
+                "Unrecognized tag kind: %s" % self.kind)
+
+    def __str__(self): return self.tag_string()
+
+    def tag_string(self):
+
+        if not self.kind:      return "-r HEAD"
+        elif self.kind == 'D': return "-D %s" % self.name
+        elif self.kind == 'r': return "-r %s" % self.name
+        else:                  return "-r HEAD"
+        
+
+    def build(desc, splitter=":"):
+        "Look for keywords in the tag string which indicate the type."
+
+        parts = desc.split(splitter, 1)
+
+        # If splitter not encountered, assume symbolic tag
+        if len(parts) == 1:
+            return Tag('r', desc)
+        else:
+            kind = Utils.complete(parts[0], Tag.descriptors.keys())
+            # If the first word fits exactly one key, use it:
+            if len(kind)==1:
+                return Tag(Tag.descriptors[kind[0]], parts[1])
+            else:
+                return desc
+        
+    build = staticmethod(build)            
+
+
+
+##---------------------------------------------------------------------------##
+class Module:
+    """Represents a module in a CVS repository.
+
+    The directory varaible is used to select a destination name for
+    the result when checked out.
+
+    >>> m = Module('draco/environment', 'environment')
+    >>> m.name
+    'draco'
+    >>> m.module
+    'draco/environment'
+    >>> m.destination
+    'environment'
+    """
+    
+    def __init__(self, module, destination="", tag="" ):
+
+        self.name        = module.split("/",1)[0]
+        self.module      = module
+        self.destination = destination
+        self.tag         = Tag.build(tag)
+
+    def dir_string(self):
+
+        if self.destination:
+            return "-d %s" % self.destination
+        else:
+            return ""
+
+    # TODO Make this spew more stuff:
+    def __str__(self):
+        return self.module
+
+        
+
+
+##---------------------------------------------------------------------------##
 class Repository:
+    """Represents a repository
+
+    >>> r = Repository('/codes/radtran/cvsroot')
+    >>> print r.location
+    /codes/radtran/cvsroot
+
+    """
+    
 
     def __init__(self, repository):
 
-        self.repository = repository
+        self.location = repository
+        assert(os.access(self.location, (os.F_OK | os.R_OK)))
 
-        assert(os.access(self.repository, (os.F_OK | os.R_OK)))
 
-    def checkout(self, package, destination, tag="", date="", directory="",
-                 verbose = Verbosity.ignore()):
 
-        if tag and date:
-            raise ArgumentError("Cannot specify both a tag and date"
-                                "for checkout")
+
+##---------------------------------------------------------------------------##
+def checkout(repository,
+             module,
+             location,
+             export  = False,
+             verbose = Verbosity.ignore()
+             ):
+    """Function checkout implemenets the checkout and export cvs
+    commands given a description of the desired module, the repository
+    it comes from, and where the result should go.
+    """
+
+    cvs_command = export and "export" or "checkout"
+
+    command = "cvs -d %s %s %s %s %s" % \
+              (repository.location,
+               cvs_command,
+               module.dir_string(),
+               module.tag.tag_string(),
+               module.module
+               )
+
+    verbose("Executing CVS command: %s" % command, 1)
+    verbose("in directory %s" % location, 2)
+
+    
+    # Switch to the indicated directory.
+    try:
+        os.chdir(location)
+    except OSError:
+        sys.exit("Could not find directory %s" % location)
             
-        if   tag:  version = "-r %s" % tag
-        elif date: version = "-D %s" % date
-        else:      version = ""
+    command_out = os.popen(command)
+    output = command_out.read()
+    error_code = command_out.close()
+    
+    if error_code:
+        raise exceptions.RuntimeError(
+            "CVS command failed with error: %s" % error_code)
 
-        if directory: directory = "-d %s" % directory
+    # Figure out what directory we just created. If we used a module
+    # destination, return that appended to the location. Otherwise,
+    # append the module path to the location and return that
 
-        command = "cvs -d %s checkout %s %s %s" % \
-                  (self.repository, directory, version, package)
+    new_path = module.destination or module.module
+    path     = os.path.join(location, new_path);
 
-        verbose("Executing cvs command: %s" % command, 1)
+    assert(os.access(path, os.F_OK | os.R_OK))
 
-        # Switch to the indicated directory.
-        try:
-            os.chdir(destination)
-        except OSError:
-            sys.exit("Could not find directory %s" % destination)
-            
-        command_out = os.popen(command)
-        output = command_out.read()
-        error_code = command_out.close()
+    return path
 
-        if error_code:
-            raise exceptions.RuntimeError( \
-                "CVS command failed with error: %s" % error_code)
 
-        
+def _test():
+    import doctest, CVS
+    return doctest.testmod(CVS)
+
+
+if __name__=="__main__":
+    _test()
--- environment/share/python/Repo.py
+++ environment/share/python/Repo.py
@@ -1,5 +1,6 @@
 
 import os, tarfile, os.path
+from Utils import disambiguate, AmbiguousKeyError, InvalidKeyError, padlist
 
 
 """Repo
@@ -14,19 +15,41 @@ information through a configuration file.
 Where should this information live?
 
 """
+
+class RepoError(Exception):
+    """An Exception Class for errors relating to the known repositiories"""
+
 ##---------------------------------------------------------------------------##
 ## Repository Information:
 ##---------------------------------------------------------------------------##
-PACKAGES = {'draco'       : "/codes/radtran/cvsroot",
-            'tools'       : "/codes/radtran/cvsroot",
-            'imcdoc'      : "/codes/radtran/cvsroot",
-            'clubimc'     : "/codes/radtran/cvsroot",
-            'milagro'     : "/codes/radtran/cvsroot",
-            'wedgehog'    : "/codes/radtran/cvsroot",
-            'uncleMcFlux' : "/codes/radtran/cvsroot"}
+REPOS = {'draco'   : "/codes/radtran/cvsroot",
+         'jayenne' : "/codes/radtran/cvsroot"
+         }
 
-def package_list():
-    return PACKAGES.keys()
+PACKAGES = {'draco'       : "draco",
+            'tools'       : "draco",
+            'imcdoc'      : "jayenne",
+            'clubimc'     : "jayenne",
+            'milagro'     : "jayenne",
+            'wedgehog'    : "jayenne",
+            'uncleMcFlux' : "jayenne"}
+
+DEPENDS = {'wedgehog'    : ['clubimc','draco'],
+           'milagro'     : ['clubimc','draco'],
+           'uncleMcFlux' : ['clubimc','draco'],
+           'clubimc'     : ['draco'],
+           'draco'       : [],
+           'tools'       : [],
+           'imcdoc'      : []
+           }
+           
+
+
+def package_list(): return PACKAGES.keys()
+
+def disambiguate_component(component):
+
+    return disambiguate(component, PACKAGES.keys())
 
 
 ##---------------------------------------------------------------------------##
@@ -35,13 +58,63 @@ def is_valid_package(package):
     return package in PACKAGES
 
 ##---------------------------------------------------------------------------##
+def is_valid_repository(repo):
+    "Is 'repo' a repositiry we know about?"
+    return repo in REPOS
+
+##---------------------------------------------------------------------------##
+def get_cvs_dir(package):
+    """Get the path of the cvs reposistory containing the given
+    package
+
+    >>> get_cvs_dir('draco')
+    '/codes/radtran/cvsroot'
+
+    """
+
+    assert(is_valid_package(package))
+
+    return REPOS[PACKAGES[package]]
+
+
+##---------------------------------------------------------------------------##
 def get_cvs_repo(package):
-    "Get the path of the cvs reposistory containing the given package"
+    """Get the repository name that a package can be found in.
+
+    >>> get_cvs_repo('uncleMcFlux')
+    'jayenne'
+
+    """
 
     assert(is_valid_package(package))
 
     return PACKAGES[package]
 
+##---------------------------------------------------------------------------##
+def get_cvs_repo_dir(repo):
+    """The the directory for a particular repository
+
+    >>> get_cvs_repo_dir('jayenne')
+    '/codes/radtran/cvsroot'
+
+    """
+
+    assert(is_valid_repository(repo))
+
+    return REPOS[repo]
+
+##---------------------------------------------------------------------------##
+def get_depends(package):
+    """Get a list of packages the given package depends on.
+
+    >>> get_depends('wedgehog')
+    ['clubimc', 'draco']
+
+    """
+
+    assert(is_valid_package(package))
+
+    return DEPENDS[package]
 
 ##---------------------------------------------------------------------------##
 def get_cvs_path(package):
@@ -61,7 +134,7 @@ def get_cvs_path(package):
 
     assert(is_valid_package(package))
 
-    repo_path =  os.path.join(PACKAGES[package], package)
+    repo_path =  os.path.join(get_cvs_dir(package), package)
 
     # Verify the existence and readability of the directory:
     assert(os.access(repo_path, os.F_OK | os.R_OK))
