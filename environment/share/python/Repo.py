fdd7b4bb945042c6f8d572ee508fa4aaa2001db6
mwbuksas <mwbuksas@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2007-10-29 15:20:27 +0000

    Moved some unrelated functionality out of the Repo package.

--- environment/share/python/Repo.py
+++ environment/share/python/Repo.py
@@ -5,14 +5,11 @@ from Utils import disambiguate, AmbiguousKeyError, InvalidKeyError, padlist
 
 """Repo
 
-A module of functions to make getting tarfiles of the cvs repostories
-easier.
+A module of functions which describe the repository structure for
+CCS-2 radiation transport codes.
 
-This module contains information specific to CCS-4 repositories and
-filesystem. This should be made more general, by specifying this
-information through a configuration file.
-
-Where should this information live?
+This module should be split into data, read from a configuration file,
+and functionality, which presents the data via functions.
 
 """
 
@@ -142,64 +139,9 @@ def get_cvs_path(package):
     return repo_path
 
 
-
-
 ##---------------------------------------------------------------------------##
-## Tarball creation:
+## Main functions.
 ##---------------------------------------------------------------------------##
-
-
-##---------------------------------------------------------------------------##
-def get_archive_name(package, compress = True):
-
-    """Transform a package name into the name of a tar repository.
-
-    Optional argument indicates gnu-zip compression. Defaults to true.
-
-    >>> get_archive_name("Whatever")
-    'Whatever.tgz'
-
-    >>> get_archive_name("andEver", False)
-    'andEver.tar'
-
-    >>> get_archive_name("Amen", True)
-    'Amen.tgz'
-
-    """
-
-    if compress:
-        extension = ".tgz"
-    else:
-        extension = ".tar"
-
-    return package + extension
-
-
-
-##---------------------------------------------------------------------------##
-def get_tar_file(package, compress, archive_path):
-    """Get a tar file for the specified package.
-
-    If archive_path is non-nil, use it as the location for the
-    archive. Otherwise, it will appear in CWD.
-    """
-
-    assert(is_valid_package(package))
-
-    mode = 'w'
-    if compress: mode += ":gz"
-
-    repository_path = get_cvs_path(package)
-
-    if not archive_path: archive_path = ""
-    archive_name = os.path.join(archive_path, get_archive_name(package, compress))
-
-    tar = tarfile.open(archive_name, mode)
-    tar.add(repository_path, package)
-    tar.close()
-
-    return archive_name
-
 
 def _test():
     import doctest, Repo
