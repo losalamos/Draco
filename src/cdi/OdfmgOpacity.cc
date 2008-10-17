60f0df7df7ddc8f1048e3df29e1097774848cbed
mwbuksas <mwbuksas@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2008-10-17 17:05:00 +0000

    Fixed intentation and relocated an empty function to a less intrusive location.

--- src/cdi/OdfmgOpacity.cc
+++ src/cdi/OdfmgOpacity.cc
@@ -14,16 +14,16 @@
 namespace rtt_cdi
 {
 
-    /*!
-     * \brief Default Opacity() destructor.
-     *
-     * This is required to correctly release memory when any
-     * object derived from OdfmgOpacity is destroyed.
-     */
-     OdfmgOpacity::~OdfmgOpacity()
-	 {
-	     // empty
-	 }
+/*!
+ * \brief Default Opacity() destructor.
+ *
+ * This is required to correctly release memory when any
+ * object derived from OdfmgOpacity is destroyed.
+ */
+OdfmgOpacity::~OdfmgOpacity()
+{
+  // empty
+}
 
 } // end namespace rtt_cdi
 
