113f22f1228d02c6aac651c93ad69f9a6d991835
Kelly (KT) Thompson <kgt@lanl.gov>
2016-05-30 11:58:27 -0600

    Attempt to stand up a style check system within travis-ci.
    The goal of this change is to add an extra check for PRs that compares modified
    code against the .clang-format ruleset.  If nonconformances are found, these are
    printed as a diff and the style check result is 'fail.'  Users can run
    'regression/check_style.sh' manually from their work directory to update code to
    match the style requirements (git-clang-format must be installed locally for
    this to work).
    
    + Add a new shell script, check_style.sh, that will run git-clang-format.
    + Remove some of the rules from .clang-format.
    + Update .travis.yml so that clang-format-3.8 is installed (hopefully) and add
      STYLE as a matrix option. Also, run the check_style.sh script after the
      coverate test is complete.
    + fixes #4

---
+++ regression/check_style.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+# Run clang-format in the current directory and list locally modified
+# files that are not compliant with the current coding standard (see
+# .clang_format in the top level source directory.)
+
+##---------------------------------------------------------------------------##
+## Environment
+##---------------------------------------------------------------------------##
+
+# Enable job control
+set -m
+
+##---------------------------------------------------------------------------##
+## Support functions
+##---------------------------------------------------------------------------##
+print_use()
+{
+    echo " "
+    echo "Usage: `basename $0` -d -n -t"
+    echo " "
+    echo "All arguments are optional."
+    echo "  -d Diff mode only. Do not modifty files."
+    echo "  -n Alias for -d."
+    echo "  -t Run as a pre-commit check, print list of non-conformant files and return"
+    echo "     with exit code = 1."
+    echo " "
+}
+
+##---------------------------------------------------------------------------##
+## Sanity Checks
+##---------------------------------------------------------------------------##
+
+# clang-format must be in the PATH
+gcf=`which git-clang-format`
+if test "${gcf}notset" = "notset"; then
+   echo "ERROR: git-clang-format was not found in your PATH."
+   exit 1
+fi
+
+##---------------------------------------------------------------------------##
+## Default values
+##---------------------------------------------------------------------------##
+pct_mode=0
+diff_mode=0
+allok=0
+
+##---------------------------------------------------------------------------##
+## Command options
+##---------------------------------------------------------------------------##
+
+while getopts ":dhnt" opt; do
+case $opt in
+d) diff_mode=1 ;;
+h) print_use; exit 0 ;;
+n) diff_mode=1 ;;
+t) pct_mode=1 ;;
+\?) echo "" ;echo "invalid option: -$OPTARG"; print_use; exit 1 ;;
+:)  echo "" ;echo "option -$OPTARG requires an argument."; print_use; exit 1 ;;
+esac
+done
+
+##---------------------------------------------------------------------------##
+## Check mode
+##---------------------------------------------------------------------------##
+
+if test "${pct_mode}" = "1"; then
+
+  # don't actually modify the files:
+  cmd='git-clang-format -f --diff --extensions hh,cc'
+  result=`eval $cmd`
+  allok=`echo $result | grep "did not modify" | wc -l`
+
+  if test $allok = 1; then
+    echo "PASS: Changes conform to draco style requirements."
+  else
+    echo "FAIL: some files do not conform to draco style requirements:"
+    echo " "
+    # rerun the command to capture color output.
+    eval $cmd
+    exit 1
+  fi
+
+##---------------------------------------------------------------------------##
+## Fix mode
+##   no options --> fix the files by running clang-format
+##   -d | -n    --> print diff of required changes.
+##---------------------------------------------------------------------------##
+
+else
+
+  if test ${diff_mode} = 1; then
+    cmd='git-clang-format -f --diff --extensions hh,cc'
+    result=`eval $cmd`
+    echo "The following non-conformances were discovered. Rerun without -d|-n to"
+    echo "automatically apply these changes:"
+    echo " "
+    # rerun command to capture color output.
+    eval $cmd
+  else
+    result=`git-clang-format -f --extensions hh,cc`
+    nonconformantfilesfound=`echo $result | grep "changed files" | wc -l`
+    echo "The following files in your working directory were modified to meet the draco"
+    echo "style requirement:"
+    echo " "
+    echo $result
+  fi
+
+fi
+
+##---------------------------------------------------------------------------##
+## End check_style.sh
+##---------------------------------------------------------------------------##
