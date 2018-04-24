3cff4aad7be1e0f02fb103b1dc126e9330011ce6
Kelly (KT) Thompson <kgt@lanl.gov>
2018-04-24 17:51:08 -0600

    Possible fix for Travis failure.

--- .travis-run-tests.sh
+++ .travis-run-tests.sh
@@ -35,8 +35,10 @@ else
   # configure
   # -Wl,--no-as-needed is a workaround for bugs.debian.org/457284 .
   echo " "
-  echo "${CMAKE} -DCMAKE_EXE_LINKER_FLAGS=\" -Wl,--no-as-needed \" .."
-  ${CMAKE} -DCMAKE_EXE_LINKER_FLAGS=" -Wl,--no-as-needed " ..
+  #echo "${CMAKE} -DCMAKE_EXE_LINKER_FLAGS=\" -Wl,--no-as-needed \" .."
+  #${CMAKE} -DCMAKE_EXE_LINKER_FLAGS=" -Wl,--no-as-needed " ..
+  echo "${CMAKE} .."
+  ${CMAKE} ..
   error_code=$?
   # if configure was successful, the start the build, otherwise abort.
   if [[ $error_code -eq 0 ]]; then
