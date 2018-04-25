bd8b892ab1fbc3cb833e4e21c597abc03b398a01
Kelly (KT) Thompson <kgt@lanl.gov>
2018-04-25 12:43:41 -0600

    Only update a few packages instead of all 172.

--- .travis-install-dependencies.sh
+++ .travis-install-dependencies.sh
@@ -66,7 +66,7 @@ if [[ ${STYLE} ]]; then
 else
 
   run "sudo apt-get update"
-  run "sudo apt-get -y upgrade"
+  run "sudo apt-get -y upgrade libc-bin libc-dev-bin libc6 libc6-dev linux-libc-dev"
   run "ls -aFl /usr/lib/libgslcblas.so"
   run "ls -aFl /usr/lib/libgslcblas.so.0.0.0"
   run "nm -a /usr/lib/libgslcblas.so | grep cblas_ctrmv"
