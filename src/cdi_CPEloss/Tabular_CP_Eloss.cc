9e4e51043b0ac8ee6ee790c978d10a1824fa8f97
Ben Ryan <brryan@lanl.gov>
2019-11-04 17:40:23 -0700

    Initial commit

--- src/cdi_CPEloss/Tabular_CP_Eloss.cc
+++ src/cdi_CPEloss/Tabular_CP_Eloss.cc
@@ -1,1 +1,130 @@
-//
+//----------------------------------*-C++-*----------------------------------//
+/*!                                                                              
+ * \file   cdi_CPEloss/Tabular_CP_Eloss.cc                                     
+ * \author Ben R. Ryan                                                        
+ * \date   2019 Nov 4                                             
+ * \brief  Tabular_CP_Eloss member definitions.                                 
+ * \note   Copyright (C) 2016-2019 Triad National Security, LLC.                 
+ *         All rights reserved. */
+//---------------------------------------------------------------------------//
+
+#include "Tabular_CP_Eloss.hh"
+
+namespace rtt_cdi_cpeloss {
+
+//---------------------------------------------------------------------------//
+// CONSTRUCTORS
+//---------------------------------------------------------------------------//
+/*!
+ * \brief Constructor for an analytic tabular eloss model. 
+ *
+ * This constructor builds an eloss model defined by the
+ * rtt_cdi_cpeloss::Tabular_Eloss_Model derived class argument.
+ *
+ * The path to an eloss datafile is passed to the constructor,
+ * which opens and parses the file. The file format is the usual
+ * LANL format for stopping powers. 
+ *
+ * \param filename_in path to eloss file
+ * \param target_zaid_in target particle zaid
+ * \param projectile_zaid_in transporting particle zaid
+ */
+Tabular_CP_Eloss::Tabular_CP_Eloss(std::string filename_in,
+                                   int32_t target_zaid_in,
+                                   int32_t projectile_zaid_in)
+    : filename(filename_in), target_zaid(target_zaid_in),
+      projectile_zaid(projectile_zaid_in) {
+  file.open(filename);
+  if (!file.is_open()) {
+    std::cout << "Eloss file " << filename << " could not be opened!"
+              << std::endl;
+    exit(-1);
+  }
+
+  std::string line;
+  std::vector<std::string> line_entries;
+  int nlines;
+  int max_entries =
+      6; // This is a statement about the file format, maximum of six entries for row.
+  
+  read_line(); // ZAID
+  int32_t projectile_zaid_file = stoi(line_entries[0]);
+  Require(projectile_zaid == projectile_zaid_file);
+
+  read_line(); // Z, A, mass
+  
+  line_entries = read_line(); // Number of bins for energy, density, temperature
+  nbin_energy = stoi(line_entries[0]);
+  nbin_density = stoi(line_entries[1]);
+  nbin_temperature = stoi(line_entries[2]);
+  
+  line_entries = read_line(); // Bin spacing for energy, density, temperature (log)
+  d_log_energy = 1./stod(line_entries[0]);
+  d_log_density = 1./stod(line_entries[1]);
+  d_log_temperature = 1./stod(line_entries[2]);
+  
+  // Get first energy support point
+  nlines = std::ceil((double)nbin_energy/max_entries);
+  line_entries = read_line();
+  min_log_energy = stod(line_entries[0]);
+  for (int n = 0; n < nlines - 1; n++) {
+    read_line();
+  }
+
+  // Get first density support point
+  nlines = std::ceil((double)nbin_density/max_entries);
+  line_entries = read_line();
+  min_log_density = stod(line_entries[0]);
+  for (int n = 0; n < nlines - 1; n++) {
+    read_line();
+  }
+  
+  // Get first temperature support point
+  nlines = std::ceil((double)nbin_temperature/max_entries);
+  line_entries = read_line();
+  min_log_temperature = stod(line_entries[0]);
+  for (int n = 0; n < nlines - 1; n++) {
+    read_line();
+  }
+}
+
+/*!
+ * \brief Split a line from an eloss datafile into a vector of strings.
+ *
+ * Convenience function, especially when reading header data which is not
+ * uniform in the number of entries per line, or the types of those entries.
+ *
+ * \param line the line read directly from the eloss datafile.
+ * \return entries the resulting vector of entries in the datafile line.
+ *
+ */
+std::vector<std::string> Tabular_CP_Eloss::split_eloss_line(std::string line) {
+  std::vector<std::string> entries;
+  std::istringstream iss(line);
+  for (std::string s; iss >> s;) {
+    entries.push_back(s);
+  }
+  return entries;
+}
+
+/*!
+ * \brief Read a line from an eloss datafile and return as a vector of strings.
+ *
+ * Convenience function, especially when reading header data which is not
+ * uniform in the number of entries per line, or the types of those entries.
+ *
+ * \return entries the resulting vector of entries in the datafile line.
+ *
+ */
+std::vector<std::string> Tabular_CP_Eloss::read_line() {
+  std::string line;
+  getline(file, line);
+  std::vector<std::string> entries;
+  std::istringstream iss(line);
+  for (std::string s; iss >> s;) {
+    entries.push_back(s);
+  }
+  return entries;
+}
+
+} // namespace rtt_cdi_cpeloss
--- src/cdi_CPEloss/Tabular_CP_Eloss.hh
+++ src/cdi_CPEloss/Tabular_CP_Eloss.hh
@@ -1,1 +1,74 @@
-//
+//----------------------------------*-C++-*----------------------------------//  
+/*!                                                                              
+ * \file   cdi_CPEloss/Tabular_CP_Eloss.hh                                     
+ * \author Ben R. Ryan                                                        
+ * \date   2019 Nov 4                                              
+ * \brief  Tabular_CP_Eloss class definition.                                   
+ * \note   Copyright (C) 2016-2019 Triad National Security, LLC.                 
+ *         All rights reserved. */                                               
+//---------------------------------------------------------------------------//
+
+#ifndef __cdi_CPEloss_Tabular_CP_Eloss_hh__
+#define __cdi_CPEloss_Tabular_CP_Eloss_hh__
+
+#include "cdi/CPEloss.hh"
+#include "ds++/DracoMath.hh"
+#include <cmath>
+#include <vector>
+
+namespace rtt_cdi_cpeloss {
+//===========================================================================//  
+/*!                                                                              
+ * \class Tabular_CP_Eloss                                                      
+ *                                                                               
+ * \brief Derived rtt_cdi::CPEloss class for tabular eloss.                     
+ *        This class implements the interface found in cdi/CPEloss.hh for        
+ *        the case where CP energy loss data is in tabular form, stored 
+ *        in a file.                
+ *                                                                               
+ */                                                                              
+//===========================================================================//
+
+class Tabular_CP_Eloss : public rtt_cdi::CPEloss {
+  public:
+    // Useful typedefs
+    typedef std::stod stod;
+    typedef std::stoi stoi;
+
+  private:
+  std::string filename;
+  std::ifstream file;
+
+  // Particle type being transported
+  int32_t projectile_zaid;
+
+  // Target species
+  int32_t target_zaid;
+
+  int32_t nbin_energy; // Number of bins in projectile energy
+  int32_t nbin_density; // Number of bins in target density
+  int32_t nbin_temperature; // Number of bins in target temperature
+  double d_log_energy; // Width of projectile energy bin in log space
+  double d_log_density; // Width of target density bin in log space
+  double d_log_temperature; // Width of target temperature bin in log space
+  double min_log_energy; // Log of minimum projectile energy
+  double min_log_density; // Log of minimum target density
+  double min_log_temperature; // Log of minimum target temperature
+
+  // Utility for reading a line of an eloss file and as a vector of strings
+  std::vector<std::string> Tabular_CP_Eloss::read_line();
+
+  public:
+  // Constructor
+  Tabular_CP_Eloss(std::string filename_in, int32_t target_zaid_in, int32_t projectile_zaid_in);
+
+  double getEloss(const double temperature, const double density, const double v0) const;
+
+  int32_t getTargetZAID() const { return target_zaid; }
+
+  int32_t getProjectileZAID() const { return projectile_zaid; }
+}
+
+} // namespace rtt_cdi_cpeloss
+
+#endif 
