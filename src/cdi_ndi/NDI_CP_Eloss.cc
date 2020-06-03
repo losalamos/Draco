a373e0eb34f01b56642b34ea1681fd94c36e69bd
Ben R. Ryan <brryan@lanl.gov>
2020-06-03 10:48:47 -0600

    A bit further

---
+++ src/cdi_ndi/NDI_CP_Eloss.cc
@@ -0,0 +1,78 @@
+//----------------------------------*-C++-*-----------------------------------//
+/*!
+ * \file   cdi_ndi/NDI_CP_Eloss.cc
+ * \author Ben R. Ryan
+ * \date   2020 Jun 3
+ * \brief  NDI_CP_Eloss member definitions.
+ * \note   Copyright (C) 2019-2020 Triad National Security, LLC.
+ *         All rights reserved. */
+//----------------------------------------------------------------------------//
+
+#include "NDI_CP_Eloss.hh"
+#include "ds++/DracoStrings.hh"
+
+namespace rtt_cdi_ndi {
+
+// Protect actual NDI calls with NDI_FOUND macro:
+#ifdef NDI_FOUND
+//----------------------------------------------------------------------------//
+// CONSTRUCTORS
+//----------------------------------------------------------------------------//
+/*!
+ * \brief Constructor for NDI reader specific to TN DEDX data with provided path
+ *        to gendir file.
+ *
+ * \param[in] gendir_in path to gendir file
+ * \param[in] library_in name of requested NDI data library
+ * \param[in] reaction_in name of requested reaction
+ * \param[in] mg_e_bounds_in energy boundaries of multigroup bins (keV)
+ */
+NDI_CP_Eloss::NDI_CP_Eloss(const std::string &gendir_in,
+                               const std::string &library_in,
+                               rtt_cdi::CParticle target_in,
+                               rtt_cdi::CParticle projectile_in)
+    : NDI_Base(gendir_in, "dedx", library_in, std::string(), std::vector<double>()),
+      rtt_cdi::CPEloss(target_in,
+      projectile_in, rtt_cdi::CPModelType::TABULAR_ETYPE,
+      rtt_cdi::CPModelAngleCutoff::NONE) {
+  // Set angle cutoff by parsing library_in
+
+  load_ndi();
+}
+/*!
+ * \brief Constructor for NDI reader specific to TN DEDX data using default
+ *        gendir file.
+ *
+ * \param[in] library_in name of requested NDI data library
+ * \param[in] reaction_in name of requested reaction
+ * \param[in] mg_e_bounds_in energy boundaries of multigroup bins (keV)
+ */
+NDI_CP_Eloss::NDI_CP_Eloss(const std::string &library_in,
+                               rtt_cdi::CParticle target_in,
+                               rtt_cdi::CParticle projectile_in)
+    : NDI_Base("dedx", library_in, std::string(), std::vector<double>()),
+      rtt_cdi::CPEloss(target_in,
+      projectile_in, rtt_cdi::CPModelType::TABULAR_ETYPE,
+      rtt_cdi::CPModelAngleCutoff::NONE) {
+  // Set angle cutoff by parsing library_in
+
+  load_ndi();
+}
+//----------------------------------------------------------------------------//
+/*!
+ * \brief Load NDI dataset.
+ *
+ * This function opens an NDI file, navigates to the appropriate dataset, reads
+ * the data into internal buffers, and closes the file. For more details on NDI,
+ * see https://xweb.lanl.gov/projects/data/nuclear/ndi/ndi.html
+ */
+void NDI_CP_Eloss::load_ndi() {
+  printf("[%s : %i] not implemented!\n", __FILE__, __LINE__);
+  exit(-1);
+}
+#endif // NDI_FOUND
+} // namespace rtt_cdi_ndi
+
+//----------------------------------------------------------------------------//
+// End cdi_ndi/NDI_CP_Eloss.cc
+//----------------------------------------------------------------------------//
---
+++ src/cdi_ndi/NDI_CP_Eloss.hh
@@ -0,0 +1,120 @@
+//----------------------------------*-C++-*-----------------------------------//
+/*!
+ * \file   cdi_ndi/NDI_CP_Eloss.hh
+ * \author Ben R. Ryan
+ * \date   2020 Jun 3
+ * \brief  NDI_CP_Eloss class definition.
+ * \note   Copyright (C) 2019-2020 Triad National Security, LLC.
+ *         All rights reserved. */
+//----------------------------------------------------------------------------//
+
+#ifndef cdi_ndi_NDI_CP_Eloss_hh
+#define cdi_ndi_NDI_CP_Eloss_hh
+
+#include "NDI_Base.hh"
+#include "rtt_cdi/CPEloss.hh"
+
+namespace rtt_cdi_ndi {
+
+//============================================================================//
+/*!
+ * \class NDI_CP_Eloss
+ *
+ * \brief Derived rtt_cdi::CPEloss class for tabular eloss.
+ *        This class implements the interface found in cdi/CPEloss.hh for
+ *        the case where CP energy loss data is in tabular form, retrieved
+ *        from NDI.
+ */
+//============================================================================//
+
+class NDI_CP_Eloss : public rtt_cdi::CPEloss, public NDI_Base {
+public:
+  typedef std::vector<double> sf_double;
+
+public:
+  //! Constructor (default gendir path)
+  NDI_CP_Eloss(const std::string &library_in, rtt_cdi::CParticle target_in,
+               rtt_cdi::CParticle projectile_in);
+
+  //! Constructor (overridden gendir path)
+  NDI_CP_Eloss(const std::string &gendir_in, const std::string &library_in,
+               rtt_cdi::CParticle target_in, rtt_cdi::CParticle projectile_in);
+
+  //! Disable default constructor
+  NDI_CP_Eloss() = delete;
+
+  //! Disable copy constructor
+  NDI_CP_Eloss(const NDI_CP_Eloss &) = delete;
+
+  // >>> ACCESSORS
+
+  //! Get a stopping power.
+  double getEloss(const double temperature, const double density,
+                  const double v0) const;
+
+  //! Query to see if data is in tabular or functional form (true)
+  static constexpr bool is_data_in_tabular_form() { return true; }
+
+  //! Get the material temperature grid.
+  sf_double getTemperatureGrid() const { return temperatures; }
+
+  //! Get the material density grid.
+  sf_double getDensityGrid() const { return densities; }
+
+  //! Get the projectile energy grid.
+  sf_double getEnergyGrid() const { return energies; }
+
+  //! Get the number of material temperature grid points.
+  size_t getNumTemperatures() const { return n_temperature; }
+
+  //! Get the number of material density grid points.
+  size_t getNumDensities() const { return n_density; }
+
+  //! Get the number of projectile energy grid points.
+  size_t getNumEnergies() const { return n_energy; }
+
+private:
+// Only implemented if NDI is found
+#ifdef NDI_FOUND
+  void load_ndi();
+#endif
+
+private:
+  rtt_units::PhysicalConstexprs<rtt_units::CGS> pc; //!< Units
+
+  uint32_t n_density;         //!< Number of gridpoints in target density
+  uint32_t n_temperature;     //!< Number of gridpoints in target temperature
+  double d_log_energy;        //!< Log spacing of projectile energy gridpoints
+  double d_log_density;       //!< Log spacing of target density gridpoints
+  double d_log_temperature;   //!< Log spacing of target temperature gridpoints
+  double min_log_energy;      //!< Log of minimum projectile energy
+  double min_log_density;     //!< Log of minimum target density
+  double min_log_temperature; //!< Log of minimum target temperature
+  double min_energy;          //!< Minimum target energy
+  double max_energy;          //!< Maximum target energy
+  double min_density;         //!< Minimum target density
+  double max_density;         //!< Maximum target density
+  double min_temperature;     //!< Minimum target temperature
+  double max_temperature;     //!< Maximum target temperature
+  sf_double energies;         //!< Vector of energy gridpoints
+  sf_double densities;        //!< Vector of density gridpoints
+  sf_double temperatures;     //!< Vector of temperature gridpoints
+  // Note that after unit conversions, *_energy is really *_speed
+
+  //! Storage for tabulated data
+  sf_double stopping_data_1d;
+
+  //! Path to gendir file, which indexes an NDI dataset
+  std::string gendir;
+
+  //! Name of library in which to find data
+  const std::string library;
+}
+
+} // namespace rtt_cdi_ndi
+
+#endif // cdi_ndi_NDI_CP_Eloss_hh
+
+//----------------------------------------------------------------------------//
+// End cdi_ndi/NDI_CP_Eloss.hh
+//----------------------------------------------------------------------------//
