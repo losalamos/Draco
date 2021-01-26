9daa7b422ec95afdc5d490dfee7ba3b7c9555a65
Kelly (KT) Thompson <kgt@lanl.gov>
2021-01-26 11:56:52 -0700

    Update release scripts for +gpu builds.
    + Some systems needed extra settings for successful installation of Draco.
    + Request longer allocation times for gpu enabled testing.
    + Use less parallelism when running tests that need access to a GPU.
    + Leave MPI_ROOT set as this is needed by SMPI+LSF environments.

--- tools/ats2-release.sh
+++ tools/ats2-release.sh
@@ -5,7 +5,7 @@ echo -e "\nConfigure, build, and test $package for $buildflavor-$rttversion."
 echo
 export steps="config build test"
 export logfile="$source_prefix/logs/release-$buildflavor-$rttversion-cbt${jobnameext}.log"
-cmd="bsub $job_launch_options -nnodes 1 -W 4:00 \
+cmd="bsub $job_launch_options -nnodes 1 -W 8:00 \
 -J rel_${package:0:3}-${buildflavor: -12}-$rttversion -o $logfile -e $logfile"
 echo "$cmd $draco_script_dir/release.msub"
 jobid=`$cmd $draco_script_dir/release.msub`
