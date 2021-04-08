5a8c7a9ca2e76e0e6d80b68cafd711ac32aeadfc
Mathew Cleveland <cleveland@lanl.gov>
2021-04-08 12:16:12 -0600

    First compiling version of quick_index.

--- src/kde/quick_index.hh
+++ src/kde/quick_index.hh
@@ -14,6 +14,9 @@
 #define rtt_kde_quick_index_hh
 
 #include "c4/global.hh"
+#include <array>
+#include <map>
+#include <vector>
 
 namespace rtt_kde {
 
@@ -28,12 +31,11 @@ namespace rtt_kde {
  */
 //================================================================================================//
 
-class template <size_t dim> quick_index {
+template <size_t dim> class quick_index {
 public:
   //! Default constructors.
-  quick_index(const double std::vector<std::array<3, double>> &locations,
-              const double max_window_size, const size_t bins_per_dimension,
-              const bool domain_decomposed);
+  quick_index(const std::vector<std::array<double, 3>> &locations, const double max_window_size,
+              const size_t bins_per_dimension, const bool domain_decomposed);
 
   // PUBLIC DATA
   // Quick index initialization data
@@ -41,36 +43,33 @@ public:
   const size_t coarse_bin_resolution;
   const double max_window_size;
   // keep a copy of the locations
-  const std::vector<std::array<3, double>> locations;
+  const std::vector<std::array<double, 3>> locations;
   const size_t n_locations;
 
   // Global bounds
-  std::array<3, double> bounding_box_min;
-  std::array<3, double> bounding_box_max;
+  std::array<double, 3> bounding_box_min;
+  std::array<double, 3> bounding_box_max;
   // Local Data map
   std::map<size_t, std::vector<size_t>> coarse_index_map;
 
   // DOMAIN DECOMPOSED DATA
   // Local bounds
-  std::array<3, double> local_bounding_box_min;
-  std::array<3, double> local_bounding_box_max;
+  std::array<double, 3> local_bounding_box_min;
+  std::array<double, 3> local_bounding_box_max;
   // Ordered list of local bins (indexes values are based on the global bin structure)
   std::vector<size_t> local_bins;
   // Size of ghost data buffer
   size_t local_ghost_buffer_size;
   // Map used to index into a local ghost buffer
-  std::map < size_t, std::vector<size_t> local_ghost_index_map;
+  std::map<size_t, std::vector<size_t>> local_ghost_index_map;
   // Local ghost locations (build at construction time)
-  std::vector<std::array<3, double>> local_ghost_locations;
+  std::vector<std::array<double, 3>> local_ghost_locations;
 
 private:
   // PRIVATE DATA
   // Map used to write local data to other processor ghost cells
-  // put_window_map.first -> global_bin_id
-  // put_window_map.second.first -> put_rank
-  // put_window_map.second.second.first -> ghost_proc_buffer_size
-  // put_window_map.second.second.second -> ghost_proc_put_offset
-  std::map<size_t, std::pair<size_t, std::pair<size_t, size_t>>> put_window_map;
+  // put_window_map[global_id] = [put_rank, ghost_proc_buffer_size, ghost_proc_put_offset]
+  std::map<size_t, std::array<size_t, 3>> put_window_map;
 };
 
 } // end namespace  rtt_kde
--- src/kde/quick_index.i.hh
+++ src/kde/quick_index.i.hh
@@ -33,21 +33,22 @@ namespace rtt_kde {
  * \param[in] domain_decomposed
  */
 template <size_t dim>
-quick_index<dim>::quick_index(const std::vector<std::array<3, double>> locations_,
+quick_index<dim>::quick_index(const std::vector<std::array<double, 3>> &locations_,
                               const double max_window_size_, const size_t bins_per_dimension_,
-                              const bool domain_decompsed_)
+                              const bool domain_decomposed_)
     : domain_decomposed(domain_decomposed_), coarse_bin_resolution(bins_per_dimension_),
       max_window_size(max_window_size_), locations(locations_), n_locations(locations_.size()) {
 
   // Build local bounding box
   bounding_box_min = {1e20, 1e20, 1e20};
   bounding_box_max = {-1e20, -1e20, -1e20};
-  for (auto locItr = locations.begin(); loc != locations.end(); locItr++) {
+  for (auto locItr = locations.begin(); locItr != locations.end(); locItr++) {
+    auto &loc = *locItr;
     for (size_t d = 0; d < dim; d++) {
-      if (*locItr[d] < bounding_box_min[d])
-        bound_box_min[d] = *locIter[d];
-      if (*locItr[d] > bounding_box_max[d])
-        bound_box_max[d] = *locIter[d];
+      if (loc[d] < bounding_box_min[d])
+        bounding_box_min[d] = loc[d];
+      if (loc[d] > bounding_box_max[d])
+        bounding_box_max[d] = loc[d];
     }
   }
 
@@ -56,25 +57,26 @@ quick_index<dim>::quick_index(const std::vector<std::array<3, double>> locations
     local_bounding_box_min = bounding_box_min;
     local_bounding_box_max = bounding_box_max;
     for (size_t d = 0; d < dim; d++) {
-      local_bounding_box_min[d] -= window_size * 0.5;
-      local_bounding_box_max[d] += window_size * 0.5;
+      local_bounding_box_min[d] -= max_window_size * 0.5;
+      local_bounding_box_max[d] += max_window_size * 0.5;
     }
     // Global reduce to get the global min and max
-    rtt_c4::global_min(bounding_box_min, 3);
-    rtt_c4::global_max(bounding_box_max, 3);
+    rtt_c4::global_min(&bounding_box_min[0], 3);
+    rtt_c4::global_max(&bounding_box_max[0], 3);
   }
 
   // build up the local hash table of into global bins
-  locIndex = 0;
-  for (auto locItr = locations.begin(); loc != locations.end(); locIter++) {
-    std::array<3, size_t> index{0, 0, 0};
+  size_t locIndex = 0;
+  for (auto locItr = locations.begin(); locItr != locations.end(); locItr++) {
+    auto &loc = *locItr;
+    std::array<size_t, 3> index{0UL, 0UL, 0UL};
     for (size_t d = 0; d < dim; d++)
-      index[d] = floor(coarse_bin_resolution * (*locIter[d] - bounding_box_min[d]) /
+      index[d] = floor(coarse_bin_resolution * (loc[d] - bounding_box_min[d]) /
                        (bounding_box_max[d] - bounding_box_min[d]));
     // build up the local index hash
     const size_t global_index = index[0] + index[1] * coarse_bin_resolution +
-                                index[2] * coarse_bin_resoltuion * coarse_bin_resolution;
-    coarse_index_map[global_index].append(locIndex);
+                                index[2] * coarse_bin_resolution * coarse_bin_resolution;
+    coarse_index_map[global_index].push_back(locIndex);
     locIndex++;
   }
 
@@ -83,15 +85,17 @@ quick_index<dim>::quick_index(const std::vector<std::array<3, double>> locations
 
     // calculate the global index range that each processor needs to
     // accommodate the specified data window size
-    std::array<3, size_t> index_min = {0, 0, 0};
-    std::array<3, size_t> index_max = {0, 0, 0};
-    std::array<3, size_t> index_size = {0, 0, 0};
+    std::array<size_t, 3> index_min = {0UL, 0UL, 0UL};
+    std::array<size_t, 3> index_max = {0UL, 0UL, 0UL};
     for (size_t d = 0; d < dim; d++) {
-        index_min[d] = floor(coarse_bin_resolution*(local_bounding_box_min[d]-bounding_box_min[d])/(bounding_box_max[d] - bounding_box_min[d]);
-        index_min[d] = std::max(index_min[d],0);
-        index_max[d] = floor(coarse_bin_resolution*(local_bounding_box_max[d]-bounding_box_min[d])/(bounding_box_max[d] - bounding_box_min[d]);
-        index_max[d] = std::min(index_max[d],coarse_bin_resolution-1);
-        index_size[d] = index_max-index_min;
+      index_min[d] =
+          std::floor(coarse_bin_resolution * (local_bounding_box_min[d] - bounding_box_min[d]) /
+                     (bounding_box_max[d] - bounding_box_min[d]));
+      index_min[d] = std::max(index_min[d], 0UL);
+      index_max[d] =
+          std::floor(coarse_bin_resolution * (local_bounding_box_max[d] - bounding_box_min[d]) /
+                     (bounding_box_max[d] - bounding_box_min[d]));
+      index_max[d] = std::min(index_max[d], coarse_bin_resolution - 1);
     }
 
     // flatten global bins
@@ -100,7 +104,7 @@ quick_index<dim>::quick_index(const std::vector<std::array<3, double>> locations
         for (size_t k = index_min[2]; i <= index_max[2]; i++) {
           size_t bin_index =
               i + j * coarse_bin_resolution + k * coarse_bin_resolution * coarse_bin_resolution;
-          local_bins.append(bin_index);
+          local_bins.push_back(bin_index);
         }
       }
     }
@@ -108,22 +112,22 @@ quick_index<dim>::quick_index(const std::vector<std::array<3, double>> locations
     // build a global map for number of entries into the global bins on each processor
     // creates a (nbins**dim)*nranks sized array
     size_t nbins = pow(coarse_bin_resolution, dim);
-    global_index_per_bin_per_proc = std::vector<size_t>(nbis * rtt_c4::nodes(), 0);
+    std::vector<size_t> global_index_per_bin_per_proc(nbins * rtt_c4::nodes(), 0UL);
     for (auto mapItr = coarse_index_map.begin(); mapItr != coarse_index_map.end(); mapItr++) {
-      size_t gipbpp_index = *mapItr.first + nbins * rtt_c4::node();
-      global_index_per_bin_per_proc[gipbpp_index] = *mapItr.second.size();
+      size_t gipbpp_index = mapItr->first + nbins * rtt_c4::node();
+      global_index_per_bin_per_proc[gipbpp_index] = mapItr->second.size();
     }
-    rtt_c4::global_sum(global_index_per_bin_per_proc, nbins * rtt_c4::nodes());
+    rtt_c4::global_sum(&global_index_per_bin_per_proc[0], nbins * rtt_c4::nodes());
 
     // calculate local ghost buffer size
     local_ghost_buffer_size = 0;
-    for (auto binItr = local_bins.begin(); binItr != local_bins.end(); binIter++) {
+    for (auto binItr = local_bins.begin(); binItr != local_bins.end(); binItr++) {
       for (size_t proc = 0; proc < rtt_c4::nodes(); proc++) {
         if (rtt_c4::node() != proc) {
           size_t gipbpp_index = *binItr + nbins * proc;
           // build up the local ghost index map
-          for (int i = 0, i < global_index_per_bin_per_proc[gipbpp_index]; i++)
-            local_ghost_index_map[*binItr].append(local_ghost_buffer_size + i);
+          for (int i = 0; i < global_index_per_bin_per_proc[gipbpp_index]; i++)
+            local_ghost_index_map[*binItr].push_back(local_ghost_buffer_size + i);
           // accumulate the total ghost points
           local_ghost_buffer_size += global_index_per_bin_per_proc[gipbpp_index];
         }
@@ -133,64 +137,63 @@ quick_index<dim>::quick_index(const std::vector<std::array<3, double>> locations
     // Build a global list of buffer sizes
     std::vector<size_t> proc_ghost_buffer_size = std::vector<size_t>(rtt_c4::nodes(), 0);
     proc_ghost_buffer_size[rtt_c4::node()] = local_ghost_buffer_size;
-    rtt_c4::global_sum(proc_ghost_buffer_size, rtt_c4::nodes());
+    rtt_c4::global_sum(&proc_ghost_buffer_size[0], rtt_c4::nodes());
 
     // calculate the put map so each node knows which processor to send data
     // and where to index that data
     for (auto mapItr = coarse_index_map.begin(); mapItr != coarse_index_map.end(); mapItr++) {
       for (size_t proc = 0; proc < rtt_c4::nodes(); proc++) {
         if (rtt_c4::node() != proc) {
-          size_t gipbpp_index = *binItr + nbins * proc;
+          size_t gipbpp_index = mapItr->first + nbins * proc;
           size_t rank_count = global_index_per_bin_per_proc[gipbpp_index];
           if (rank_count > 0) {
             // calculate offset for flat_index_id vector
             size_t offset = 0;
-            for (size_t sum_i = nbins * proc; sum_i < gipbpp; sum_ii++)
+            for (size_t sum_i = nbins * proc; sum_i < gipbpp_index; sum_i++)
               offset += global_index_per_bin_per_proc[sum_i];
-            put_window_map[*mapItr] =
-                std::pair<size_t, std::pari<size_t, size_t>>{proc, std::pair < size_t, size_t} {
-              proc_ghost_buffer_size[proc], offset
-            }
-          };
+            // build up map data
+            put_window_map[mapItr->first] =
+                std::array<size_t, 3>{proc, proc_ghost_buffer_size[proc], offset};
           }
         }
       }
     }
 
     // allocate the ghost data location array
-    local_ghost_location =
-        std::vector<std::array<3, double>>(local_ghost_buffer_size, {0.0, 0.0, 0.0});
+    local_ghost_locations =
+        std::vector<std::array<double, 3>>(local_ghost_buffer_size, {0.0, 0.0, 0.0});
     // Use one sided MPI Put commands to fill up the ghost cell location data
     std::vector<double> position_ghost_vector(local_ghost_buffer_size);
     MPI_Win win;
     MPI_Win_create_dynamic(MPI_INFO_NULL, MPI_COMM_WORLD, &win);
-    MPI_Win_attach(win, position_ghost_vector, local_ghost_buffer_size * sizeof(double));
+    MPI_Win_attach(win, &position_ghost_vector[0], local_ghost_buffer_size * sizeof(double));
 
     // working from my local data put the ghost data on the other ranks
     for (size_t d = 0; d < dim; d++) {
-      for (auto putIter = local_ghost_index_map.begin(); putIter != local_ghost_index_map.end();
-           putIter++) {
-        const auto &index_vector = coarse_index_map[*putIter.first].second;
+      for (auto putItr = local_ghost_index_map.begin(); putItr != local_ghost_index_map.end();
+           putItr++) {
+        const auto &index_vector = coarse_index_map[putItr->first];
         // fill up the current ghost cell data for this dimension
         std::vector<double> put_buffer(index_vector.size());
         int putIndex = 0;
-        for (auto indexIter = index_vector.begin(); indexIter != index_vector.end(); indexIter++) {
-          put_buffer[putIndex] = *indexIter[d];
+        for (auto indexItr = index_vector.begin(); indexItr != index_vector.end(); indexItr++) {
+          put_buffer[putIndex] = locations[*indexItr][d];
           putIndex++;
         }
-        int put_rank = *putIter.second.first;
-        int put_rank_buffer_size = *putIter.second.second.first;
-        int put_offset = *putIter.second.second.second;
-        MPI_Put(put_buffer.begin(), put_buffer.size(), put_rank, put_offset, put_rank_buffer_size,
-                double, win);
+        int put_rank = putItr->second[0];
+        int put_rank_buffer_size = putItr->second[1];
+        int put_offset = putItr->second[2];
+        MPI_Put(&put_buffer[0], put_buffer.size(), rtt_c4::C4_Traits<double>::tag, put_rank,
+                put_offset, put_rank_buffer_size, rtt_c4::C4_Traits<double>::tag, win);
       }
       // alright move the position buffer to the weighting
-      rtt_c4::barrier(); // be sure everyone is done placing their data
+      rtt_c4::global_barrier(); // be sure everyone is done placing their data
       int posIndex = 0;
-      for (auto posIter = position_ghost_vector.begin(); posIter != position_ghost_vector.end();
-           posIter++)
-        local_ghost_location[posIndex][d] = *posIter;
+      for (auto posItr = position_ghost_vector.begin(); posItr != position_ghost_vector.end();
+           posItr++)
+        local_ghost_locations[posIndex][d] = *posItr;
     }
+  } // End domain decomposed data construction
 }
 
 } // end namespace  rtt_kde
